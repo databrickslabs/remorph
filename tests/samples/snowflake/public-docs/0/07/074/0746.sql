6640c9ee96e5fa1aa064b2679a460990a98ffc74
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-10-29 08:46:08 +0100

    Update 0746.sql

--- tests/samples/snowflake/public-docs/0/07/074/0746.sql
+++
@@ -1,9 +0,0 @@
-TO_CHAR( <expr> )
-TO_CHAR( <numeric_expr> [, '<format>' ] )
-TO_CHAR( <date_or_time_expr> [, '<format>' ] )
-TO_CHAR( <binary_expr> [, '<format>' ] )
-
-TO_VARCHAR( <expr> )
-TO_VARCHAR( <numeric_expr> [, '<format>' ] )
-TO_VARCHAR( <date_or_time_expr> [, '<format>' ] )
-TO_VARCHAR( <binary_expr> [, '<format>' ] )
