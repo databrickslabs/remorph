8ea67c898d99f616e9b3bce8781daefc57da851c
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-10-29 08:41:41 +0100

    Update 0700.sql

--- tests/samples/snowflake/public-docs/0/07/070/0700.sql
+++
@@ -1,9 +0,0 @@
-TO_TIME( <string_expr> [, <format> ] )
-TO_TIME( <timestamp_expr> )
-TO_TIME( '<integer>' )
-TO_TIME( <variant_expr> )
-
-TIME( <string_expr> )
-TIME( <timestamp_expr> )
-TIME( '<integer>' )
-TIME( <variant_expr> )
