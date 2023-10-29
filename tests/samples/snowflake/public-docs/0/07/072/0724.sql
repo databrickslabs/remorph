60b1d6880c890c2142b2f6d976723b66ea170e4a
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-10-29 08:44:13 +0100

    Update 0724.sql

--- tests/samples/snowflake/public-docs/0/07/072/0724.sql
+++
@@ -1,5 +0,0 @@
-TO_DECIMAL( <expr> [, '<format>' ] [, <precision> [, <scale> ] ] )
-
-TO_NUMBER( <expr> [, '<format>' ] [, <precision> [, <scale> ] ] )
-
-TO_NUMERIC( <expr> [, '<format>' ] [, <precision> [, <scale> ] ] )
