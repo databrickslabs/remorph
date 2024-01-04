828012e6e782fefbf3a3065750dbcf3b88380bb3
Bishwajit <bishwajit.dey@databricks.com>
2024-01-04 15:14:14 +0530

    Refactor code.

--- src/databricks/labs/remorph/snow/dialect_utils.py
+++ src/databricks/labs/remorph/snow/dialect_utils.py
@@ -1,38 +1,39 @@
 from sqlglot import ErrorLevel, exp, parse
+from sqlglot.expressions import Expression
 
 from databricks.labs.remorph.snow.snowflake import Snow
 
 
-def check_for_unsupported_lca(dialect, sql, filename, error_list):
-    # Check for presence of unsupported lateral column aliases in window expressions and where clauses
+def check_for_unsupported_lca(
+    dialect: str,
+    sql: str,
+    filename: str,
+    error_list: list[tuple[str, str]],
+):
+    """Check for presence of unsupported lateral column aliases in window expressions and where clauses"""
     if dialect.upper() == "SNOWFLAKE":
         dialect = Snow
     try:
         for expr in parse(sql, read=dialect, error_level=ErrorLevel.IGNORE):
-            if not expr:
-                continue
-            for select in expr.find_all(exp.Select):
-                alias_names = {alias.output_name for alias in select.find_all(exp.Alias)}
-                for window in select.find_all(exp.Window):
-                    for column in window.find_all(exp.Column):
-                        if column.name in alias_names:
-                            error_list.append(
-                                (
-                                    filename,
-                                    f"Unsupported operation found in file {filename}. "
-                                    f"Lateral column alias `{column.name}` found in window expression. "
-                                    f"Needs manual review.",
-                                )
-                            )
-                for where in select.find_all(exp.Where):
-                    for column in where.find_all(exp.Column):
-                        if column.name in alias_names:
-                            error_list.append(
-                                (
-                                    filename,
-                                    f"Unsupported operation found in file {filename}. "
-                                    f"Lateral column alias `{column.name}` found in where clause. Needs manual review.",
-                                )
-                            )
+            if expr:
+                for select in expr.find_all(exp.Select):
+                    _check_lca_usage_in_select(select, filename, error_list)
     except Exception as e:
         error_list.append((filename, f"Error while preprocessing {filename}: {e}"))
+
+
+def _check_lca_usage_in_select(
+    select: Expression,
+    filename: str,
+    error_list: list[tuple[str, str]],
+):
+    alias_names = {alias.output_name for alias in select.find_all(exp.Alias)}
+    for child in select.find_all(exp.Window, exp.Where):
+        for column in child.find_all(exp.Column):
+            if column.name in alias_names:
+                err_message = (
+                    f"Unsupported operation found in file {filename}. "
+                    f"Lateral column alias `{column.name}` found in window expression or where clause. "
+                    "Needs manual review."
+                )
+                error_list.append((filename, err_message))
