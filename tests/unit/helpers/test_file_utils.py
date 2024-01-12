4f778c05177ebe36aa0568a0c9172a9bf3a893be
Ganesh Dogiparthi <ganeshdogiparthi88@gmail.com>
2024-01-12 18:16:48 +0530

    Added test case for dir_walk

--- tests/unit/helpers/test_file_utils.py
+++ tests/unit/helpers/test_file_utils.py
@@ -1,8 +1,9 @@
 import os
 import tempfile
 import unittest
+from pathlib import Path
 
-from databricks.labs.remorph.helpers.file_utils import is_sql_file, make_dir, remove_bom
+from databricks.labs.remorph.helpers.file_utils import dir_walk, is_sql_file, make_dir, remove_bom
 
 
 class TestUtilsMethods(unittest.TestCase):
@@ -37,3 +38,45 @@ class TestUtilsMethods(unittest.TestCase):
 
         # Check if the directory now exists
         self.assertTrue(os.path.exists(new_dir_path))
+
+    def test_dir_walk(self):
+        """Test 1 - correct structure for single file"""
+        path = Path("test_dir")
+        path.mkdir()
+        (Path(path) / "test_file.txt").touch()
+        result = list(dir_walk(path))
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][0], path)
+        self.assertEqual(len(result[0][1]), 0)
+        self.assertEqual(len(result[0][2]), 1)
+        os.remove(Path(path) / "test_file.txt")
+        os.rmdir(path)
+
+        """Test 2 - correct structure for nested directories and files"""
+        path = Path("test_dir")
+        path.mkdir()
+        (Path(path) / "test_file.txt").touch()
+        (Path(path) / "nested_dir").mkdir()
+        (Path(path) / "nested_dir" / "nested_file.txt").touch()
+        result = list(dir_walk(path))
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0][0], path)
+        self.assertEqual(len(result[0][1]), 1)
+        self.assertEqual(len(result[0][2]), 1)
+        self.assertEqual(result[1][0], Path(path) / "nested_dir")
+        self.assertEqual(len(result[1][1]), 0)
+        self.assertEqual(len(result[1][2]), 1)
+        os.remove(Path(path) / "test_file.txt")
+        os.remove(Path(path) / "nested_dir" / "nested_file.txt")
+        os.rmdir(Path(path) / "nested_dir")
+        os.rmdir(path)
+
+        """Test 3 - empty directory"""
+        path = Path("empty_dir")
+        path.mkdir()
+        result = list(dir_walk(path))
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][0], path)
+        self.assertEqual(len(result[0][1]), 0)
+        self.assertEqual(len(result[0][2]), 0)
+        os.rmdir(path)
