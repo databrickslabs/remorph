1bd3528e60ea4bfcc48caf158702ba914879da07
Bishwajit <bishwajit.dey@databricks.com>
2024-01-15 13:53:16 +0530

    Add test cases for LCA validation and refactor code

--- src/databricks/labs/remorph/snow/dialect_utils.py
+++ src/databricks/labs/remorph/snow/dialect_utils.py
@@ -1,6 +1,7 @@
 from sqlglot import ErrorLevel, exp, parse
 from sqlglot.expressions import Expression
 
+from databricks.labs.remorph.helpers.morph_status import ParseError, ValidationError
 from databricks.labs.remorph.snow.snowflake import Snow
 
 
@@ -8,25 +9,31 @@ def check_for_unsupported_lca(
     dialect: str,
     sql: str,
     filename: str,
-    error_list: list[tuple[str, str]],
-):
-    """Check for presence of unsupported lateral column aliases in window expressions and where clauses"""
+) -> list[ValidationError | ParseError]:
+    """
+    Check for presence of unsupported lateral column aliases in window expressions and where clauses
+    :return: A list of errors if found
+    """
+    error_list = []
+
     if dialect.upper() == "SNOWFLAKE":
         dialect = Snow
     try:
-        for expr in parse(sql, read=dialect, error_level=ErrorLevel.IGNORE):
+        for expr in parse(sql, read=dialect, error_level=ErrorLevel.RAISE):
             if expr:
                 for select in expr.find_all(exp.Select):
-                    _check_lca_usage_in_select(select, filename, error_list)
+                    error_list.extend(_check_lca_usage_in_select(select, filename))
     except Exception as e:
-        error_list.append((filename, f"Error while preprocessing {filename}: {e}"))
+        error_list.append(ParseError(filename, f"Error while preprocessing {filename}: {e}"))
+
+    return error_list
 
 
 def _check_lca_usage_in_select(
     select: Expression,
     filename: str,
-    error_list: list[tuple[str, str]],
-):
+) -> list[ValidationError]:
+    error_list = []
     alias_names = {alias.output_name for alias in select.find_all(exp.Alias)}
     for child in select.find_all(exp.Window, exp.Where):
         for column in child.find_all(exp.Column):
@@ -36,4 +43,6 @@ def _check_lca_usage_in_select(
                     f"Lateral column alias `{column.name}` found in window expression or where clause. "
                     "Needs manual review."
                 )
-                error_list.append((filename, err_message))
+                error_list.append(ValidationError(filename, err_message))
+
+    return error_list
---
+++ tests/unit/snow/test_dialect_utils.py
@@ -0,0 +1,61 @@
+from databricks.labs.remorph.snow.dialect_utils import check_for_unsupported_lca
+
+
+class TestDialectUtils:
+    def test_snow_query_with_no_unsupported_lca_usage(self):
+        dialect = "SNOWFLAKE"
+        sql = """
+            SELECT
+                t.col1,
+                t.col2,
+                t.col3 AS ca,
+            FROM table1 t
+        """
+        filename = "test_file1.sql"
+
+        error_list = check_for_unsupported_lca(dialect, sql, filename)
+        assert not error_list
+
+    def test_snow_query_with_lca_in_where(self):
+        dialect = "SNOWFLAKE"
+        sql = """
+            SELECT
+                t.col1,
+                t.col2,
+                t.col3 AS ca,
+            FROM table1 t
+            WHERE ca in ('v1', 'v2')
+        """
+        filename = "test_file2.sql"
+
+        error_list = check_for_unsupported_lca(dialect, sql, filename)
+        assert error_list
+
+    def test_snow_query_with_lca_in_window(self):
+        dialect = "SNOWFLAKE"
+        sql = """
+            SELECT
+                t.col1,
+                t.col2,
+                t.col3 AS ca,
+                ROW_NUMBER() OVER (PARTITION by ca ORDER BY t.col2 DESC) AS rn
+            FROM table1 t
+        """
+        filename = "test_file3.sql"
+
+        error_list = check_for_unsupported_lca(dialect, sql, filename)
+        assert error_list
+
+    def test_snow_query_with_error(self):
+        dialect = "SNOWFLAKE"
+        sql = """
+            SELECT
+                t.col1
+                t.col2,
+                t.col3 AS ca,
+            FROM table1 t
+        """
+        filename = "test_file4.sql"
+
+        error_list = check_for_unsupported_lca(dialect, sql, filename)
+        assert error_list
