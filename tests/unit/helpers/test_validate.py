5fbcea5ab160424d2b28fdce7ba73a760c6e7dec
Bishwajit <147722855+bishwajit-db@users.noreply.github.com>
2024-01-19 10:24:11 +0530

    Add test cases for query validator (#82)
    Add test cases for query validator

--- tests/unit/helpers/test_validate.py
+++ tests/unit/helpers/test_validate.py
@@ -1,17 +1,22 @@
-import unittest
-from io import StringIO
-from unittest.mock import patch
+from unittest.mock import MagicMock, Mock, create_autospec, patch
 
 import pytest
+from databricks.connect import DatabricksSession
+from pyspark.sql.utils import AnalysisException, ParseException
 
 from databricks.labs.remorph.config import MorphConfig
 from databricks.labs.remorph.helpers.validate import Validate
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-class TestValidate(unittest.TestCase):
-    def setUp(self):
-        self.config = MorphConfig(
+class TestValidate:
+    @pytest.fixture
+    def spark_session(self):
+        spark = create_autospec(DatabricksSession.builder.getOrCreate())
+        yield spark
+
+    @pytest.fixture
+    def morph_config(self):
+        yield MorphConfig(
             source="snowflake",
             input_sql="input_sql",
             output_folder="output_folder",
@@ -19,48 +24,86 @@ class TestValidate(unittest.TestCase):
             catalog_name="catalog",
             schema_name="schema",
         )
-        self.validate = Validate()
 
-    @patch.object(Validate, "query")
-    def test_query_valid(self, mock_validate_query):
-        mock_validate_query.return_value = (True, None)
-        flag, exception = self.validate.query(
-            "SELECT current_timestamp()", self.config.catalog_name, self.config.schema_name
-        )
-        self.assertTrue(flag)
-        self.assertIsNone(exception)
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_valid_query(self, spark_session):
+        validator = Validate()
+        validator.spark = spark_session
+        query = "SELECT * FROM a_table"
+        result, exception = validator.query(query)
+        spark_session.sql.assert_called()
+        assert result is True
+        assert exception is None
 
-    @patch.object(Validate, "query")
-    def test_query_invalid(self, mock_validate_query):
-        mock_validate_query.return_value = (True, "[TABLE_OR_VIEW_NOT_FOUND]")
-        flag, exception = self.validate.query(
-            "SELECT * FROM mockdb.mocktable", self.config.catalog_name, self.config.schema_name
-        )
-        self.assertTrue(flag)
-        self.assertIsNotNone(exception)
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_valid_query_with_explicit_catalog(self, spark_session):
+        validator = Validate()
+        validator.spark = spark_session
+        query = "SELECT * FROM a_table"
+        result, exception = validator.query(query, catalog_name="c_name", schema_name="s_name")
+        spark_session.sql.assert_called()
+        assert result is True
+        assert exception is None
 
-    @patch.object(Validate, "validate_format_result")
-    def test_validate_format_result_valid_query(self, mock_validate_format_result):
-        output = "SELECT current_timestamp()"
-        mock_validate_format_result.return_value = (output + "\n;\n", None)
-        result, exception = self.validate.validate_format_result(self.config, output)
-        self.assertEqual(result, output + "\n;\n")
-        self.assertIsNone(exception)
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_query_with_syntax_error(self, spark_session):
+        validator = Validate()
+        validator.spark = spark_session
+        validator.spark.sql = MagicMock(side_effect=ParseException("[Syntax error]"))
+        query = "SELECT * a_table"
+        result, exception = validator.query(query)
+        spark_session.sql.assert_called()
+        assert result is False
+        assert "[Syntax error]" in exception
 
-    @patch.object(Validate, "validate_format_result")
-    def test_validate_format_result_invalid_query(self, mock_validate_format_result):
-        input_sql = "SELECT current_timestamps()"
-        exception = "[UNRESOLVED_ROUTINE]"
-        buffer = StringIO()
-        buffer.write("-------------- Exception Start-------------------\n")
-        buffer.write("/* \n")
-        buffer.write(exception)
-        buffer.write("\n */ \n")
-        buffer.write(input_sql)
-        buffer.write("\n ---------------Exception End --------------------\n")
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_query_with_analysis_error(self, spark_session):
+        error_types = [
+            ("[TABLE_OR_VIEW_NOT_FOUND]", True),
+            ("[TABLE_OR_VIEW_ALREADY_EXISTS]", True),
+            ("[UNRESOLVED_ROUTINE]", False),
+            ("Hive support is required to CREATE Hive TABLE (AS SELECT).;", True),
+            ("Some other analysis error", False),
+        ]
 
-        expected_result = buffer.getvalue()
-        mock_validate_format_result.return_value = (expected_result, exception)
-        result, exception = self.validate.validate_format_result(self.config, input_sql)
-        self.assertEqual(result, expected_result)
-        self.assertTrue("[UNRESOLVED_ROUTINE]" in exception)
+        for err, status in error_types:
+            validator = Validate()
+            validator.spark = spark_session
+            validator.spark.sql = MagicMock(side_effect=AnalysisException(err))
+            query = "SELECT * FROM a_table"
+            result, exception = validator.query(query)
+            spark_session.sql.assert_called()
+            assert result is status
+            assert err in exception
+
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_query_with_error(self, spark_session):
+        validator = Validate()
+        validator.spark = spark_session
+        validator.spark.sql = MagicMock(side_effect=Exception("[Some error]"))
+        query = "SELECT * FROM a_table"
+        result, exception = validator.query(query)
+        spark_session.sql.assert_called()
+        assert result is False
+        assert "[Some error]" in exception
+
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_validate_format_result_with_valid_query(self, spark_session, morph_config):
+        validator = Validate()
+        validator.spark = spark_session
+        query = "SELECT current_timestamp()"
+        result, exception = validator.validate_format_result(morph_config, query)
+        spark_session.sql.assert_called()
+        assert query in result
+        assert exception is None
+
+    @patch("databricks.labs.remorph.helpers.validate.DatabricksSession")
+    def test_validate_format_result_with_invalid_query(self, spark_session, morph_config):
+        validator = Validate()
+        validator.spark = spark_session
+        validator.query = Mock()
+        validator.query.return_value = (False, "[UNRESOLVED_ROUTINE]")
+        input_query = "SELECT fn() FROM tab"
+        result, exception = validator.validate_format_result(morph_config, input_query)
+        assert "Exception Start" in result
+        assert "[UNRESOLVED_ROUTINE]" in exception
