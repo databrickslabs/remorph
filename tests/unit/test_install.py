eb5c100732fe581f77d9293b1c78fb3c2b4f962c
vijaypavann-db <vijay.pavan@databricks.com>
2024-02-22 12:46:10 +0530

    Updated test cases for Morph Config

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -191,8 +191,8 @@ def test_get_schema(ws):
 
 
 def test_config(ws):
-    with pytest.raises(TypeError):
+    with pytest.raises(ValueError):
         config = MorphConfig(
-            source="snowflake", skip_validation=True, catalog_name="test_catalog", schema_name="test_schema"
+            source="snowflake", skip_validation=False, catalog_name="test_catalog", schema_name="test_schema", sdk_config=None
         )
         assert isinstance(config, MorphConfig)
