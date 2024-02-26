1fde40bae9d0a18b69c5e79f9655cb51488440f1
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2024-02-26 09:58:06 +0530

    Install Script (#106)
    Blueprint upgrade
    Install script
    
    ---------
    
    Co-authored-by: vijaypavann-db <vijay.pavan@databricks.com>
    Co-authored-by: Vijay Pavan Nissankararao <84904851+vijaypavann-db@users.noreply.github.com>
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- src/databricks/labs/remorph/config.py
+++ src/databricks/labs/remorph/config.py
@@ -8,10 +8,13 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class MorphConfig:
+    __file__ = "config.yml"
+    __version__ = 1
+
     source: str
-    input_sql: str
-    output_folder: str | None
     sdk_config: Config | None
+    input_sql: str | None = None
+    output_folder: str | None = None
     skip_validation: bool = False
     catalog_name: str = "transpiler_test"
     schema_name: str = "convertor_test"
