e05499deb5c428efdb8dddd093da5a2931f8e8f6
Ravikumar Thangaraj <ravi.t@databricks.com>
2024-03-12 10:12:07 +0530

    Moved the column extraction to a seperate function

--- src/databricks/labs/remorph/reconcile/execute.py
+++ src/databricks/labs/remorph/reconcile/execute.py
@@ -59,27 +59,9 @@ class Reconciliation:
 def build_query(table_conf: Tables, schema: list[Schema], layer: str, source: str) -> str:
     try:
         sql_query = StringIO()
-        # Extract distinct column names
-        join_columns = {col.source_name for col in table_conf.join_columns}
         schema_info = {getattr(v, "column_name"): v for v in schema}
 
-        if table_conf.select_columns is None:
-            select_columns = {sch.column_name for sch in schema}
-        else:
-            select_columns = {col for col in table_conf.select_columns}
-
-        if table_conf.jdbc_reader_options:
-            partition_column = {table_conf.jdbc_reader_options.partition_column}
-        else:
-            partition_column = set()
-
-        # Combine all column names
-        all_columns = join_columns | select_columns | partition_column
-
-        # Remove threshold and drop columns
-        threshold_columns = {thresh.column_name for thresh in table_conf.thresholds or []}
-        drop_columns = set(table_conf.drop_columns or [])
-        final_columns = all_columns - threshold_columns - drop_columns
+        final_columns, key_columns = _get_column_list(table_conf, schema)
 
         col_transformation = generate_transformation_rule_mapping(final_columns, schema_info, table_conf, source, layer)
 
@@ -88,26 +70,30 @@ def build_query(table_conf: Tables, schema: list[Schema], layer: str, source: st
 
         hash_expr = generate_hash_algorithm(source, final_expr)
 
-        # TODO  need to add the join and jdbc reader partition column in select
-        key_columns = join_columns | partition_column
-
         key_column_expr = [TransformRuleMapping.get_column_expression_with_alias(trans) for trans in col_transformation
                            if trans.column_name in key_columns]
 
+        # construct select query
         sql_query.write("select ")
+
+        # add hash column
         sql_query.write(hash_expr)
         sql_query.write(f" as {Constants.hash_column_name}")
         sql_query.write(" , ")
+
+        # add join column
         sql_query.write(",".join(key_column_expr))
         sql_query.write(" from ")
 
-        # table name
+        # add table name
         if layer == "source":
             table_name = table_conf.source_name
         else:
             table_name = table_conf.target_name
 
-            # where/filter clause
+        sql_query.write(table_name)
+
+        # where/filter clause
         if table_conf.filters:
             if layer == "source" and table_conf.filters.source:
                 query_filter = table_conf.filters.source
@@ -118,8 +104,6 @@ def build_query(table_conf: Tables, schema: list[Schema], layer: str, source: st
 
             sql_query.write(" where ")
             sql_query.write(query_filter)
-
-        sql_query.write(table_name)
 
         final_query = sql_query.getvalue()
         sql_query.close()
@@ -132,12 +116,12 @@ def build_query(table_conf: Tables, schema: list[Schema], layer: str, source: st
         raise Exception(message) from e
 
 
-def generate_transformation_rule_mapping(final_columns, schema, table_conf, source, layer):
+def generate_transformation_rule_mapping(columns, schema, table_conf, source, layer):
     transformations_dict = table_conf.list_to_dict(Transformation, "column_name")
     column_mapping_dict = table_conf.list_to_dict(ColumnMapping, "target_name")
 
     transformation_rule_mapping = []
-    for column in final_columns:
+    for column in columns:
         if column in transformations_dict.keys():
             transformation = getattr(transformations_dict.get(column), layer)
         else:
@@ -152,6 +136,31 @@ def generate_transformation_rule_mapping(final_columns, schema, table_conf, sour
         transformation_rule_mapping.append(TransformRuleMapping(column, transformation, column_alias))
 
     return transformation_rule_mapping
+
+
+def _get_column_list(table_conf, schema):
+    join_columns = {col.source_name for col in table_conf.join_columns}
+
+    if table_conf.select_columns is None:
+        select_columns = {sch.column_name for sch in schema}
+    else:
+        select_columns = {col for col in table_conf.select_columns}
+
+    if table_conf.jdbc_reader_options:
+        partition_column = {table_conf.jdbc_reader_options.partition_column}
+    else:
+        partition_column = set()
+
+    # Combine all column names
+    all_columns = join_columns | select_columns | partition_column
+
+    # Remove threshold and drop columns
+    threshold_columns = {thresh.column_name for thresh in table_conf.thresholds or []}
+    drop_columns = set(table_conf.drop_columns or [])
+    final_columns = all_columns - threshold_columns - drop_columns
+    key_columns = join_columns | partition_column
+
+    return final_columns, key_columns
 
 
 def _get_default_transformation(source, data_type):
