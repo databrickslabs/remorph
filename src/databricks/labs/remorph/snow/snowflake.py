dc2f149d79a8a1a513b7416a39c6206700667547
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2024-03-12 18:05:14 +0530

    `TO_NUMBER` without `format` edge case  (#172)

--- src/databricks/labs/remorph/snow/snowflake.py
+++ src/databricks/labs/remorph/snow/snowflake.py
@@ -197,8 +197,15 @@ def _parse_tonumber(args: list) -> local_expression.ToNumber:
 
     match len(args):
         case 1:
+            msg = (
+                "Precision and Scale are not specified, assuming defaults `precision`[38] and `scale`[0]. "
+                "If Format is not specified, it will be inferred as simple cast as decimal"
+            )
+            logger.warning(msg)
             return local_expression.ToNumber(this=seq_get(args, 0))
         case 3:
+            msg = "If Format is not specified, it will be inferred as simple cast as decimal"
+            logger.warning(msg)
             return local_expression.ToNumber(this=seq_get(args, 0), precision=seq_get(args, 1), scale=seq_get(args, 2))
         case 4:
             return local_expression.ToNumber(
