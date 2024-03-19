512fcdf1a3267bb4883c391bf26e6d6e490c50d6
Ganesh Dogiparthi <ganeshdogiparthi88@gmail.com>
2024-03-19 14:27:10 +0530

    review comments

--- src/databricks/labs/remorph/reconcile/connectors/snowflake.py
+++ src/databricks/labs/remorph/reconcile/connectors/snowflake.py
@@ -15,30 +15,38 @@ class SnowflakeDataSource(DataSource):
         return url
 
     def read_data(self, schema_name: str, catalog_name: str, table_or_query: str, table_conf: Tables) -> DataFrame:
-        if table_conf.jdbc_reader_options is None:
-            df = self.reader(table_or_query)
-        else:
-            df = (
+        try:
+            if table_conf.jdbc_reader_options is None:
+                return self.reader(table_or_query)
+
+            return (
                 self._get_jdbc_reader(table_or_query, self.get_jdbc_url, SourceDriver.SNOWFLAKE.value)
                 .options(**self._get_jdbc_reader_options(table_conf.jdbc_reader_options))
                 .load()
             )
-        return df
+        except PySparkException as e:
+            error_msg = f"An error occurred while fetching Snowflake Data using the following {table_or_query} in SnowflakeDataSource : {e!s}"
+            raise PySparkException(error_msg) from e
 
     def get_schema(self, table_name: str, schema_name: str, catalog_name: str) -> list[Schema]:
-        schema_query = self._get_schema_query(table_name, schema_name, catalog_name)
-        schema_df = self.reader(schema_query).load()
-        schema = [Schema(field.column_name.lower(), field.data_type.lower()) for field in schema_df.collect()]
-        return schema
+        try:
+            schema_query = self._get_schema_query(table_name, schema_name, catalog_name)
+            schema_df = self.reader(schema_query).load()
+            return [Schema(field.column_name.lower(), field.data_type.lower()) for field in schema_df.collect()]
+        except PySparkException as e:
+            error_msg = (
+                f"An error occurred while fetching Snowflake Schema using the following {table_name} in "
+                f"SnowflakeDataSource: {e!s}"
+            )
+            raise PySparkException(error_msg) from e
 
     def reader(self, query: str) -> DataFrame:
-        df = (
+        return (
             self.spark.read.format("snowflake")
             .option("dbtable", f"({query}) as tmp")
             .options(**self.connection_params)
             .load()
         )
-        return df
 
     @staticmethod
     def _get_schema_query(table_name: str, schema_name: str, catalog_name: str):
@@ -46,5 +54,3 @@ class SnowflakeDataSource(DataSource):
         when lower(data_type) = 'text' then concat('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')  else data_type end as data_type from 
         {catalog_name}.INFORMATION_SCHEMA.COLUMNS where lower(table_name)='{table_name}' and lower(table_schema) = '{schema_name}' order by ordinal_position
         """
-
-    snowflake_datatype_mapper = {}
