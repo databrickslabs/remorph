037834a06a61432966a823057e39a78f75b26273
Bishwajit <147722855+bishwajit-db@users.noreply.github.com>
2024-03-31 17:29:08 +0530

    Add serverless validation using lsql library (#176)
    Add serverless validation using lsql library
    
    ---------
    
    Co-authored-by: sundar.shankar <sundar.shankar@databricks.com>

---
+++ src/databricks/labs/remorph/helpers/db_sql.py
@@ -0,0 +1,32 @@
+import logging
+
+from databricks.labs.lsql.backends import (
+    DatabricksConnectBackend,
+    SqlBackend,
+    StatementExecutionBackend,
+)
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.errors.base import DatabricksError
+
+from databricks.labs.remorph.config import MorphConfig
+
+logger = logging.getLogger(__name__)
+
+
+def get_sql_backend(ws: WorkspaceClient, config: MorphConfig) -> SqlBackend:
+    sdk_config = ws.config
+    warehouse_id = isinstance(sdk_config.warehouse_id, str) and sdk_config.warehouse_id
+    catalog_name = config.catalog_name
+    schema_name = config.schema_name
+    if warehouse_id:
+        sql_backend = StatementExecutionBackend(ws, warehouse_id, catalog=catalog_name, schema=schema_name)
+    else:
+        sql_backend = DatabricksConnectBackend(ws)
+        try:
+            sql_backend.execute(f"use catalog {catalog_name}")
+            sql_backend.execute(f"use {schema_name}")
+        except DatabricksError as dbe:
+            logger.error(f"Catalog or Schema could not be selected: {dbe}")
+            raise dbe
+    logger.info(f"SQL Backend used for validation: {type(sql_backend).__name__}")
+    return sql_backend
