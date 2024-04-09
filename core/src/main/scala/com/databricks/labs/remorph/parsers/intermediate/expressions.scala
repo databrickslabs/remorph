f27cc3b77398a24f6b974799aec01b2a44de60f9
Serge Smertin <serge.smertin@databricks.com>
2024-04-09 18:16:56 +0200

    ..

--- core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/expressions.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/expressions.scala
@@ -4,20 +4,113 @@ package com.databricks.labs.remorph.parsers.intermediate
 // expressions in SQL appear.
 abstract class Expression extends TreeNode {}
 
-case class Literal() extends Expression {}
-case class UnresolvedAttribute() extends Expression {}
-case class UnresolvedFunction() extends Expression {}
-case class ExpressionString() extends Expression {}
-case class UnresolvedStar() extends Expression {}
-case class Alias() extends Expression {}
-case class Cast() extends Expression {}
-case class UnresolvedRegex() extends Expression {}
-case class SortOrder() extends Expression {}
-case class LambdaFunction() extends Expression {}
-case class Window() extends Expression {}
-case class UnresolvedExtractValue() extends Expression {}
-case class UpdateFields() extends Expression {}
-case class UnresolvedNamedLambdaVariable() extends Expression {}
-case class CommonInlineUserDefinedFunction() extends Expression {}
-case class CallFunction() extends Expression {}
-case class NamedArgumentExpression() extends Expression {}
+abstract class FrameType
+case object UndefinedFrame extends FrameType
+case object RangeFrame extends FrameType
+case object RowsFrame extends FrameType
+
+case class FrameBoundary(current_row: Boolean, unbounded: Boolean, value: Expression)
+
+case class WindowFrame(frame_type: FrameType, lower: FrameBoundary, upper: FrameBoundary)
+
+case class Window(
+    window_function: Expression,
+    partition_spec: Seq[Expression],
+    sort_order: Seq[SortOrder],
+    frame_spec: WindowFrame)
+    extends Expression {}
+
+abstract class SortDirection
+case object UnspecifiedSortDirection extends SortDirection
+case object AscendingSortDirection extends SortDirection
+case object DescendingSortDirection extends SortDirection
+
+abstract class NullOrdering
+case object SortNullsUnspecified extends NullOrdering
+case object SortNullsFirst extends NullOrdering
+case object SortNullsLast extends NullOrdering
+
+case class SortOrder(child: Expression, direction: SortDirection, nullOrdering: NullOrdering) extends Expression {}
+
+case class Cast(expr: Expression, dataType: DataType, type_str: String) extends Expression {}
+
+case class Decimal(value: String, precision: Option[Int], scale: Option[Int]) extends Expression {}
+
+case class CalendarInterval(months: Int, days: Int, microseconds: Long) extends Expression {}
+
+case class ArrayExpr(dataType: DataType, elements: Seq[Literal]) extends Expression {}
+
+case class MapExpr(key_type: DataType, value_type: DataType, keys: Seq[Literal], values: Seq[Literal])
+    extends Expression {}
+
+case class Struct(struct_type: DataType, elements: Seq[Literal]) extends Expression {}
+
+case class Literal(
+    nullType: DataType,
+    binary: Array[Byte],
+    boolean: Boolean,
+    byte: Int,
+    short: Int,
+    integer: Int,
+    float: Float,
+    decimal: Decimal,
+    double: Double,
+    string: String,
+    date: Int,
+    timestamp: Long,
+    timestamp_ntz: Long,
+    calendar_interval: CalendarInterval,
+    year_month_interval: Int,
+    day_time_interval: Long,
+    array: ArrayExpr,
+    map: MapExpr,
+    struct: Struct)
+    extends Expression {}
+
+case class UnresolvedAttribute(unparsed_identifier: String, plan_id: Long, is_metadata_column: Boolean)
+    extends Expression {}
+
+case class UnresolvedFunction(
+    function_name: String,
+    arguments: Seq[Expression],
+    is_distinct: Boolean,
+    is_user_defined_function: Boolean)
+    extends Expression {}
+
+case class ExpressionString(expression: String) extends Expression {}
+
+case class UnresolvedStar(unparsed_target: String) extends Expression {}
+
+case class UnresolvedRegex(col_name: String, plan_id: Long) extends Expression {}
+
+case class UnresolvedExtractValue(child: Expression, extraction: Expression) extends Expression {}
+
+case class UpdateFields(struct_expression: Expression, field_name: String, value_expression: Option[Expression])
+    extends Expression {}
+
+case class Alias(expr: Expression, name: Seq[String], metadata: Option[String]) extends Expression {}
+
+case class LambdaFunction(function: Expression, arguments: Seq[UnresolvedNamedLambdaVariable]) extends Expression {}
+
+case class UnresolvedNamedLambdaVariable(name_parts: Seq[String]) extends Expression {}
+
+case class PythonUDF(output_type: DataType, eval_type: Int, command: Array[Byte], python_ver: String)
+    extends Expression {}
+
+case class ScalarScalaUDF(payload: Array[Byte], inputTypes: Seq[DataType], outputType: DataType, nullable: Boolean)
+    extends Expression {}
+
+case class JavaUDF(class_name: String, output_type: Option[DataType], aggregate: Boolean) extends Expression {}
+
+case class CommonInlineUserDefinedFunction(
+    function_name: String,
+    deterministic: Boolean,
+    arguments: Seq[Expression],
+    python_udf: Option[PythonUDF],
+    scalar_scala_udf: Option[ScalarScalaUDF],
+    java_udf: Option[JavaUDF])
+    extends Expression {}
+
+case class CallFunction(function_name: String, arguments: Seq[Expression]) extends Expression {}
+
+case class NamedArgumentExpression(key: String, value: Expression) extends Expression {}
