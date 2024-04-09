c316517724e439685ab4f62cd8a92b4ea40e5b9e
Serge Smertin <serge.smertin@databricks.com>
2024-04-09 18:39:42 +0200

    ..

--- core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/Plan.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/Plan.scala
@@ -4,6 +4,4 @@ package com.databricks.labs.remorph.parsers.intermediate
 // client to the server. A [[Plan]] can either be of the type [[Relation]] which is a reference
 // to the underlying logical plan or it can be of the [[Command]] type that is used to execute
 // commands on the server.
-abstract class Plan {
-
-}
+abstract class Plan
