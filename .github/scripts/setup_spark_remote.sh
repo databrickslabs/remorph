e110da2ab72078d1b4721767aa631bce452a27b5
Ravikumar <ravi.t@databricks.com>
2024-04-10 19:07:02 +0530

    Fixed the shell script

--- .github/scripts/setup_spark_remote.sh
+++ .github/scripts/setup_spark_remote.sh
@@ -8,16 +8,16 @@ cd $HOME/spark
 hatch shell
 
 spark-version=$(wget -O - https://dlcdn.apache.org/spark/ | grep 'href="spark' | sed 's:</a>:\n:g' | sed -n 's/.*>//p' | tr -d / | sort -r --version-sort | head -1)
-spark=${version}-bin-hadoop3
+spark=${spark-version}-bin-hadoop3
 spark_connect=spark-connect_2.12
 
-wget https://dlcdn.apache.org/spark/spark-${version}/${spark}.tgz
+wget https://dlcdn.apache.org/spark/spark-${spark-version}/${spark}.tgz
 
 tar -xvf ${spark}.tgz
 
-$HOME/spark/${spark}/sbin/start-connect-server.sh --packages org.apache.spark:${spark_connect}:${version}
+$HOME/spark/${spark}/sbin/start-connect-server.sh --packages org.apache.spark:${spark_connect}:${spark-version}
 
 #sleep time to start the server
 sleep 2m
 
-cat $HOME/spark/spark-${version}-bin-hadoop3/logs/spark-runner-org.apache.spark.sql.connect.service.SparkConnectServer*.out
+cat $HOME/spark/${spark-version}-bin-hadoop3/logs/spark-runner-org.apache.spark.sql.connect.service.SparkConnectServer*.out
