51fadc467d9e0b4fe951bb24a63fc61319888dc1
Vijay Pavan Nissankararao <84904851+vijaypavann-db@users.noreply.github.com>
2024-04-25 13:42:50 +0530

    Added `Configure Secrets` support to `databricks labs remorph configure-secrets` cli command  (#254)

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -4,8 +4,10 @@ from unittest.mock import create_autospec, patch
 import pytest
 import yaml
 
+from databricks.labs.blueprint.tui import MockPrompts
 from databricks.labs.remorph import cli
 from databricks.labs.remorph.config import MorphConfig
+from databricks.labs.remorph.helpers.recon_config_utils import ReconConfigPrompts
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import NotFound
 
@@ -283,3 +285,23 @@ def test_recon_with_valid_input(mock_workspace_client_cli):
     with patch("os.path.exists", return_value=True), patch("databricks.labs.remorph.cli.recon") as mock_recon:
         cli.reconcile(mock_workspace_client_cli, recon_conf, conn_profile, source, report)
         mock_recon.assert_called_once_with(recon_conf, conn_profile, source, report)
+
+
+def test_configure_secrets_databricks(mock_workspace_client):
+    source_dict = {"databricks": "0", "netezza": "1", "oracle": "2", "snowflake": "3"}
+    prompts = MockPrompts(
+        {
+            r"Select the source": source_dict["databricks"],
+        }
+    )
+
+    recon_conf = ReconConfigPrompts(mock_workspace_client, prompts)
+    recon_conf.prompt_source()
+
+    recon_conf.prompt_and_save_connection_details()
+
+
+def test_cli_configure_secrets_config(mock_workspace_client):
+    with patch("databricks.labs.remorph.cli.ReconConfigPrompts") as mock_recon_config:
+        cli.configure_secrets(mock_workspace_client)
+        mock_recon_config.assert_called_once_with(mock_workspace_client)
