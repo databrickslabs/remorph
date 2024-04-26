03ff87c45ced697bdd20bbcda6b7dc1ac87bb6ce
sundar.shankar <sundar.shankar@databricks.com>
2024-04-26 13:41:34 +0530

    Simple Select Query

--- core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlAstBuilder.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlAstBuilder.scala
@@ -4,6 +4,33 @@ import com.databricks.labs.remorph.parsers.tsql.TSqlParser._
 import com.databricks.labs.remorph.parsers.{intermediate => ir}
 
 import scala.collection.JavaConverters._
-class TSqlAstBuilder extends TSqlParserBaseVisitor[ir.TreeNode]{
-// noop
+
+/**
+ * @see
+ *   org.apache.spark.sql.catalyst.parser.AstBuilder
+ */
+class TSqlAstBuilder extends TSqlParserBaseVisitor[ir.TreeNode] {
+
+  // TODO investigate why this is needed
+  override protected def aggregateResult(aggregate: ir.TreeNode, nextResult: ir.TreeNode): ir.TreeNode = {
+    if (nextResult == null) {
+      aggregate
+    } else {
+      nextResult
+    }
+  }
+
+  override def visitSelect_statement_standalone(ctx: Select_statement_standaloneContext): ir.TreeNode = {
+    val rawExpression = ctx.select_statement().query_expression().query_specification()
+    val column_expression = rawExpression
+      .select_list()
+      .select_list_elem()
+      .asScala
+      .map(_.accept(new TSqlExpressionBuilder))
+    val from_table = rawExpression.table_sources().accept(new TSqlRelationBuilder)
+
+    ir.Project(from_table, column_expression)
+
+  }
+
 }
