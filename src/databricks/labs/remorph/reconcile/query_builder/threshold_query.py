a769bc3d8977d332d4eea5cb9760b21ecf94391c
Ganesh Dogiparthi <ganesh.dogiparthi@databricks.com>
2024-05-14 10:18:19 +0530

    threshold src and target queries

--- src/databricks/labs/remorph/reconcile/query_builder/threshold_query.py
+++ src/databricks/labs/remorph/reconcile/query_builder/threshold_query.py
@@ -175,3 +175,13 @@ class ThresholdQueryBuilder(QueryBuilder):
             true=exp.Identifier(this="Warning", quoted=True),
         )
         return exp.Case(ifs=[eq_if, between_if], default=exp.Identifier(this="Failed", quoted=True))
+
+    def build_threshold_query(self) -> str:
+        keys: list[str] = sorted(self.partition_column.union(self.join_columns).union(self.threshold_columns))
+        select_clause = [
+            build_column(this=col, alias=self.table_conf.get_tgt_to_src_col_mapping(col, self.layer)) for col in keys
+        ]
+        select_clause = self.add_transformations(select_clause, self.source)
+        query = select(*select_clause).from_(":tbl").where(self.filter)
+        logger.info(f"{self.source} Threshold query: {query}")
+        return query.sql(dialect=self.source)
