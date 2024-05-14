c67fe84bbbdcfc4493a119433546072348f04688
Ganesh Dogiparthi <ganesh.dogiparthi@databricks.com>
2024-05-14 12:25:03 +0530

    added docs

--- src/databricks/labs/remorph/reconcile/query_builder/threshold_query.py
+++ src/databricks/labs/remorph/reconcile/query_builder/threshold_query.py
@@ -177,6 +177,18 @@ class ThresholdQueryBuilder(QueryBuilder):
         return exp.Case(ifs=[eq_if, between_if], default=exp.Identifier(this="Failed", quoted=True))
 
     def build_threshold_query(self) -> str:
+        """
+        This method builds a threshold query based on the configuration of the table and the columns involved.
+
+        The query is constructed by selecting the necessary columns (partition, join, and threshold columns)
+        from a specified table. Any transformations specified in the table configuration are applied to the
+        selected columns. The query also includes a WHERE clause based on the filter defined in the table configuration.
+
+        The resulting query is then converted to a SQL string using the dialect of the source database.
+
+        Returns:
+            str: The SQL string representation of the threshold query.
+        """
         keys: list[str] = sorted(self.partition_column.union(self.join_columns).union(self.threshold_columns))
         select_clause = [
             build_column(this=col, alias=self.table_conf.get_tgt_to_src_col_mapping(col, self.layer)) for col in keys
