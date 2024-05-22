52ff3fc9c6a76e831c5932d97dec378e909bce7b
Ganesh Dogiparthi <ganesh.dogiparthi@databricks.com>
2024-05-22 14:07:05 +0530

    added acceptance.yaml for github actions

--- tests/integration/reconcile/test_execute.py
+++ tests/integration/reconcile/test_execute.py
@@ -1,6 +1,10 @@
 from databricks.connect import DatabricksSession
+from databricks.sdk.retries import retried
+from databricks.sdk.errors import NotFound
+from datetime import timedelta
 
 
+@retried(on=[NotFound], timeout=timedelta(minutes=5))
 def test_recon_gd(ws):
     spark = DatabricksSession.builder.sdkConfig(ws.config).getOrCreate()
     spark.sql("select 1 as col1, 2 as col2").show(100, False)
