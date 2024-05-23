d86ccbfc85b42b6227df1660aa75775706cbad1f
Jim Idle <jim.idle@databricks.com>
2024-05-23 13:06:09 -0600

    fix: See if GitHub code coverage can be sensible

--- core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlRelationBuilder.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlRelationBuilder.scala
@@ -56,9 +56,9 @@ class TSqlRelationBuilder extends TSqlParserBaseVisitor[ir.Relation] {
             ir.CrossJoin,
             Seq.empty,
             ir.JoinDataType(is_left_struct = false, is_right_struct = false)))
-      // GCOVR_EXCL_START  ctx.tableSource always returns at least 1 element
+      // $COVERAGE-OFF$  ctx.tableSource always returns at least 1 element
       case _ => ir.NoTable
-      // GCOVR_EXCL_STOP
+      // $COVERAGE-ON$
     }
   }
 
