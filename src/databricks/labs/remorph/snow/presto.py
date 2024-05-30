17848f55e33d7852f3632b64a116a295c23f128d
Ganesh Dogiparthi <ganesh.dogiparthi@databricks.com>
2024-05-30 11:08:05 +0530

    formating and test case

--- src/databricks/labs/remorph/snow/presto.py
+++ src/databricks/labs/remorph/snow/presto.py
@@ -1,35 +1,39 @@
-from typing import ClassVar
-
 from sqlglot.dialects.presto import Presto as presto
-from sqlglot.tokens import TokenType
 from sqlglot import exp
-from sqlglot.helper import apply_index_offset, seq_get
+from sqlglot.helper import seq_get
+from sqlglot.errors import ParseError
 
 
 def _build_approx_percentile(args: list) -> exp.Expression:
     if len(args) == 4:
         arg3 = seq_get(args, 3)
-        try:
-            number = float(arg3.this)
-            return exp.ApproxQuantile(
-                this=seq_get(args, 0),
-                weight=seq_get(args, 1),
-                quantile=seq_get(args, 2),
-                accuracy=exp.Literal(this=f'{int((1/number) * 100)} ', is_string=False),
-            )
-        except ValueError:
-            raise ValueError(f"Expected a string representation of a number for argument 2, but got {arg3.this}")
+        if arg3 is not None:
+            try:
+                number = float(arg3.this)
+                return exp.ApproxQuantile(
+                    this=seq_get(args, 0),
+                    weight=seq_get(args, 1),
+                    quantile=seq_get(args, 2),
+                    accuracy=exp.Literal(this=f'{int((1/number) * 100)} ', is_string=False),
+                )
+            except ValueError as exc:
+                raise ParseError(
+                    f"Expected a string representation of a number for argument 2, but got {arg3.this}"
+                ) from exc
     if len(args) == 3:
         arg2 = seq_get(args, 2)
-        try:
-            number = float(arg2.this)
-            return exp.ApproxQuantile(
-                this=seq_get(args, 0),
-                quantile=seq_get(args, 1),
-                accuracy=exp.Literal(this=f'{int((1/number) * 100)}', is_string=False)
-            )
-        except ValueError:
-            raise ValueError(f"Expected a string representation of a number for argument 2, but got {arg2.this}")
+        if arg2 is not None:
+            try:
+                number = float(arg2.this)
+                return exp.ApproxQuantile(
+                    this=seq_get(args, 0),
+                    quantile=seq_get(args, 1),
+                    accuracy=exp.Literal(this=f'{int((1/number) * 100)}', is_string=False),
+                )
+            except ValueError as exc:
+                raise ParseError(
+                    f"Expected a string representation of a number for argument 2, but got {arg2.this}"
+                ) from exc
     return exp.ApproxQuantile.from_arg_list(args)
 
 
--- tests/resources/functional/presto/test_approx_percentile/test_approx_percentile_5.sql
+++
@@ -1,7 +0,0 @@
-
--- presto sql:
-SELECT approx_percentile(height, weight, 0.5) FROM people;
-
--- databricks sql:
-SELECT approx_percentile(height, weight, 0.5) FROM people;
-
