639ea81d487cbc61fd2a3e3233f2288b15b3209b
Ganesh Dogiparthi <ganesh.dogiparthi@databricks.com>
2024-05-30 20:45:32 +0530

    fmt issues

--- remorph_transpile/transpiled/call_center.ddl
+++
@@ -1,5 +0,0 @@
-CREATE TABLE call_center (
-  cc_call_center_sk DECIMAL(38, 0),
-  cc_call_center_id STRING
-)
-;
--- remorph_transpile/transpiled/query2.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  wswscs.d_week_seq AS d_week_seq1,
-  sun_sales AS sun_sales1,
-  mon_sales AS mon_sales1
-FROM wswscs, date_dim
-WHERE
-  date_dim.d_week_seq = wswscs.d_week_seq AND d_year = 2001
-;
