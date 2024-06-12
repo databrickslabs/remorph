b51a57788e8ef34fe2a250b06510b9083f3338cf
Jim Idle <jim.idle@databricks.com>
2024-06-12 07:56:58 -0600

    Remove redundant whitespace
    Co-authored-by: Valentin Kasas <valentin.kasas@xebia.com>

--- tests/resources/functional/tsql/select/test_cte_xml.sql
+++ tests/resources/functional/tsql/select/test_cte_xml.sql
@@ -6,7 +6,7 @@
 -- tsql sql:
 WITH XMLNAMESPACES ('somereference' as namespace)
 SELECT col1 as 'namespace:col1',
-       col2      as 'namespace:col2'
+       col2 as 'namespace:col2'
 FROM  Table1
 WHERE col2 = 'xyz'
 FOR XML RAW ('namespace:namespace'), ELEMENTS;
