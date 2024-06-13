ec730fd8d697a94635e866a87d94ec9b7b4aec84
Bishwajit <147722855+bishwajit-db@users.noreply.github.com>
2024-06-13 15:24:39 +0530

    Remove USE catalog/schema statement as lsql has added the feature. (#465)
    Fixes #463

--- src/databricks/labs/remorph/helpers/db_sql.py
+++ src/databricks/labs/remorph/helpers/db_sql.py
@@ -7,9 +7,9 @@ from databricks.labs.lsql.backends import (
     SqlBackend,
     StatementExecutionBackend,
 )
-from databricks.labs.remorph.config import MorphConfig
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.errors.base import DatabricksError
+
+from databricks.labs.remorph.config import MorphConfig
 
 logger = logging.getLogger(__name__)
 
@@ -23,14 +23,8 @@ def get_sql_backend(ws: WorkspaceClient, config: MorphConfig) -> SqlBackend:
     if warehouse_id:
         sql_backend: SqlBackend = StatementExecutionBackend(ws, warehouse_id, catalog=catalog_name, schema=schema_name)
     else:
-        # assigning cluster id explicitly to the config as user can provide them during installation
+        # Assigning cluster id explicitly to the config as user can provide them during installation
         ws.config.cluster_id = cluster_id if cluster_id else ws.config.cluster_id
         sql_backend = RuntimeBackend() if "DATABRICKS_RUNTIME_VERSION" in os.environ else DatabricksConnectBackend(ws)
-        try:
-            sql_backend.execute(f"use catalog {catalog_name}")
-            sql_backend.execute(f"use {schema_name}")
-        except DatabricksError as dbe:
-            logger.error(f"Catalog or Schema could not be selected: {dbe}")
-            raise dbe
     logger.info(f"SQL Backend used for validation: {type(sql_backend).__name__}")
     return sql_backend
