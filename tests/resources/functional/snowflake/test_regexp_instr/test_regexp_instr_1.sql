208e4c85717252cddba06927331b1a76ca705447
Valentin Kasas <valentin.kasas@xebia.com>
2024-06-24 12:11:37 +0200

    Improve Snowflake coverage test success rate by removing unused tokens, adding more nonReservedKeywords and defining some functions

--- tests/resources/functional/snowflake/test_regexp_instr/test_regexp_instr_1.sql
+++ tests/resources/functional/snowflake/test_regexp_instr/test_regexp_instr_1.sql
@@ -1,6 +1,6 @@
 
 -- snowflake sql:
-SELECT regexp_instr(col1) AS regexp_instr_col1 FROM tabl;
+SELECT regexp_instr(col1, '.') AS regexp_instr_col1 FROM tabl;
 
 -- databricks sql:
-SELECT REGEXP_INSTR(col1) AS regexp_instr_col1 FROM tabl;
+SELECT REGEXP_INSTR(col1, '.') AS regexp_instr_col1 FROM tabl;
