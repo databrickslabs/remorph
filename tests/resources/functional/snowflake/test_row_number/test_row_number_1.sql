a10c501f734063e6b6da1acb90759692ef69c97d
vijaypavann-db <vijay.pavan@databricks.com>
2024-07-05 15:13:29 +0530

    Fixed Acceptance Testcases, updated queries with Window Specification

--- tests/resources/functional/snowflake/test_row_number/test_row_number_1.sql
+++ tests/resources/functional/snowflake/test_row_number/test_row_number_1.sql
@@ -1,6 +1,6 @@
 
 -- snowflake sql:
-SELECT symbol, exchange, shares, ROW_NUMBER() OVER (PARTITION BY exchange) AS row_number FROM trades;
+SELECT symbol, exchange, shares, ROW_NUMBER() OVER (PARTITION BY exchange ORDER BY shares) AS row_number FROM trades;
 
 -- databricks sql:
-SELECT symbol, exchange, shares, ROW_NUMBER() OVER (PARTITION BY exchange) AS row_number FROM trades;
+SELECT symbol, exchange, shares, ROW_NUMBER() OVER (PARTITION BY exchange ORDER BY shares NULLS LAST) AS row_number FROM trades;
