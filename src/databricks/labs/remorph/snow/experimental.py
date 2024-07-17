8bfb098e726b4f0d85cf9ad7ca29688a704b49f2
sundar.shankar <sundar.shankar@databricks.com>
2024-07-17 11:38:56 +0530

    Fix PARSE_JSON_NAME test failures

--- src/databricks/labs/remorph/snow/experimental.py
+++ src/databricks/labs/remorph/snow/experimental.py
@@ -11,8 +11,10 @@ class DatabricksExperimental(extended_databricks.Databricks):
     databricks = extended_databricks.Databricks()
 
     class Generator(extended_databricks.Databricks.Generator):
+        PARSE_JSON_NAME = "PARSE_JSON"
         TRANSFORMS = {
             **extended_databricks.Databricks.Generator.TRANSFORMS,
+            #exp.ParseJSON: _parse_json,
         }
         TRANSFORMS.pop(exp.ParseJSON, None)
 
