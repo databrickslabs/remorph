098e2eeb930977cff8d0f6867a71c400c8a85e68
vijaypavann-db <vijay.pavan@databricks.com>
2024-07-18 20:20:30 +0530

    Updated Transpile CLI to use Workspace config

--- src/databricks/labs/remorph/cli.py
+++ src/databricks/labs/remorph/cli.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 from pyspark.sql import SparkSession
@@ -7,72 +6,26 @@ from databricks.connect import DatabricksSession
 from databricks.labs.blueprint.cli import App
 from databricks.labs.blueprint.entrypoint import get_logger
 from databricks.labs.blueprint.installation import Installation
-from databricks.labs.remorph.config import SQLGLOT_DIALECTS, MorphConfig
+from databricks.labs.remorph.config import SQLGLOT_DIALECTS, DIALECTS
 from databricks.labs.remorph.helpers.recon_config_utils import ReconConfigPrompts
 from databricks.labs.remorph.helpers.reconcile_utils import ReconcileUtils
+from databricks.labs.remorph.helpers.transpile_utils import TranspileUtils, raise_validation_exception
 from databricks.labs.remorph.lineage import lineage_generator
-from databricks.labs.remorph.transpiler.execute import morph
 from databricks.sdk import WorkspaceClient
 
 remorph = App(__file__)
 logger = get_logger(__file__)
 
-DIALECTS = {name for name, dialect in SQLGLOT_DIALECTS.items()}
-
-
-def raise_validation_exception(msg: str) -> Exception:
-    raise ValueError(msg)
-
 
 @remorph.command
-def transpile(
-    w: WorkspaceClient,
-    source: str,
-    input_sql: str,
-    output_folder: str | None,
-    skip_validation: str,
-    catalog_name: str,
-    schema_name: str,
-    mode: str,
-):
-    """transpiles source dialect to databricks dialect"""
-    installation = Installation.current(w, 'remorph')
-    default_config = installation.load(MorphConfig)
-    mode = mode if mode else "current"  # not checking for default config as it will always be current
+def transpile(w: WorkspaceClient):
+    """[EXPERIMENTAL] reconciles source to databricks datasets"""
+    logger.info(f"user: {w.current_user.me()}")
 
-    if source.lower() not in SQLGLOT_DIALECTS:
-        raise_validation_exception(f"Error: Invalid value for '--source': '{source}' is not one of {DIALECTS}. ")
-    if not os.path.exists(input_sql) or input_sql in {None, ""}:
-        raise_validation_exception(f"Error: Invalid value for '--input_sql': Path '{input_sql}' does not exist.")
-    if not output_folder:
-        output_folder = default_config.output_folder if default_config.output_folder else None
-    if skip_validation.lower() not in {"true", "false"}:
-        raise_validation_exception(
-            f"Error: Invalid value for '--skip_validation': '{skip_validation}' is not one of 'true', 'false'. "
-        )
-    if mode.lower() not in {"current", "experimental"}:
-        raise_validation_exception(
-            f"Error: Invalid value for '--mode': '{mode}' " f"is not one of 'current', 'experimental'. "
-        )
+    installation = Installation.assume_user_home(w, "remorph")
 
-    sdk_config = default_config.sdk_config if default_config.sdk_config else None
-    catalog_name = catalog_name if catalog_name else default_config.catalog_name
-    schema_name = schema_name if schema_name else default_config.schema_name
-
-    config = MorphConfig(
-        source=source.lower(),
-        input_sql=input_sql,
-        output_folder=output_folder,
-        skip_validation=skip_validation.lower() == "true",  # convert to bool
-        catalog_name=catalog_name,
-        schema_name=schema_name,
-        mode=mode,
-        sdk_config=sdk_config,
-    )
-
-    status = morph(w, config)
-
-    print(json.dumps(status))
+    utils = TranspileUtils(w, installation)
+    utils.run()
 
 
 @remorph.command
---
+++ src/databricks/labs/remorph/helpers/transpile_utils.py
@@ -0,0 +1,89 @@
+import json
+import logging
+import os
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.errors import NotFound, PermissionDenied
+
+from databricks.labs.blueprint.installation import Installation, SerdeError
+from databricks.labs.blueprint.tui import Prompts
+from databricks.labs.remorph.config import MorphConfig, SQLGLOT_DIALECTS, DIALECTS
+from databricks.labs.remorph.transpiler.execute import morph
+
+logger = logging.getLogger(__name__)
+
+
+def raise_validation_exception(msg: str) -> Exception:
+    raise ValueError(msg)
+
+
+class TranspileUtils:
+    def __init__(
+        self,
+        w: WorkspaceClient,
+        installation: Installation,
+        prompts: Prompts = Prompts(),
+    ):
+        self._ws = w
+        self._installation = installation
+        self._prompts = prompts
+
+    def _load_config(self) -> MorphConfig | None:
+        transpile_config = None
+        try:
+            logger.info("Loading MorphConfig `config.yml` from Databricks Workspace...")
+            transpile_config = self._installation.load(MorphConfig)
+        except NotFound as err:
+            logger.warning(f"Cannot find previous `transpile` installation: {err}")
+        except (PermissionDenied, SerdeError, ValueError, AttributeError):
+            logger.warning(f"Existing installation at {self._installation.install_folder()} is corrupted. Skipping...")
+
+        reconfigure_msg = "Please use `remorph install` to re-configure ** transpile ** module"
+
+        #   Re-configure `transpile` module:
+        #    * when there is no `config.yml` config on Databricks workspace OR
+        #    * when there is a `config.yml` config and user wants to overwrite it
+        if not transpile_config:
+            logger.error(f"Transpile `config.yml` not found / corrupted on Databricks Workspace.\n{reconfigure_msg}")
+            return None
+        if self._prompts.confirm(
+            f"Would you like to overwrite workspace `Transpile Config` values:\n" f" {transpile_config.__dict__}?"
+        ):
+            logger.info(reconfigure_msg)
+            return None
+
+        return transpile_config
+
+    def run(self):
+
+        transpile_config = self._load_config()
+
+        assert transpile_config, "Error: Cannot load Transpile `config.yml` from Databricks Workspace"
+
+        if transpile_config.source.lower() not in SQLGLOT_DIALECTS:
+            print(f"Error: Invalid value for '--source': '{transpile_config.source}' is not one of {DIALECTS}. ")
+
+        if not os.path.exists(transpile_config.input_sql) or transpile_config.input_sql in {None, ""}:
+            print(f"Error: Invalid value for '--input_sql':" f" Path '{transpile_config.input_sql}' does not exist.")
+
+        if transpile_config.mode not in {"current", "experimental"}:
+            print(
+                f"Error: Invalid value for '--mode': '{transpile_config.mode}' "
+                f"is not one of 'current', "
+                f"'experimental'."
+            )
+
+        config = MorphConfig(
+            source=transpile_config.source.lower(),
+            input_sql=transpile_config.input_sql,
+            output_folder=transpile_config.output_folder,
+            skip_validation=transpile_config.skip_validation,
+            catalog_name=transpile_config.catalog_name,
+            schema_name=transpile_config.schema_name,
+            mode=transpile_config.mode,
+            sdk_config=transpile_config.sdk_config,
+        )
+
+        status = morph(self._ws, config)
+
+        print(json.dumps(status))
