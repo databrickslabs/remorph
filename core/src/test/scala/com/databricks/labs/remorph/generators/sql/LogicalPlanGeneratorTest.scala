64f93736ab0022966c203b164fe1f9dba7d5aa93
Valentin Kasas <valentin.kasas@xebia.com>
2024-07-30 12:01:56 +0200

    Omit the FROM clause when the child of a Project is NoTable

--- core/src/test/scala/com/databricks/labs/remorph/generators/sql/LogicalPlanGeneratorTest.scala
+++ core/src/test/scala/com/databricks/labs/remorph/generators/sql/LogicalPlanGeneratorTest.scala
@@ -12,6 +12,7 @@ class LogicalPlanGeneratorTest extends AnyWordSpec with GeneratorTestCommon[ir.L
     "transpile to SELECT" in {
       ir.Project(namedTable("t1"), Seq(ir.Id("c1"))) generates "SELECT c1 FROM t1"
       ir.Project(namedTable("t1"), Seq(ir.Star(None))) generates "SELECT * FROM t1"
+      ir.Project(ir.NoTable(), Seq(ir.Literal(1))) generates "SELECT 1"
     }
   }
 
