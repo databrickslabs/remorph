04155647a25ea0644e167cbfeff41df43c530f80
Jim Idle <jim.idle@databricks.com>
2024-07-31 10:09:45 -0600

    feat: Add Rule to check for INSERT DEFAULT VALUES
      - Also adds test for the rule

---
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/rules/TrapInsertDefaultsAction.scala
@@ -0,0 +1,16 @@
+package com.databricks.labs.remorph.parsers.tsql.rules
+
+import com.databricks.labs.remorph.parsers.intermediate._
+
+object TrapInsertDefaultsAction extends Rule[LogicalPlan] {
+
+  override def apply(plan: LogicalPlan): LogicalPlan = plan transformDown {
+    case merge @ MergeIntoTable(_, _, _, _, notMatchedActions, _) =>
+      notMatchedActions.collectFirst { case InsertDefaultsAction(_) =>
+        throw new IllegalArgumentException(
+          "The MERGE action 'INSERT DEFAULT VALUES' is not supported in Databricks SQL")
+      }
+      merge
+    case _ => plan
+  }
+}
