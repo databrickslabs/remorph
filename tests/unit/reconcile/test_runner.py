59a1fcefc0c62f17a423e35df015444c631c6148
vijaypavann-db <vijay.pavan@databricks.com>
2024-08-07 10:28:15 +0530

    Updated variables, description as per the PR review comments

--- tests/unit/reconcile/test_runner.py
+++ tests/unit/reconcile/test_runner.py
@@ -382,60 +382,66 @@ def test_aggregates_reconcile_run_with_job_id_in_state(monkeypatch):
             r"Would you like to open the job run URL .*": "yes",
         }
     )
+    state = {
+        "resources": {"jobs": {RECON_JOB_NAME: "1234"}},
+        "version": 1,
+    }
+
+    reconcile = {
+        "data_source": "snowflake",
+        "database_config": {
+            "source_catalog": "abc",
+            "source_schema": "def",
+            "target_catalog": "tgt",
+            "target_schema": "sch",
+        },
+        "report_type": "all",
+        "secret_scope": "remorph",
+        "tables": {
+            "filter_type": "all",
+            "tables_list": ["*"],
+        },
+        "metadata_config": {
+            "catalog": "remorph",
+            "schema": "reconcile",
+            "volume": "reconcile_volume",
+        },
+        "version": 1,
+    }
+
+    sf_recon_config = {
+        "source_catalog": "abc",
+        "source_schema": "def",
+        "tables": [
+            {
+                "aggregates": [
+                    {"type": "MIN", "agg_columns": ["discount"], "group_by_columns": ["p_id"]},
+                    {"type": "AVG", "agg_columns": ["discount"], "group_by_columns": ["p_id"]},
+                    {"type": "MAX", "agg_columns": ["p_id"], "group_by_columns": ["creation_date"]},
+                    {"type": "MAX", "agg_columns": ["p_name"]},
+                    {"type": "SUM", "agg_columns": ["p_id"]},
+                    {"type": "MAX", "agg_columns": ["creation_date"]},
+                    {"type": "MAX", "agg_columns": ["p_id"], "group_by_columns": ["creation_date"]},
+                ],
+                "column_mapping": [
+                    {"source_name": "p_id", "target_name": "product_id"},
+                    {"source_name": "p_name", "target_name": "product_name"},
+                ],
+                "join_columns": ["p_id"],
+                "select_columns": ["p_id", "p_name"],
+                "source_name": "product",
+                "target_name": "product_delta",
+            }
+        ],
+        "target_catalog": "tgt",
+        "target_schema": "sch",
+    }
+
     installation = MockInstallation(
         {
-            "state.json": {
-                "resources": {"jobs": {RECON_JOB_NAME: "1234"}},
-                "version": 1,
-            },
-            "reconcile.yml": {
-                "data_source": "snowflake",
-                "database_config": {
-                    "source_catalog": "abc",
-                    "source_schema": "def",
-                    "target_catalog": "tgt",
-                    "target_schema": "sch",
-                },
-                "report_type": "all",
-                "secret_scope": "remorph",
-                "tables": {
-                    "filter_type": "all",
-                    "tables_list": ["*"],
-                },
-                "metadata_config": {
-                    "catalog": "remorph",
-                    "schema": "reconcile",
-                    "volume": "reconcile_volume",
-                },
-                "version": 1,
-            },
-            "recon_config_snowflake_abc_all.json": {
-                "source_catalog": "abc",
-                "source_schema": "def",
-                "tables": [
-                    {
-                        "aggregates": [
-                            {"type": "MIN", "agg_columns": ["discount"], "group_by_columns": ["p_id"]},
-                            {"type": "AVG", "agg_columns": ["discount"], "group_by_columns": ["p_id"]},
-                            {"type": "MAX", "agg_columns": ["p_id"], "group_by_columns": ["creation_date"]},
-                            {"type": "MAX", "agg_columns": ["p_name"]},
-                            {"type": "SUM", "agg_columns": ["p_id"]},
-                            {"type": "MAX", "agg_columns": ["creation_date"]},
-                            {"type": "MAX", "agg_columns": ["p_id"], "group_by_columns": ["creation_date"]},
-                        ],
-                        "column_mapping": [
-                            {"source_name": "p_id", "target_name": "product_id"},
-                            {"source_name": "p_name", "target_name": "product_name"},
-                        ],
-                        "join_columns": ["p_id"],
-                        "select_columns": ["p_id", "p_name"],
-                        "source_name": "product",
-                        "target_name": "product_delta",
-                    }
-                ],
-                "target_catalog": "tgt",
-                "target_schema": "sch",
-            },
+            "state.json": state,
+            "reconcile.yml": reconcile,
+            "recon_config_snowflake_abc_all.json": sf_recon_config,
         }
     )
     install_state = InstallState.from_installation(installation)
