15334152645429b2dfdf3e342c8502f04fcfe75c
Vijay Pavan Nissankararao <84904851+vijaypavann-db@users.noreply.github.com>
2024-08-12 15:24:21 +0530

    Added logger statements in get_schema function (#756)
    Added logger statements in Metadata (Schema) fetch functions.
    * logging `Schema Query`, `start time` and `end time`.

--- src/databricks/labs/remorph/reconcile/connectors/databricks.py
+++ src/databricks/labs/remorph/reconcile/connectors/databricks.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from datetime import datetime
 
 from pyspark.errors import PySparkException
 from pyspark.sql import DataFrame, SparkSession
@@ -20,10 +21,14 @@ def _get_schema_query(catalog: str, schema: str, table: str):
     if catalog == "hive_metastore":
         return f"describe table {catalog}.{schema}.{table}"
 
-    query = f"""select lower(column_name) as col_name, full_data_type as data_type from 
-                {catalog}.information_schema.columns where lower(table_catalog)='{catalog}' 
-                and lower(table_schema)='{schema}' and lower(table_name) ='{table}' order by 
-                col_name"""
+    query = f"""select 
+                            lower(column_name) as col_name,
+                             full_data_type as data_type
+                       from {catalog}.information_schema.columns
+                       where lower(table_catalog)='{catalog}' 
+                                    and lower(table_schema)='{schema}'
+                                     and lower(table_name) ='{table}'
+                       order by col_name"""
     return re.sub(r'\s+', ' ', query)
 
 
@@ -69,7 +74,10 @@ class DatabricksDataSource(DataSource, SecretsMixin):
         catalog_str = catalog if catalog else "hive_metastore"
         schema_query = _get_schema_query(catalog_str, schema, table)
         try:
-            schema_df = self._spark.sql(schema_query).where("col_name not like '#%'").distinct()
-            return [Schema(field.col_name.lower(), field.data_type.lower()) for field in schema_df.collect()]
+            logger.debug(f"Fetching schema using query: \n`{schema_query}`")
+            logger.info(f"Fetching Schema: Started at: {datetime.now()}")
+            schema_metadata = self._spark.sql(schema_query).where("col_name not like '#%'").distinct().collect()
+            logger.info(f"Schema fetched successfully. Completed at: {datetime.now()}")
+            return [Schema(field.col_name.lower(), field.data_type.lower()) for field in schema_metadata]
         except (RuntimeError, PySparkException) as e:
             return self.log_and_throw_exception(e, "schema", schema_query)
