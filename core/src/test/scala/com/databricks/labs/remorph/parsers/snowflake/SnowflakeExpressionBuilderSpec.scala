databrickslabs/remorph

commit fed469ed5b97a6d49d72760b5076c39d44fb8e33

Add `LET` and `DECLARE` statements parsing in Snowflake PL/SQL procedures (#548)
Added Grammar for 
`DECLARE` and `LET` statements
`RETURN` Statement
Parameterized Expression
Refactor using ScalaSubquery

Added IR visitor for `DECLARE` and `LET` with Variable Assignment and
ResultSet Assignment
Not covered `CURSOR`

core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeExpressionBuilderSpec.scala
@@ -158,6 +158,13 @@ class SnowflakeExpressionBuilderSpec
         _.columnElemStar(),
         Star(Some(ObjectReference(Id("db1"), Id("schema1"), Id("table1")))))
     }
+
+    "translate scalar subquery" in {
+      exampleExpr(
+        query = "(SELECT col1 from table_expr)",
+        rule = _.expr(),
+        expectedAst = ScalarSubquery(Project(namedTable("table_expr"), Seq(Column(None, Id("col1"))))))
+    }
   }
 
   "SnowflakeExpressionBuilder.buildSortOrder" should {

core/src/main/antlr4/com/databricks/labs/remorph/parsers/snowflake/SnowflakeLexer.g4
@@ -195,6 +195,7 @@ CSV                                           : 'CSV';
 CSV_Q                                         : '\'CSV\'';
 CUBE                                          : 'CUBE';
 CURRENT                                       : 'CURRENT';
+CURSOR                                        : 'CURSOR';
 CUSTOM                                        : 'CUSTOM';
 DAILY                                         : 'DAILY';
 DATA                                          : 'DATA';
@@ -205,6 +206,7 @@ DATE_FORMAT                                   : 'DATE_FORMAT';
 DATE_INPUT_FORMAT                             : 'DATE_INPUT_FORMAT';
 DATE_OUTPUT_FORMAT                            : 'DATE_OUTPUT_FORMAT';
 DAYS_TO_EXPIRY                                : 'DAYS_TO_EXPIRY';
+DECLARE                                       : 'DECLARE';
 DEFAULT                                       : 'DEFAULT';
 DEFAULT_DDL_COLLATION_                        : 'DEFAULT_DDL_COLLATION';
 DEFAULT_NAMESPACE                             : 'DEFAULT_NAMESPACE';
@@ -263,6 +265,7 @@ ESCAPE                                        : 'ESCAPE';
 ESCAPE_UNENCLOSED_FIELD                       : 'ESCAPE_UNENCLOSED_FIELD';
 EVENT                                         : 'EVENT';
 EXCEPT                                        : 'EXCEPT';
+EXCEPTION                                     : 'EXCEPTION';
 EXCHANGE                                      : 'EXCHANGE';
 EXECUTE                                       : 'EXEC' 'UTE'?;
 EXECUTION                                     : 'EXECUTION';
@@ -392,6 +395,7 @@ LAST                                           : 'LAST';
 LAST_NAME                                      : 'LAST_NAME';
 LAST_QUERY_ID                                  : 'LAST_QUERY_ID';
 LATERAL                                        : 'LATERAL';
+LET                                            : 'LET';
 LEAD                                           : 'LEAD';
 LEFT                                           : 'LEFT';
 LENGTH                                         : 'LENGTH';
@@ -591,6 +595,7 @@ RESTRICT                                 : 'RESTRICT';
 RESTRICTIONS                             : 'RESTRICTIONS';
 RESULT                                   : 'RESULT';
 RESUME                                   : 'RESUME';
+RETURN                                   : 'RETURN';
 RETURNS                                  : 'RETURNS';
 RETURN_ALL_ERRORS                        : 'RETURN_ALL_ERRORS';
 RETURN_ERRORS                            : 'RETURN_ERRORS';
@@ -845,6 +850,7 @@ NUMERIC          : 'NUMERIC';
 NVARCHAR2        : 'NVARCHAR2';
 NVARCHAR         : 'NVARCHAR';
 REAL_            : 'REAL';
+RESULTSET        : 'RESULTSET';
 SMALLINT         : 'SMALLINT';
 STRING_          : 'STRING';
 TEXT             : 'TEXT';
@@ -907,6 +913,7 @@ GE   : '>=';
 LT   : '<';
 LE   : '<=';
 
+ASSIGN      : ':=';
 PIPE_PIPE   : '||';
 DOT         : '.';
 AT          : '@';

core/src/main/antlr4/com/databricks/labs/remorph/parsers/snowflake/SnowflakeParser.g4
@@ -144,6 +144,20 @@ otherCommand
     | unset
     | call
     | beginTxn
+    | declareCommand
+    | let
+    ;
+
+procStatement
+    : call
+    | declareCommand
+    | let
+    | executeImmediate
+    | returnStatement
+    | unresolvedStatement
+    ;
+
+unresolvedStatement: STRING | DBL_DOLLAR
     ;
 
 beginTxn: BEGIN (WORK | TRANSACTION)? (NAME id)? | START TRANSACTION ( NAME id)?
@@ -160,6 +174,18 @@ copyIntoTable
         | userStage
         | namedStage
     ) R_PAREN files? pattern? fileFormat? copyOptions*
+    ;
+
+// see https://docs.snowflake.com/en/sql-reference/snowflake-scripting/declare
+declareCommand: DECLARE declareStatement+
+    ;
+
+declareStatement
+    : id dataType SEMI                                   # declareSimple
+    | id dataType DEFAULT expr SEMI                      # declareWithDefault
+    | id CURSOR FOR expr SEMI                            # declareCursor
+    | id RESULTSET ((ASSIGN | DEFAULT) expr)? SEMI       # declareResultSet
+    | id EXCEPTION L_PAREN INT COMMA STRING R_PAREN SEMI # declareException
     ;
 
 externalLocation
@@ -197,6 +223,15 @@ formatName: FORMAT_NAME EQ string
     ;
 
 formatType: TYPE EQ typeFileformat formatTypeOptions*
+    ;
+
+let
+    // variable and resultset are covered under the same visitor since expr is common
+    : LET? id (dataType | RESULTSET)? (ASSIGN | DEFAULT) expr SEMI # letVariableAssignment
+    | LET? id CURSOR FOR (selectStatement | id) SEMI               # letCursor
+    ;
+
+returnStatement: RETURN expr SEMI
     ;
 
 stageFileFormat
@@ -1601,16 +1636,19 @@ callerOwner: CALLER | OWNER
 executaAs: EXECUTE AS callerOwner
     ;
 
-procedureDefinition: string | DBL_DOLLAR
+procedureDefinition: DBL_DOLLAR | declareCommand? BEGIN procStatement+ END SEMI
     ;
 
 notNull: NOT NULL_
     ;
 
+table_: TABLE (L_PAREN (colDecl (COMMA colDecl)*)? R_PAREN) | (functionCall)
+    ;
+
 createProcedure
     : CREATE orReplace? PROCEDURE objectName L_PAREN (argDecl (COMMA argDecl)*)? R_PAREN RETURNS (
         dataType
-        | TABLE L_PAREN (colDecl (COMMA colDecl)*)? R_PAREN
+        | table_
     ) notNull? LANGUAGE SQL (CALLED ON NULL_ INPUT | RETURNS NULL_ ON NULL_ INPUT | STRICT)? (
         VOLATILE
         | IMMUTABLE
@@ -3115,7 +3153,6 @@ expr
     | expr COLLATE string                       # exprCollate
     | caseExpression                            # exprCase
     | iffExpr                                   # exprIff
-    | bracketExpression                         # exprBracket
     | sign expr                                 # exprSign
     | expr op = (STAR | DIVIDE | MODULE) expr   # exprPrecedence0
     | expr op = (PLUS | MINUS | PIPE_PIPE) expr # exprPrecedence1
@@ -3133,6 +3170,10 @@ expr
     | DISTINCT expr                             # exprDistinct
     //Should be latest rule as it's nearly a catch all
     | primitiveExpression # exprPrimitive
+    | parameterExpression # exprParameter
+    ;
+
+subQueryExpr: L_PAREN subquery R_PAREN
     ;
 
 withinGroup: WITHIN GROUP L_PAREN orderByClause R_PAREN
@@ -3210,6 +3251,10 @@ primitiveExpression
     | BOTH_Q            # primExprBoth
     | ARRAY_Q           # primExprArray
     | OBJECT_Q          # primExprObject
+    | COLON id          # primVariable
+    ;
+
+parameterExpression: COLON id
     ;
 
 overClause: OVER L_PAREN (PARTITION BY expr (COMMA expr)*)? windowOrderingAndFrame? R_PAREN

core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/commands.scala
@@ -87,3 +87,10 @@ case class WriteStreamOperationStart(
     foreach_batch: Option[StreamingForeachFunction])
     extends LeafNode
     with Command
+
+case class CreateVariable(name: Id, dataType: DataType, defaultExpr: Option[Expression] = None, replace: Boolean)
+    extends LeafNode
+    with Command
+
+case class SetVariable(name: Id, value: Expression, dataType: Option[DataType] = None) extends LeafNode with Command
+

core/src/main/scala/com/databricks/labs/remorph/parsers/intermediate/extensions.scala
@@ -87,18 +87,18 @@ case class Batch(children: Seq[LogicalPlan]) extends LogicalPlan {
 
 case class FunctionParameter(name: String, dataType: DataType, defaultValue: Option[Expression])
 
-sealed trait UDFRuntimeInfo
-case class JavaUDFInfo(runtimeVersion: Option[String], imports: Seq[String], handler: String) extends UDFRuntimeInfo
-case class PythonUDFInfo(runtimeVersion: Option[String], packages: Seq[String], handler: String) extends UDFRuntimeInfo
-case object JavascriptUDFInfo extends UDFRuntimeInfo
-case class ScalaUDFInfo(runtimeVersion: Option[String], imports: Seq[String], handler: String) extends UDFRuntimeInfo
-case class SQLUDFInfo(memoizable: Boolean) extends UDFRuntimeInfo
+sealed trait RuntimeInfo
+case class JavaRuntimeInfo(runtimeVersion: Option[String], imports: Seq[String], handler: String) extends RuntimeInfo
+case class PythonRuntimeInfo(runtimeVersion: Option[String], packages: Seq[String], handler: String) extends RuntimeInfo
+case object JavaScriptRuntimeInfo extends RuntimeInfo
+case class ScalaRuntimeInfo(runtimeVersion: Option[String], imports: Seq[String], handler: String) extends RuntimeInfo
+case class SQLRuntimeInfo(memoizable: Boolean) extends RuntimeInfo
 
 case class CreateInlineUDF(
     name: String,
     returnType: DataType,
     parameters: Seq[FunctionParameter],
-    runtimeInfo: UDFRuntimeInfo,
+    runtimeInfo: RuntimeInfo,
     acceptsNullParameters: Boolean,
     comment: Option[String],
     body: String)

core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeCommandBuilder.scala
@@ -1,0 +1,55 @@
+package com.databricks.labs.remorph.parsers.snowflake
+
+import com.databricks.labs.remorph.parsers.snowflake.SnowflakeParser._
+import com.databricks.labs.remorph.parsers.{IncompleteParser, ParserCommon, intermediate => ir}
+
+class SnowflakeCommandBuilder
+    extends SnowflakeParserBaseVisitor[ir.Command]
+    with ParserCommon[ir.Command]
+    with IncompleteParser[ir.Command] {
+
+  private val expressionBuilder = new SnowflakeExpressionBuilder
+
+  protected override def wrapUnresolvedInput(unparsedInput: String): ir.UnresolvedCommand =
+    ir.UnresolvedCommand(unparsedInput)
+
+  // TODO: Implement Cursor and Exception for Declare Statements.
+  // TODO: Implement Cursor for Let Statements.
+
+  override def visitDeclareWithDefault(ctx: DeclareWithDefaultContext): ir.Command = {
+    val variableName = ctx.id().accept(expressionBuilder).asInstanceOf[ir.Id]
+    val dataType = DataTypeBuilder.buildDataType(ctx.dataType())
+    val variableValue = ctx.expr().accept(expressionBuilder)
+    ir.CreateVariable(variableName, dataType, Some(variableValue), replace = false)
+  }
+
+  override def visitDeclareSimple(ctx: DeclareSimpleContext): ir.Command = {
+    val variableName = ctx.id().accept(expressionBuilder).asInstanceOf[ir.Id]
+    val dataType = DataTypeBuilder.buildDataType(ctx.dataType())
+    ir.CreateVariable(variableName, dataType, None, replace = false)
+  }
+
+  override def visitDeclareResultSet(ctx: DeclareResultSetContext): ir.Command = {
+    val variableName = ctx.id().accept(expressionBuilder).asInstanceOf[ir.Id]
+    val dataType = ir.StructType()
+    val variableValue = ctx.expr() match {
+      case null => None
+      case stmt => Some(stmt.accept(expressionBuilder))
+    }
+
+    ir.CreateVariable(variableName, dataType, variableValue, replace = false)
+  }
+
+  override def visitLetVariableAssignment(ctx: LetVariableAssignmentContext): ir.Command = {
+    val variableName = ctx.id().accept(expressionBuilder).asInstanceOf[ir.Id]
+    val variableValue = ctx.expr().accept(expressionBuilder)
+
+    val variableDataType = variableValue match {
+      case _: ir.ScalarSubquery => Some(ir.StructType())
+      case _ => Option(ctx.dataType()).flatMap(dt => Some(DataTypeBuilder.buildDataType(dt)))
+    }
+
+    ir.SetVariable(variableName, variableValue, variableDataType)
+  }
+
+}

core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeDDLBuilder.scala
@@ -1,6 +1,5 @@
 package com.databricks.labs.remorph.parsers.snowflake
 
-import com.databricks.labs.remorph.parsers.intermediate.AddColumn
 import com.databricks.labs.remorph.parsers.snowflake.SnowflakeParser.{StringContext => StrContext, _}
 import com.databricks.labs.remorph.parsers.{IncompleteParser, ParserCommon, intermediate => ir}
 
@@ -22,9 +21,9 @@ class SnowflakeDDLBuilder
     val runtimeInfo = ctx match {
       case c if c.JAVA() != null => buildJavaUDF(c)
       case c if c.PYTHON() != null => buildPythonUDF(c)
-      case c if c.JAVASCRIPT() != null => ir.JavascriptUDFInfo
+      case c if c.JAVASCRIPT() != null => ir.JavaScriptRuntimeInfo
       case c if c.SCALA() != null => buildScalaUDF(c)
-      case c if c.SQL() != null || c.LANGUAGE() == null => ir.SQLUDFInfo(c.MEMOIZABLE() != null)
+      case c if c.SQL() != null || c.LANGUAGE() == null => ir.SQLRuntimeInfo(c.MEMOIZABLE() != null)
     }
     val name = ctx.objectName().getText
     val returnType = DataTypeBuilder.buildDataType(ctx.dataType())
@@ -48,11 +47,11 @@ class SnowflakeDDLBuilder
     case c if c.string() != null => extractString(c.string())
   }).trim
 
-  private def buildJavaUDF(ctx: CreateFunctionContext): ir.UDFRuntimeInfo = buildJVMUDF(ctx)(ir.JavaUDFInfo.apply)
-  private def buildScalaUDF(ctx: CreateFunctionContext): ir.UDFRuntimeInfo = buildJVMUDF(ctx)(ir.ScalaUDFInfo.apply)
+  private def buildJavaUDF(ctx: CreateFunctionContext): ir.RuntimeInfo = buildJVMUDF(ctx)(ir.JavaRuntimeInfo.apply)
+  private def buildScalaUDF(ctx: CreateFunctionContext): ir.RuntimeInfo = buildJVMUDF(ctx)(ir.ScalaRuntimeInfo.apply)
 
   private def buildJVMUDF(ctx: CreateFunctionContext)(
-      ctr: (Option[String], Seq[String], String) => ir.UDFRuntimeInfo): ir.UDFRuntimeInfo = {
+      ctr: (Option[String], Seq[String], String) => ir.RuntimeInfo): ir.RuntimeInfo = {
     val imports =
       ctx
         .stringList()
@@ -69,7 +68,7 @@ class SnowflakeDDLBuilder
   private def extractHandler(ctx: CreateFunctionContext): String =
     Option(ctx.HANDLER()).flatMap(h => ctx.string().asScala.find(occursBefore(h, _))).map(extractString).get
 
-  private def buildPythonUDF(ctx: CreateFunctionContext): ir.PythonUDFInfo = {
+  private def buildPythonUDF(ctx: CreateFunctionContext): ir.PythonRuntimeInfo = {
     val packages =
       ctx
         .stringList()
@@ -77,7 +76,7 @@ class SnowflakeDDLBuilder
         .find(occursBefore(ctx.PACKAGES(0), _))
         .map(_.string().asScala.map(extractString))
         .getOrElse(Seq())
-    ir.PythonUDFInfo(extractRuntimeVersion(ctx), packages, extractHandler(ctx))
+    ir.PythonRuntimeInfo(extractRuntimeVersion(ctx), packages, extractHandler(ctx))
   }
 
   override def visitCreateTable(ctx: CreateTableContext): ir.Catalog = {
@@ -168,7 +167,7 @@ class SnowflakeDDLBuilder
 
   private[snowflake] def buildColumnActions(ctx: TableColumnActionContext): Seq[ir.TableAlteration] = ctx match {
     case c if c.ADD() != null =>
-      c.fullColDecl().asScala.map(buildColumnDeclaration).map(AddColumn.apply)
+      c.fullColDecl().asScala.map(buildColumnDeclaration).map(ir.AddColumn.apply)
     case c if !c.alterColumnClause().isEmpty =>
       c.alterColumnClause().asScala.map(buildColumnAlterations)
     case c if c.DROP() != null =>
@@ -219,4 +218,5 @@ class SnowflakeDDLBuilder
       affectedColumns.map(col => ir.DropConstraint(Some(col), constraint))
     }
   }
+
 }

core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeExpressionBuilder.scala
@@ -434,4 +434,8 @@ class SnowflakeExpressionBuilder()
   override def visitSetColumnValue(ctx: SetColumnValueContext): ir.Expression = {
     ir.Assign(ctx.id().accept(this), ctx.expr().accept(this))
   }
+
+  override def visitExprSubquery(ctx: ExprSubqueryContext): ir.Expression = {
+    ir.ScalarSubquery(ctx.subquery().accept(new SnowflakeRelationBuilder))
+  }
 }

commit a8fe9afeaeecaf3ee899b2cfc0c6a17343da0de6

feat: Create missing tests for parenthesised expressions broken by https://github.com/databrickslabs/remorph/pull/548
   - The PR incorrectly removed support for parentheised expressions. It was not caught
     because we had no coverage tests for expression precedence

core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeExpressionBuilderSpec.scala
@@ -2,6 +2,7 @@ package com.databricks.labs.remorph.parsers.snowflake
 
 import com.databricks.labs.remorph.parsers.intermediate._
 import com.databricks.labs.remorph.parsers.snowflake.SnowflakeParser.{ComparisonOperatorContext, LiteralContext}
+import com.databricks.labs.remorph.parsers.{intermediate => ir}
 import org.mockito.Mockito._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -48,6 +49,183 @@ class SnowflakeExpressionBuilderSpec
         "\"My Table\".x",
         _.columnName(),
         Column(Some(ObjectReference(Id("My Table", caseSensitive = true))), Id("x")))
+    }
+    
+    "translate simple numeric binary expressions" in {
+      exampleExpr("1 + 2", _.expr(), ir.Add(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 +2", _.expr(), ir.Add(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 - 2", _.expr(), ir.Subtract(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 -2", _.expr(), ir.Subtract(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 * 2", _.expr(), ir.Multiply(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 / 2", _.expr(), ir.Divide(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr("1 % 2", _.expr(), ir.Mod(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))))
+      exampleExpr(
+        "'A' || 'B'",
+        _.expr(),
+        ir.Concat(Seq(ir.Literal(string = Some("A")), ir.Literal(string = Some("B")))))
+    }
+
+    "translate complex binary expressions" in {
+      exampleExpr(
+        "a + b * 2",
+        _.expr(),
+        ir.Add(Id("a"), ir.Multiply(Id("b"), ir.Literal(short = Some(2)))))
+      exampleExpr(
+        "(a + b) * 2",
+        _.expr(),
+        ir.Multiply(ir.Add(Id("a"), Id("b")), ir.Literal(short = Some(2))))
+      exampleExpr(
+        "a & b | c",
+        _.expr(),
+        ir.BitwiseOr(ir.BitwiseAnd(Id("a"), Id("b")), Id("c")))
+      exampleExpr(
+        "(a & b) | c",
+        _.expr(),
+        ir.BitwiseOr(ir.BitwiseAnd(Id("a"), Id("b")), Id("c")))
+      exampleExpr(
+        "a + b * 2",
+        _.expr(),
+        ir.Add(Id("a"), ir.Multiply(Id("b"), ir.Literal(short = Some(2)))))
+      exampleExpr(
+        "(a + b) * 2",
+        _.expr(),
+        ir.Multiply(ir.Add(Id("a"), Id("b")), ir.Literal(short = Some(2))))
+      exampleExpr(
+        "a & b | c",
+        _.expr(),
+        ir.BitwiseOr(ir.BitwiseAnd(Id("a"), Id("b")), Id("c")))
+      exampleExpr(
+        "(a & b) | c",
+        _.expr(),
+        ir.BitwiseOr(ir.BitwiseAnd(Id("a"), Id("b")), Id("c")))
+      exampleExpr(
+        "a % 3 + b * 2 - c / 5",
+        _.expr(),
+        ir.Subtract(
+          ir.Add(
+            ir.Mod(Id("a"), ir.Literal(short = Some(3))),
+            ir.Multiply(Id("b"), ir.Literal(short = Some(2)))),
+          ir.Divide(Id("c"), ir.Literal(short = Some(5)))))
+      exampleExpr(
+        query = "a || b || c",
+        _.expr(),
+        ir.Concat(Seq(ir.Concat(Seq(Id("a"), Id("b"))), Id("c"))))
+    }
+
+    "correctly apply operator precedence and associativity" in {
+      exampleExpr(
+        "1 + -++-2",
+        _.expr(),
+        ir.Add(ir.Literal(short = Some(1)), ir.UMinus(ir.UPlus(ir.UPlus(ir.UMinus(ir.Literal(short = Some(2))))))))
+      exampleExpr(
+        "1 + ~ 2 * 3",
+        _.expr(),
+        ir.Add(
+          ir.Literal(short = Some(1)),
+          ir.Multiply(ir.BitwiseNot(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))))
+      exampleExpr(
+        "1 + -2 * 3",
+        _.expr(),
+        ir.Add(
+          ir.Literal(short = Some(1)),
+          ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))))
+      exampleExpr(
+        "1 + -2 * 3 + 7 & 66",
+        _.expr(),
+        ir.BitwiseAnd(
+          ir.Add(
+            ir.Add(
+              ir.Literal(short = Some(1)),
+              ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))),
+            ir.Literal(short = Some(7))),
+          ir.Literal(short = Some(66))))
+      exampleExpr(
+        "1 + -2 * 3 + 7 ^ 66",
+        _.expr(),
+        ir.BitwiseXor(
+          ir.Add(
+            ir.Add(
+              ir.Literal(short = Some(1)),
+              ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))),
+            ir.Literal(short = Some(7))),
+          ir.Literal(short = Some(66))))
+      exampleExpr(
+        "1 + -2 * 3 + 7 | 66",
+        _.expr(),
+        ir.BitwiseOr(
+          ir.Add(
+            ir.Add(
+              ir.Literal(short = Some(1)),
+              ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))),
+            ir.Literal(short = Some(7))),
+          ir.Literal(short = Some(66))))
+      exampleExpr(
+        "1 + -2 * 3 + 7 + ~66",
+        _.expr(),
+        ir.Add(
+          ir.Add(
+            ir.Add(
+              ir.Literal(short = Some(1)),
+              ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))),
+            ir.Literal(short = Some(7))),
+          ir.BitwiseNot(ir.Literal(short = Some(66)))))
+      exampleExpr(
+        "1 + -2 * 3 + 7 | 1980 || 'leeds1' || 'leeds2' || 'leeds3'",
+        _.expr(),
+        ir.Concat(
+          Seq(
+            ir.Concat(Seq(
+              ir.Concat(Seq(
+                ir.BitwiseOr(
+                  ir.Add(
+                    ir.Add(
+                      ir.Literal(short = Some(1)),
+                      ir.Multiply(ir.UMinus(ir.Literal(short = Some(2))), ir.Literal(short = Some(3)))),
+                    ir.Literal(short = Some(7))),
+                  ir.Literal(short = Some(1980))),
+                ir.Literal(string = Some("leeds1")))),
+              ir.Literal(string = Some("leeds2")))),
+            ir.Literal(string = Some("leeds3")))))
+    }
+    "correctly respect explicit precedence with parentheses" in {
+      exampleExpr(
+        "(1 + 2) * 3",
+        _.expr(),
+        ir.Multiply(ir.Add(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))), ir.Literal(short = Some(3))))
+      exampleExpr(
+        "1 + (2 * 3)",
+        _.expr(),
+        ir.Add(ir.Literal(short = Some(1)), ir.Multiply(ir.Literal(short = Some(2)), ir.Literal(short = Some(3)))))
+      exampleExpr(
+        "(1 + 2) * (3 + 4)",
+        _.expr(),
+        ir.Multiply(
+          ir.Add(ir.Literal(short = Some(1)), ir.Literal(short = Some(2))),
+          ir.Add(ir.Literal(short = Some(3)), ir.Literal(short = Some(4)))))
+      exampleExpr(
+        "1 + (2 * 3) + 4",
+        _.expr(),
+        ir.Add(
+          ir.Add(ir.Literal(short = Some(1)), ir.Multiply(ir.Literal(short = Some(2)), ir.Literal(short = Some(3)))),
+          ir.Literal(short = Some(4))))
+      exampleExpr(
+        "1 + (2 * 3 + 4)",
+        _.expr(),
+        ir.Add(
+          ir.Literal(short = Some(1)),
+          ir.Add(ir.Multiply(ir.Literal(short = Some(2)), ir.Literal(short = Some(3))), ir.Literal(short = Some(4)))))
+      exampleExpr(
+        "1 + (2 * (3 + 4))",
+        _.expr(),
+        ir.Add(
+          ir.Literal(short = Some(1)),
+          ir.Multiply(ir.Literal(short = Some(2)), ir.Add(ir.Literal(short = Some(3)), ir.Literal(short = Some(4))))))
+      exampleExpr(
+        "(1 + (2 * (3 + 4)))",
+        _.expr(),
+        ir.Add(
+          ir.Literal(short = Some(1)),
+          ir.Multiply(ir.Literal(short = Some(2)), ir.Add(ir.Literal(short = Some(3)), ir.Literal(short = Some(4))))))
     }
 
     "translate functions with special syntax" in {