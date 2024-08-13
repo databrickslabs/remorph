aef76ca6add5cd0cdc7ea3d8c6efc25a01cc70ca
Serge Smertin <serge.smertin@databricks.com>
2024-08-13 18:44:40 +0200

    Use real backends

---
+++ coverage/src/main/scala/com/databricks/labs/remorph/coverage/runners/TSqlRunner.scala
@@ -0,0 +1,49 @@
+package com.databricks.labs.remorph.coverage.runners
+
+import com.github.tototoshi.csv.CSVWriter
+
+import java.io.StringWriter
+import java.sql.{Connection, DriverManager, ResultSet}
+
+class TSqlRunner(env: EnvGetter) {
+  // scalastyle:off
+  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+  // scalastyle:on
+
+  private val url = env.get("TEST_TSQL_JDBC")
+  private val user = env.get("TEST_TSQL_USER")
+  private val pass = env.get("TEST_TSQL_PASS")
+  private val connection = DriverManager.getConnection(url, user, pass)
+
+  def queryToCSV(query: String): String = {
+    val statement = connection.createStatement()
+    val resultSet = statement.executeQuery(query)
+    val csv = resultSetToCSV(resultSet)
+    resultSet.close()
+    statement.close()
+    csv
+  }
+
+  private def resultSetToCSV(resultSet: ResultSet): String = {
+    val writer = new StringWriter()
+    val csvWriter = new CSVWriter(writer)
+
+    // write the header
+    val metaData = resultSet.getMetaData
+    val columnCount = metaData.getColumnCount
+    val header = (1 to columnCount).map(metaData.getColumnName)
+    csvWriter.writeRow(header)
+
+    // write the data
+    while (resultSet.next()) {
+      val row = (1 to columnCount).map(resultSet.getString)
+      csvWriter.writeRow(row)
+    }
+
+    writer.toString()
+  }
+
+  def close() {
+    connection.close()
+  }
+}
