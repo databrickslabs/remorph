de7ba9635e8a60094eb6d09970027656e862cd6f
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-08-19 18:20:13 +0530

    PR review comments implemented

--- core/src/test/scala/com/databricks/labs/remorph/transpilers/SnowflakeToDatabricksTranspilerTest.scala
+++ core/src/test/scala/com/databricks/labs/remorph/transpilers/SnowflakeToDatabricksTranspilerTest.scala
@@ -10,14 +10,14 @@ class SnowflakeToDatabricksTranspilerTest extends AnyWordSpec with TranspilerTes
 
     "transpile queries" in {
 
-      /* Testing BANG ! command */
+      /* Test BANG ! command */
       "!set error_flag = true;" transpilesTo "--!set error_flag = true;"
       "!define tablename=CENUSTRACKONE;" transpilesTo "--!define tablename=CENUSTRACKONE;"
       "!print Include This Text" transpilesTo "--!print Include This Text;"
       "!abort 77589bd1" transpilesTo "--!abort 77589bd1;"
       "SELECT * FROM employees WHERE department_id != 10;" transpilesTo
         "SELECT * FROM employees WHERE department_id != 10;"
-      /* Testing DECLARE command END */
+      /* Test BANG command END */
 
       "SELECT * FROM t1;" transpilesTo "SELECT * FROM t1;"
 
