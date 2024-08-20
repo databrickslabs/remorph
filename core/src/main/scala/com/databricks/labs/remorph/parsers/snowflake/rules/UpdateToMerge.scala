28fa06defa3a8041ab71bbc92d19060a7952e54e
Serge Smertin <serge.smertin@databricks.com>
2024-08-20 14:28:06 +0200

    ...

--- core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/rules/UpdateToMerge.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/rules/UpdateToMerge.scala
@@ -1,11 +1,37 @@
 package com.databricks.labs.remorph.parsers.snowflake.rules
 
-import com.databricks.labs.remorph.parsers.intermediate.{Assign, LogicalPlan, MergeIntoTable, Rule, UpdateAction, UpdateTable}
+import com.databricks.labs.remorph.parsers.intermediate.{Assign, Expression, Join, LogicalPlan, MergeAction, MergeIntoTable, Noop, NoopNode, Rule, UpdateAction, UpdateTable}
+
 
 class UpdateToMerge extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan transform {
-    case UpdateTable(target, Some(source), assigns, Some(where), _, _) =>
-      val set = assigns.filter(_.isInstanceOf[Assign]).map(_.asInstanceOf[Assign])
-      MergeIntoTable(target, source, where, matchedActions = Seq(UpdateAction(None, set)))
+    case update @ UpdateTable(_, None, _, _, _, _) => update
+    case update: UpdateTable =>
+      MergeIntoTable(
+        update.target,
+        source(update),
+        condition(update),
+        matchedActions = matchedActions(update)
+      )
+  }
+
+  private def matchedActions(update: UpdateTable): Seq[MergeAction] = {
+    val set = update.set.collect{ case a: Assign => a }
+    Seq(UpdateAction(None, set))
+  }
+
+  private def source(update: UpdateTable): LogicalPlan = update.source match {
+    case Some(plan) => plan match {
+      case Join(_, source, _, _, _, _) =>
+        // TODO: figure out why there's a join in the update plan
+        source
+      case _ => plan
+    }
+    case None => NoopNode
+  }
+
+  private def condition(update: UpdateTable): Expression = update.where match {
+    case Some(condition) => condition
+    case None => Noop
   }
 }
