b2803f32435205e8a3b4620c676bbb8c46e9a241
sundar.shankar <sundar.shankar@databricks.com>
2024-08-20 12:25:21 +0530

    Init Commit

--- core/src/main/scala/com/databricks/labs/remorph/transpilers/Transpiler.scala
+++ core/src/main/scala/com/databricks/labs/remorph/transpilers/Transpiler.scala
@@ -3,9 +3,23 @@ package com.databricks.labs.remorph.transpilers
 import com.databricks.labs.remorph.parsers.{intermediate => ir}
 import com.github.vertical_blank.sqlformatter.SqlFormatter
 import com.github.vertical_blank.sqlformatter.core.FormatConfig
+import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
 
 trait Transpiler {
-  def transpile(input: String): String
+  def transpile(input: String, skipValiation: Boolean = false): String
+}
+
+trait Validator {
+  def validate(transpiledCode: String): String = {
+    try {
+      CatalystSqlParser.parsePlan(transpiledCode)
+      transpiledCode
+    } catch {
+      // TODO Enhance custom messaging to support complex errors we encounter
+      case e: Exception =>
+        e.getMessage ++ transpiledCode
+    }
+  }
 }
 
 trait Formatter {
@@ -19,7 +33,7 @@ trait Formatter {
   def format(input: String): String = SqlFormatter.format(input, sqlFormat)
 }
 
-abstract class BaseTranspiler extends Transpiler with Formatter {
+abstract class BaseTranspiler extends Transpiler with Formatter with Validator{
 
   protected def parse(input: String): ir.LogicalPlan
 
@@ -27,10 +41,11 @@ abstract class BaseTranspiler extends Transpiler with Formatter {
 
   protected def generate(optimizedLogicalPlan: ir.LogicalPlan): String
 
-  override def transpile(input: String): String = {
+  override def transpile(input: String, skipValidation: Boolean): String = {
     val parsed = parse(input)
     val optimized = optimize(parsed)
     val generated = generate(optimized)
+    if (!skipValidation) return format(validate(generated))
     format(generated)
   }
 }
