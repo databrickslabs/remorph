6f9c60aded6dac067f4cdf2453430d694aebc9fc
sundar.shankar <sundar.shankar@databricks.com>
2024-08-20 11:53:53 +0530

    Review Comments-part2

--- core/src/main/scala/com/databricks/labs/remorph/RootTableIdentifier.scala
+++ core/src/main/scala/com/databricks/labs/remorph/RootTableIdentifier.scala
@@ -5,44 +5,26 @@ import com.databricks.labs.remorph.transpilers.Transpiler
 
 class RootTableIdentifier(transpiler: Transpiler) {
 
-  def processQuery(sql: String, dag: Lineage): Lineage = {
+  def processQuery(sql: String, graph: Lineage): Lineage = {
     val parsedSql = transpiler.parse(sql)
-    findTables(dag, parsedSql)
+    findTables(graph, parsedSql)
   }
 
-  private def getTablName(p: ir.LogicalPlan): Set[String] = {
-    p.find(_.isInstanceOf[ir.NamedTable]).map(_.asInstanceOf[ir.NamedTable].unparsed_identifier).toSet
+  private def getTableNode(p: ir.LogicalPlan): Set[Node] = {
+    p.collect {
+      case nt: ir.NamedTable => Node(nt.unparsed_identifier)
+    }.toSet
   }
 
-  private def findTables(dag: Lineage, plan: ir.LogicalPlan): Lineage = {
-    var child = ""
-    var parent = Set.empty[String]
+  private def findTables(graph: Lineage, plan: ir.LogicalPlan): Lineage = {
 
-    def collectTables(node: ir.LogicalPlan): Unit = {
-      node match {
-        case n: ir.InsertIntoTable =>
-          child = n.target
-            .find(_.isInstanceOf[ir.NamedTable])
-            .map(_.asInstanceOf[ir.NamedTable].unparsed_identifier)
-            .getOrElse("unknown") // Unknown is when query is a Select Query. May be file name becomes the table name.
-
-        case p: ir.Project => parent = parent ++ getTablName(p)
-
-        case j: ir.Join => parent = parent ++ getTablName(j.left) ++ getTablName(j.right)
-        case _ => // Do nothing for other nodes
-      }
-      node.children.foreach(collectTables)
+    plan.collect {
+      case nt: ir.NamedTable => nt.unparsed_identifier
+      case _ => getTableNode(_)
     }
 
-    collectTables(plan)
-    val childNode = Node(child)
-    dag.addNode(childNode)
-    parent.foreach { parentTable =>
-      val parentNode = Node(parentTable)
-      dag.addNode(parentNode)
-      dag.addEdge(parentNode, childNode)
-    }
-    dag
+    graph
+
   }
 
 }
