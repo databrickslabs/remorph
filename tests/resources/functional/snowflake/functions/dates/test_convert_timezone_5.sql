416d337ea99b2b9edced68b35100635cf1fda2a7
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2024-08-20 21:02:51 +0530

    Gen/snow date time functions pyfix (#822)
    `remorph-py` fixes
    
    ---------
    
    Co-authored-by: Valentin Kasas <valentin.kasas@xebia.com>

--- tests/resources/functional/snowflake/functions/dates/test_convert_timezone_5.sql
+++ tests/resources/functional/snowflake/functions/dates/test_convert_timezone_5.sql
@@ -4,4 +4,4 @@ SELECT
                 CONVERT_TIMEZONE('Europe/Warsaw', 'UTC', '2019-01-01 00:00:00 +03:00'::timestamp_ntz);
 
 -- databricks sql:
-SELECT CONVERT_TIMEZONE('Europe/Warsaw', 'UTC', '2019-01-01 00:00:00 +03:00');
+SELECT CONVERT_TIMEZONE('Europe/Warsaw', 'UTC', CAST('2019-01-01 00:00:00 +03:00' AS TIMESTAMP_NTZ));
