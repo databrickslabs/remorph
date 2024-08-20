ed721034f17a84dd87d7fb45fcdaee15fcbad706
Serge Smertin <serge.smertin@databricks.com>
2024-08-20 22:26:16 +0200

    [chore] increase coverage by 8%
    A lot of plans/expressions have `null` by mistake, which fails earlier than we want at the moment. We'll work on improving the coverage in subsequent PRs, but with better errors this change gives us.

--- core/src/main/scala/com/databricks/labs/remorph/generators/sql/ExpressionGenerator.scala
+++ core/src/main/scala/com/databricks/labs/remorph/generators/sql/ExpressionGenerator.scala
@@ -1,7 +1,6 @@
 package com.databricks.labs.remorph.generators.sql
 
 import com.databricks.labs.remorph.generators.{Generator, GeneratorContext}
-import com.databricks.labs.remorph.parsers.intermediate.WithinGroup
 import com.databricks.labs.remorph.parsers.{intermediate => ir}
 import com.databricks.labs.remorph.transpilers.TranspileException
 
@@ -50,7 +49,7 @@ class ExpressionGenerator(val callMapper: ir.CallMapper = new ir.CallMapper())
       case w: ir.Window => window(ctx, w)
       case o: ir.SortOrder => sortOrder(ctx, o)
       case ir.Exists(subquery) => s"EXISTS (${ctx.logical.generate(ctx, subquery)})"
-      case w: ir.WithinGroup => withinGroup(ctx, w)
+      case null => "" // don't fail transpilation if the expression is null
       case x => throw TranspileException(s"Unsupported expression: $x")
     }
   }
@@ -384,10 +383,6 @@ class ExpressionGenerator(val callMapper: ir.CallMapper = new ir.CallMapper())
       case ir.SortNullsUnspecified => Seq()
     }
     (Seq(orderBy) ++ direction ++ nulls).mkString(" ")
-  }
-
-  private def withinGroup(ctx: GeneratorContext, w: ir.WithinGroup): String = {
-    //
   }
 
   private def orNull(option: Option[String]): String = option.getOrElse("NULL")
