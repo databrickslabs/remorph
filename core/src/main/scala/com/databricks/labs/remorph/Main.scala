6a9db2b776cce83a07823f428717b2a14324de33
Serge Smertin <serge.smertin@databricks.com>
2024-08-22 22:02:34 +0200

    [internal] added JVM command proxy in development mode
    This PR adds basic functionality to proxy commands into JVM subprocess through Python. Later on it'll be implemented in the Databricks CLI itself. In order to use this, you have to install `remorph` on any workspace via `databricks labs install .`, so that `.databricks-login.json` file gets created with the following contents:
    
    ```
    {
      "workspace_profile": "labs-azure-tool",
      "cluster_id": "0708-200540-wcwi4i9e"
    }
    ```
    
    then run `make dev-cli` to collect classpath information. And then invoke commands, like `databricks labs remorph debug-script --name file`. Add `--debug` flag to recompile project each run.

--- core/src/main/scala/com/databricks/labs/remorph/Main.scala
+++ core/src/main/scala/com/databricks/labs/remorph/Main.scala
@@ -1,5 +1,22 @@
 package com.databricks.labs.remorph
 
+case class Payload(command: String, flags: Map[String, String])
+
 object Main extends App {
-  // noop
+  // scalastyle:off println
+  route match {
+    case Payload("debug-script", args) =>
+      println("Debugging script...")
+      println(args)
+    case Payload(command, _) =>
+      println(s"Unknown command: $command")
+  }
+
+  // parse json from the last CLI argument
+  private def route: Payload = {
+    val payload = ujson.read(args.last).obj
+    val command = payload("command").str
+    val flags = payload("flags").obj.mapValues(_.str).toMap
+    Payload(command, flags)
+  }
 }
