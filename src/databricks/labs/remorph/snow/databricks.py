d1e268da7a67b6e33712e5d5183b52132f67e7fd
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-08-27 13:04:03 +0530

    extra ";" generation has been taken care for Bang command

--- src/databricks/labs/remorph/snow/databricks.py
+++ src/databricks/labs/remorph/snow/databricks.py
@@ -240,8 +240,7 @@ def _array_slice(self: org_databricks.Databricks.Generator, expression: local_ex
 
 def _to_command(self, expression: exp.Command):
     this_sql = self.sql(expression, 'this')
-    prefix = '-- snowsql command:' if this_sql == '!' else '-- '
-    return f"{prefix}{this_sql}{self.sql(expression, 'expression')}"
+    return f"{'--'}{this_sql}{self.sql(expression, 'expression')}"
 
 
 def _parse_json(self, expr: exp.ParseJSON):
