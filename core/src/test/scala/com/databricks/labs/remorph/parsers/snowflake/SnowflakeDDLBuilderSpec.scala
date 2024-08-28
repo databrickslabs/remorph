64f34ef53956c192d62c7a969125654ce49cae73
sundar.shankar <sundar.shankar@databricks.com>
2024-08-28 10:55:37 +0530

    Fix review comments

--- core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeDDLBuilderSpec.scala
+++ core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeDDLBuilderSpec.scala
@@ -147,10 +147,9 @@ class SnowflakeDDLBuilderSpec
                   |  AS 'a * b';""".stripMargin,
         expectedAst = CreateInlineUDF(
           name = "multiply1",
-          returnType = DecimalType(None, None),
-          parameters = Seq(
-            FunctionParameter("a", DecimalType(None, None), None),
-            FunctionParameter("b", DecimalType(None, None), None)),
+          returnType = DecimalType(38, 0),
+          parameters =
+            Seq(FunctionParameter("a", DecimalType(38, 0), None), FunctionParameter("b", DecimalType(38, 0), None)),
           runtimeInfo = SQLRuntimeInfo(memoizable = false),
           acceptsNullParameters = false,
           comment = Some("multiply two numbers"),
