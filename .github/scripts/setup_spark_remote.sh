b2c8c5800dba6b84282b686e6ccf127c346d0b16
vijaypavann-db <vijay.pavan@databricks.com>
2024-09-02 13:20:21 +0530

    Updated Spark script to check spark gzip file already exists

--- .github/scripts/setup_spark_remote.sh
+++ .github/scripts/setup_spark_remote.sh
@@ -2,31 +2,36 @@
 
 set -xv
 
-mkdir -p $HOME/spark
-cd $HOME/spark
+mkdir -p "$HOME"/spark
+cd "$HOME"/spark || exit
 
 version=$(wget -O - https://dlcdn.apache.org/spark/ | grep 'href="spark' | grep -v 'preview' | sed 's:</a>:\n:g' | sed -n 's/.*>//p' | tr -d spark- | tr -d / | sort -r --version-sort | head -1)
 spark=spark-${version}-bin-hadoop3
-spark_connect=spark-connect_2.12
+spark_connect="spark-connect_2.12"
 
-mkdir ${spark}
-cd {spark}
+mkdir "${spark}"
+
 
 SERVER_SCRIPT=$HOME/spark/${spark}/sbin/start-connect-server.sh
 
 ## check the spark version already exist ,if not download the respective version
-if [ -f ${SERVER_SCRIPT} ];then
+if [ -f "${SERVER_SCRIPT}" ];then
   echo "Spark Version already exists"
 else
-  wget https://dlcdn.apache.org/spark/spark-${version}/${spark}.tgz
-  tar -xvf ${spark}.tgz
+  if [ -f "${spark}.tgz" ];then
+    echo "${spark}.tgz already exists"
+  else
+    wget "https://dlcdn.apache.org/spark/spark-${version}/${spark}.tgz"
+  fi
+  tar -xvf "${spark}.tgz"
 fi
 
+cd "${spark}" || exit
 ## check spark remote is running,if not start the spark remote
-${SERVER_SCRIPT} --packages org.apache.spark:${spark_connect}:${version} > $HOME/spark/log.out
+${SERVER_SCRIPT} --packages org.apache.spark:${spark_connect}:"${version}" > "$HOME"/spark/log.out
 
 if [ $? -ne 0 ]; then
-    count=$(tail ${HOME}/spark/log.out | grep "SparkConnectServer running as process" | wc -l)
+    count=$(tail "${HOME}"/spark/log.out | grep -c "SparkConnectServer running as process")
     if [ "${count}" == "0" ]; then
             echo "Failed to start the server"
         exit 1
