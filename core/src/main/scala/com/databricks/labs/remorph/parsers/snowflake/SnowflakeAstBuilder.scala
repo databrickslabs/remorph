defa9b00713756aa43f8b8aa09dbb12db8ab2f0a
sundar.shankar <sundar.shankar@databricks.com>
2024-09-03 13:52:00 +0530

    added unresolvedCommand for ExecuteTask

--- core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeAstBuilder.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeAstBuilder.scala
@@ -14,6 +14,7 @@ class SnowflakeAstBuilder extends SnowflakeParserBaseVisitor[ir.LogicalPlan] wit
   private val relationBuilder = new SnowflakeRelationBuilder
   private val ddlBuilder = new SnowflakeDDLBuilder
   private val dmlBuilder = new SnowflakeDMLBuilder
+  private val commandBuilder = new SnowflakeCommandBuilder
 
   // TODO investigate why this is needed
   override protected def aggregateResult(aggregate: ir.LogicalPlan, nextResult: ir.LogicalPlan): ir.LogicalPlan = {
@@ -62,6 +63,10 @@ class SnowflakeAstBuilder extends SnowflakeParserBaseVisitor[ir.LogicalPlan] wit
   override def visitDmlCommand(ctx: DmlCommandContext): ir.LogicalPlan = ctx match {
     case c if c.queryStatement() != null => c.queryStatement().accept(this)
     case c => c.accept(dmlBuilder)
+  }
+
+  override def visitOtherCommand(ctx: OtherCommandContext): ir.LogicalPlan = {
+    ctx.accept(commandBuilder)
   }
 
   override def visitSnowSqlCommand(ctx: SnowSqlCommandContext): ir.LogicalPlan = {
