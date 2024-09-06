0bc0009b8e03b3cd74afe136624ed429dcea6adb
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2024-09-06 21:45:21 +0530

    Fix Query Generation IR for Select Distinct (#887)
    now it generates either Project or Deduplicates.
    
    ---------
    
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- tests/resources/functional/snowflake/test_skip_unsupported_operations/test_skip_unsupported_operations_11.sql
+++ tests/resources/functional/snowflake/test_skip_unsupported_operations/test_skip_unsupported_operations_11.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT
   , dd.store_id
   FROM proddb.public.dimension_salesforce_tasks dst
   JOIN edw.finance.dimension_deliveries dd
-    ON dd.delivery_id = dst.delivery_id --this ensures delivery_id is associated
+    ON dd.delivery_id = dst.delivery_id
   WHERE dst.mto_flag = 1
       AND dst.customer_type IN ('Consumer')
       AND dd.STORE_ID IN (SELECT store_id FROM foo.bar.cng_stores_stage)
@@ -29,7 +29,7 @@ SELECT DISTINCT
     dd.store_id
   FROM proddb.public.dimension_salesforce_tasks AS dst
   JOIN edw.finance.dimension_deliveries AS dd
-    ON dd.delivery_id = dst.delivery_id /* this ensures delivery_id is associated */
+    ON dd.delivery_id = dst.delivery_id
   WHERE
     dst.mto_flag = 1
     AND dst.customer_type IN ('Consumer')
@@ -40,9 +40,9 @@ SELECT DISTINCT
     )
     AND dd.is_test = false
     AND dst.origin IN ('Chat')
-    AND NOT dd_agent_id IS NULL
+    AND dd_agent_id IS NOT NULL
     AND dst.CREATED_DATE > CURRENT_DATE - 7
   ORDER BY
     1 DESC NULLS FIRST,
-    3 DESC NULLS FIRST
+    3 DESC NULLS FIRST;
 
