b788ec21b9d2d6b5bfbf604d7f430076c543b79d
Valentin Kasas <valentin.kasas@xebia.com>
2024-09-17 10:49:36 +0200

    TSQL coverage tests quick wins

--- tests/resources/functional/tsql/functions/test_lag_2.sql
+++ tests/resources/functional/tsql/functions/test_lag_2.sql
@@ -3,7 +3,7 @@
 -- The LAG function is identical in TSql and Databricks when IGNORING or RESPECTING NULLS (default).
 
 -- tsql sql:
-SELECT col1, col2, LAG(col2, 1, 0) OVER (ORDER BY col2 DESC) IGNORE NULLS AS lag_value FROM tabl;
+SELECT col1, col2, LAG(col2, 1, 0) IGNORE NULLS OVER (ORDER BY col2 DESC) AS lag_value FROM tabl;
 
 -- databricks sql:
-SELECT col1, col2, LAG(col2, 1, 0) OVER (ORDER BY col2 DESC) IGNORE NULLS AS lag_value FROM tabl;
+SELECT col1, col2, LAG(col2, 1, 0) IGNORE NULLS OVER (ORDER BY col2 DESC) AS lag_value FROM tabl;
