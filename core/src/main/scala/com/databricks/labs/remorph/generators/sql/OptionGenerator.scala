9d9cc7f6ee16a80170c222d6102c51ff9eee1880
Jim Idle <jim.idle@databricks.com>
2024-09-18 14:37:27 -0600

    feat: Clean imports for option generator

--- core/src/main/scala/com/databricks/labs/remorph/generators/sql/OptionGenerator.scala
+++ core/src/main/scala/com/databricks/labs/remorph/generators/sql/OptionGenerator.scala
@@ -1,29 +1,29 @@
 package com.databricks.labs.remorph.generators.sql
 
 import com.databricks.labs.remorph.generators.GeneratorContext
-import com.databricks.labs.remorph.parsers.intermediate.{GenericOption, OptionAuto, OptionDefault, OptionExpression, OptionOff, OptionOn, OptionString, OptionUnresolved}
+import com.databricks.labs.remorph.parsers.{intermediate => ir}
 
 class OptionGenerator(expr: ExpressionGenerator) {
 
-  def generateOption(ctx: GeneratorContext, option: GenericOption): String =
+  def generateOption(ctx: GeneratorContext, option: ir.GenericOption): String =
     option match {
-      case OptionExpression(id, value, supplement) =>
+      case ir.OptionExpression(id, value, supplement) =>
         s"$id = ${expr.generate(ctx, value)}" + supplement.map(s => s" $s").getOrElse("")
-      case OptionString(id, value) =>
+      case ir.OptionString(id, value) =>
         s"$id = '$value'"
-      case OptionOn(id) =>
+      case ir.OptionOn(id) =>
         s"$id = ON"
-      case OptionOff(id) =>
+      case ir.OptionOff(id) =>
         s"$id = OFF"
-      case OptionAuto(id) =>
+      case ir.OptionAuto(id) =>
         s"$id = AUTO"
-      case OptionDefault(id) =>
+      case ir.OptionDefault(id) =>
         s"$id = DEFAULT"
-      case OptionUnresolved(text) =>
+      case ir.OptionUnresolved(text) =>
         s"$text"
     }
 
-  def generateOptionList(ctx: GeneratorContext, options: Seq[GenericOption]): String =
+  def generateOptionList(ctx: GeneratorContext, options: Seq[ir.GenericOption]): String =
     options.map(generateOption(ctx, _)).mkString(", ")
 
 }
