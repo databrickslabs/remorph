11f4e796c32febd9e12137885233eca8dc5a1b52
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-09-18 11:55:09 +0530

    draft code added

--- core/src/main/scala/com/databricks/labs/remorph/discovery/SnowflakeTableDefination.scala
+++ core/src/main/scala/com/databricks/labs/remorph/discovery/SnowflakeTableDefination.scala
@@ -38,40 +38,7 @@ class SnowflakeTableDefination(conn: Connection) {
    */
   def getTableDefinitions: Seq[TableDefinition] = {
     val stmt = conn.createStatement()
-    val columnSchemaQuery = s"""SELECT
-                               |    sft.TABLE_CATALOG,
-                               |    sft.TABLE_SCHEMA,
-                               |    sft.TABLE_NAME,
-                               |    t.Schema as derivedSchema,
-                               |    sft.BYTES,
-                               |    sft.TABLE_TYPE
-                               |FROM (
-                               |    SELECT
-                               |        TABLE_CATALOG,
-                               |        TABLE_SCHEMA,
-                               |        TABLE_NAME,
-                               |        LISTAGG(column_name || ':' ||
-                               |            CASE
-                               |                WHEN numeric_precision IS NOT NULL AND numeric_scale IS NOT NULL
-                               |                THEN
-                               |                    CONCAT(data_type, '(', numeric_precision, ',' , numeric_scale, ')')
-                               |                WHEN LOWER(data_type) = 'text'
-                               |                THEN
-                               |                    CONCAT('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')
-                               |                ELSE data_type
-                               |             END|| ':' || TO_BOOLEAN(CASE WHEN IS_NULLABLE = 'YES' THEN 'true' ELSE 'false' END),
-                               |        '~') WITHIN GROUP (ORDER BY ordinal_position) AS Schema
-                               |    FROM
-                               |        SNOWFLAKE.INFORMATION_SCHEMA.COLUMNS
-                               |    GROUP BY
-                               |        TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
-                               |) t
-                               |JOIN SNOWFLAKE.INFORMATION_SCHEMA.TABLES sft
-                               |    ON t.TABLE_CATALOG = sft.TABLE_CATALOG
-                               |    AND t.TABLE_SCHEMA = sft.TABLE_SCHEMA
-                               |    AND t.TABLE_NAME = sft.TABLE_NAME
-                               |ORDER BY
-                               |    sft.TABLE_CATALOG, sft.TABLE_SCHEMA, sft.TABLE_NAME;""".stripMargin
+    val columnSchemaQuery = Constant.tableDefinitionQuery
     try {
       val tableDefinitionList = new scala.collection.mutable.ListBuffer[TableDefinition]()
       val rs = stmt.executeQuery(columnSchemaQuery)
@@ -83,7 +50,7 @@ class SnowflakeTableDefination(conn: Connection) {
           val columns = new ListBuffer[StructField]()
           rs.getString(4)
             .split("~")
-            .map(x => {
+            .foreach(x => {
               val data = x.split(":")
               val name = data(0)
               val dataType = getDataType(data(1))
