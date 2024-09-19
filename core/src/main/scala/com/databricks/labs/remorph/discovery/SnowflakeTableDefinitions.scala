38f342e9d082bcdd0614d78764b7cfab6090a385
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-09-19 17:25:22 +0530

    code implemented

--- core/src/main/scala/com/databricks/labs/remorph/discovery/SnowflakeTableDefinitions.scala
+++ core/src/main/scala/com/databricks/labs/remorph/discovery/SnowflakeTableDefinitions.scala
@@ -28,51 +28,51 @@ class SnowflakeTableDefinitions(conn: Connection) {
 
   private def getTableDefinitionQuery(catalogName: String): String = {
     s"""WITH column_info AS (
-       |    SELECT
-       |        TABLE_CATALOG,
-       |        TABLE_SCHEMA,
-       |        TABLE_NAME,
-       |        LISTAGG(column_name || ':' ||
-       |            CASE
-       |                WHEN numeric_precision IS NOT NULL AND numeric_scale IS NOT NULL
-       |                THEN
-       |                    CONCAT(data_type, '(', numeric_precision, ',' , numeric_scale, ')')
-       |                WHEN LOWER(data_type) = 'text'
-       |                THEN
-       |                    CONCAT('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')
-       |                ELSE data_type
-       |             END|| ':' || TO_BOOLEAN(CASE WHEN IS_NULLABLE = 'YES' THEN 'true' ELSE 'false' END),
-       |        '~') WITHIN GROUP (ORDER BY ordinal_position) AS Schema
-       |    FROM
-       |        ${catalogName}.INFORMATION_SCHEMA.COLUMNS
-       |    GROUP BY
-       |        TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
+       |  SELECT
+       |      TABLE_CATALOG,
+       |      TABLE_SCHEMA,
+       |      TABLE_NAME,
+       |      LISTAGG(column_name || ':' ||
+       |          CASE
+       |              WHEN numeric_precision IS NOT NULL AND numeric_scale IS NOT NULL
+       |              THEN
+       |                  CONCAT(data_type, '(', numeric_precision, ',' , numeric_scale, ')')
+       |              WHEN LOWER(data_type) = 'text'
+       |              THEN
+       |                  CONCAT('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')
+       |              ELSE data_type
+       |           END|| ':' || TO_BOOLEAN(CASE WHEN IS_NULLABLE = 'YES' THEN 'true' ELSE 'false' END),
+       |      '~') WITHIN GROUP (ORDER BY ordinal_position) AS Schema
+       |  FROM
+       |      ${catalogName}.INFORMATION_SCHEMA.COLUMNS
+       |  GROUP BY
+       |      TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
        |)
        |SELECT
-       |    sft.TABLE_CATALOG,
-       |    sft.TABLE_SCHEMA,
-       |    sft.TABLE_NAME,
-       |    sfe.location,
-       |    sfe.file_format_name,
-       |    sfv.view_definition,
-       |    column_info.Schema as DERIVED_SCHEMA,
-       |    sft.BYTES
+       |  sft.TABLE_CATALOG,
+       |  sft.TABLE_SCHEMA,
+       |  sft.TABLE_NAME,
+       |  sfe.location,
+       |  sfe.file_format_name,
+       |  sfv.view_definition,
+       |  column_info.Schema as DERIVED_SCHEMA,
+       |  sft.BYTES
        |FROM
-       |    column_info
+       |  column_info
        |JOIN ${catalogName}.INFORMATION_SCHEMA.TABLES sft
-       |    ON column_info.TABLE_CATALOG = sft.TABLE_CATALOG
-       |    AND column_info.TABLE_SCHEMA = sft.TABLE_SCHEMA
-       |    AND column_info.TABLE_NAME = sft.TABLE_NAME
+       |  ON column_info.TABLE_CATALOG = sft.TABLE_CATALOG
+       |  AND column_info.TABLE_SCHEMA = sft.TABLE_SCHEMA
+       |  AND column_info.TABLE_NAME = sft.TABLE_NAME
        |LEFT JOIN ${catalogName}.INFORMATION_SCHEMA.VIEWS sfv
-       |    ON column_info.TABLE_CATALOG = sfv.TABLE_CATALOG
-       |    AND column_info.TABLE_SCHEMA = sfv.TABLE_SCHEMA
-       |    AND column_info.TABLE_NAME = sfv.TABLE_NAME
+       |  ON column_info.TABLE_CATALOG = sfv.TABLE_CATALOG
+       |  AND column_info.TABLE_SCHEMA = sfv.TABLE_SCHEMA
+       |  AND column_info.TABLE_NAME = sfv.TABLE_NAME
        |LEFT JOIN ${catalogName}.INFORMATION_SCHEMA.EXTERNAL_TABLES sfe
-       |    ON column_info.TABLE_CATALOG = sfe.TABLE_CATALOG
-       |    AND column_info.TABLE_SCHEMA = sfe.TABLE_SCHEMA
-       |    AND column_info.TABLE_NAME = sfe.TABLE_NAME
+       |  ON column_info.TABLE_CATALOG = sfe.TABLE_CATALOG
+       |  AND column_info.TABLE_SCHEMA = sfe.TABLE_SCHEMA
+       |  AND column_info.TABLE_NAME = sfe.TABLE_NAME
        |ORDER BY
-       |    sft.TABLE_CATALOG, sft.TABLE_SCHEMA, sft.TABLE_NAME;""".stripMargin
+       |  sft.TABLE_CATALOG, sft.TABLE_SCHEMA, sft.TABLE_NAME;""".stripMargin
   }
 
   /**
@@ -108,7 +108,7 @@ class SnowflakeTableDefinitions(conn: Connection) {
               Option(rs.getString("FILE_FORMAT_NAME")), // FORMAT
               Option(rs.getString("VIEW_DEFINITION")), // view text
               columns,
-              rs.getInt("BYTES") / (1024 * 1024 * 1024) // sizeGb
+              sizeGb = rs.getInt("BYTES") / (1024 * 1024 * 1024) // sizeGb
             ))
         }
         tableDefinitionList
