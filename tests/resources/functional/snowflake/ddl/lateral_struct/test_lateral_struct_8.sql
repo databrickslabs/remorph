d5d0565a9a1fc46ccdf3a63eaccfd8f735208735
Valentin Kasas <valentin.kasas@xebia.com>
2024-09-20 09:54:31 +0200

    Simplify lateral_struct tests to use PARSE_JSON instead of FROM_JSON (#897)
    I've also manually checked that all the `test_lateral_struct_*` tests
    work on Snowflake and DB and yield the same results

--- tests/resources/functional/snowflake/ddl/lateral_struct/test_lateral_struct_8.sql
+++ tests/resources/functional/snowflake/ddl/lateral_struct/test_lateral_struct_8.sql
@@ -6,14 +6,7 @@ FROM VALUES
 
 -- databricks sql:
 SELECT
-  FROM_JSON(src.col, schema_of_json('{SRC.COL_SCHEMA}')).c AS c
-FROM VALUES
-   ('{"a": "1", "b": "2", "c": null}'),
-   ('{"a": "1", "b": "2", "c": "3"}') AS src(col);
-
--- experimental sql:
-SELECT
-  PARSE_JSON(src.col).c AS c
+  PARSE_JSON(src.col):c AS c
 FROM VALUES
    ('{"a": "1", "b": "2", "c": null}'),
    ('{"a": "1", "b": "2", "c": "3"}') AS src(col);
