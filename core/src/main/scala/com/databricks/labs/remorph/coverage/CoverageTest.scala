c7d33f7f2e7bba678f99bcbd8dccae551300b687
Valentin Kasas <valentin.kasas@xebia.com>
2024-09-26 17:52:39 +0200

    Make debug-coverage a proper command

--- core/src/main/scala/com/databricks/labs/remorph/coverage/CoverageTest.scala
+++ core/src/main/scala/com/databricks/labs/remorph/coverage/CoverageTest.scala
@@ -4,7 +4,13 @@ import com.databricks.labs.remorph.queries.{CommentBasedQueryExtractor, NestedFi
 
 import java.time.Instant
 
+case class DialectCoverageTest(dialectName: String, queryRunner: QueryRunner)
+
 class CoverageTest {
+
+  private val dialectCoverageTests = Seq(
+    DialectCoverageTest("snowflake", new IsTranspiledFromSnowflakeQueryRunner),
+    DialectCoverageTest("tsql", new IsTranspiledFromTSqlQueryRunner))
 
   private def getCurrentCommitHash: Option[String] = {
     val gitRevParse = os.proc("/usr/bin/git", "rev-parse", "--short", "HEAD").call(os.pwd)
@@ -19,45 +25,54 @@ class CoverageTest {
     val epoch = Instant.ofEpochMilli(0)
     java.time.Duration.between(epoch, instant).toNanos
   }
-  def run(sourceDir: os.Path, outputPath: os.Path, extractor: String, dialect: String): Unit = {
+  def run(sourceDir: os.Path, outputPath: os.Path, extractor: String): Unit = {
 
     val now = Instant.now
 
-    val dialectFullName = dialect match {
-      case "Snow" => "snowflake"
-      case "Tsql" => "tsql"
-    }
     val project = "remorph-core"
     val commitHash = getCurrentCommitHash
-    val testSource = new NestedFiles(sourceDir.toNIO)
-    val queryExtractor = extractor match {
-      case "comment" => new CommentBasedQueryExtractor(dialectFullName, "databricks")
-      case "full" => new WholeFileQueryExtractor
-    }
-    val queryRunner = dialect match {
-      case "Snow" => new IsTranspiledFromSnowflakeQueryRunner
-      case "Tsql" => new IsTranspiledFromTSqlQueryRunner
-    }
 
-    val outputFilePath = outputPath / s"$project-$dialect-databricks-${timeToEpochNanos(now)}.json"
+    val outputFilePath = outputPath / s"$project-coverage-${timeToEpochNanos(now)}.json"
 
     os.makeDir.all(outputPath)
 
-    testSource.listTests.foreach { test =>
-      queryExtractor.extractQuery(test.inputFile).foreach { exampleQuery =>
-        val runner = queryRunner
-        val header = ReportEntryHeader(
-          project = project,
-          commit_hash = commitHash,
-          version = "latest",
-          timestamp = now.toString,
-          source_dialect = dialect,
-          target_dialect = "databricks",
-          file = os.Path(test.inputFile).relativeTo(sourceDir).toString)
-        val report = runner.runQuery(exampleQuery)
-        val reportEntryJson = ReportEntry(header, report).asJson
-        os.write.append(outputFilePath, ujson.write(reportEntryJson, indent = -1) + "\n")
+    val reportsByDialect: Seq[(String, Seq[ReportEntry])] = dialectCoverageTests.map { t =>
+      val queryExtractor = extractor match {
+        case "comment" => new CommentBasedQueryExtractor(t.dialectName, "databricks")
+        case "full" => new WholeFileQueryExtractor
       }
+
+      t.dialectName -> new NestedFiles((sourceDir / t.dialectName).toNIO).listTests.flatMap { test =>
+        queryExtractor
+          .extractQuery(test.inputFile)
+          .map { exampleQuery =>
+            val runner = t.queryRunner
+            val header = ReportEntryHeader(
+              project = project,
+              commit_hash = commitHash,
+              version = "latest",
+              timestamp = now.toString,
+              source_dialect = t.dialectName,
+              target_dialect = "databricks",
+              file = os.Path(test.inputFile).relativeTo(sourceDir).toString)
+            val report = runner.runQuery(exampleQuery)
+            ReportEntry(header, report)
+          }
+          .toSeq
+
+      }
+    }
+
+    reportsByDialect.foreach { case (dialect, reports) =>
+      reports.foreach(report => os.write.append(outputFilePath, ujson.write(report.asJson, indent = -1) + "\n"))
+      val total = reports.size
+      val parsed = reports.map(_.report.parsed).sum
+      val transpiled = reports.map(_.report.transpiled).sum
+      // scalastyle:off
+      println(
+        s"remorph -> $dialect: ${100 * parsed / total}% parsed ($parsed / $total)," +
+          s" ${100 * transpiled / total}% transpiled ($transpiled / $total)")
+    // scalastyle:on
     }
   }
 }
