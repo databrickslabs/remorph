6d1af9e7368aa6387d727c93a5dacb233b210d95
sundar.shankar <sundar.shankar@databricks.com>
2024-09-27 12:37:33 +0530

    Add edges Init Commit

--- core/src/main/scala/com/databricks/labs/remorph/graph/TableDependencyGraph.scala
+++ core/src/main/scala/com/databricks/labs/remorph/graph/TableDependencyGraph.scala
@@ -9,12 +9,23 @@ import com.databricks.labs.remorph.parsers.{intermediate => ir}
 class TableDependencyGraph(parser: PlanParser[_]) extends DependencyGraph with LazyLogging {
   private val nodes = scala.collection.mutable.Set.empty[TableDefinition]
   private val edges = scala.collection.mutable.Set.empty[(TableDefinition, TableDefinition)]
-  private val nodeMetadata = scala.collection.mutable.Map.empty[TableDefinition, Map[String, String]]
+  private val nodeMetadata = scala.collection.mutable.Map.empty[TableDefinition, Map[String, Set[String]]]
   private val edgeMetadata = scala.collection.mutable.Map.empty[(TableDefinition, TableDefinition), Map[String, String]]
 
   override protected def addNode(id: TableDefinition, metadata: Map[String, String]): Unit = {
-    nodes += id
-    nodeMetadata += id -> metadata
+    // Metadata list of query ids and add node only if it is not already present.
+    // for Example if a table is used in multiple queries, we need to consolidate the metadata
+    // here we are storing only query hash id as metadata not the query itself
+    if (nodes.contains(id)) {
+      val existingMetadata = nodeMetadata(id)
+      val consolidatedMetadata = (existingMetadata.toSeq ++ metadata.toSeq)
+        .groupBy(_._1)
+        .mapValues(_.map(_._2.toString).toSet)
+      nodeMetadata.update(id, consolidatedMetadata)
+    } else {
+      nodes += id
+      nodeMetadata += id -> metadata.mapValues(Set(_))
+    }
   }
 
   override protected def addEdge(from: TableDefinition, to: TableDefinition, metadata: Map[String, String]): Unit = {
@@ -26,24 +37,45 @@ class TableDependencyGraph(parser: PlanParser[_]) extends DependencyGraph with L
 
   override protected def getEdges: Set[(TableDefinition, TableDefinition)] = edges.toSet
 
+  private def walkWithParent(plan: ir.LogicalPlan)(f: (ir.LogicalPlan, Option[ir.LogicalPlan]) => Unit): Unit = {
+    def recurse(node: ir.LogicalPlan, parent: Option[ir.LogicalPlan]): Unit = {
+      f(node, parent)
+      node.children.foreach(child => recurse(child, Some(node)))
+    }
+    recurse(plan, None)
+  }
+
+  private def generateEdges(plan: ir.LogicalPlan, tableDefinition: Seq[TableDefinition], queryId: String): Unit = {
+    walkWithParent(plan) {
+      case (node: ir.NamedTable, Some(parent)) =>
+        parent match {
+          case _: ir.Project | _: ir.Join | _: ir.SubqueryAlias | _: ir.Filter =>
+            val fromTable = tableDefinition.filter(_.table == node.unparsed_identifier).head
+            val toTable = tableDefinition.filter(_.table == parent.asInstanceOf[ir.NamedTable].unparsed_identifier).head
+            // TODO figure out a simpler way to add Action
+            addEdge(fromTable, toTable, Map("query" -> queryId, "action" -> "action"))
+          case _ => // Do nothing for other parent types
+        }
+      case _ => // Do nothing for other node types
+    }
+  }
+
   def buildGraph(queryHistory: QueryHistory, tableDefinition: Seq[TableDefinition]): Unit = {
     queryHistory.queries.foreach { query =>
       val plan = parser.parse(SourceCode(query.source)).flatMap(parser.visit)
-
       plan match {
         case Result.Success(plan) =>
-          plan collect ({
-            case ir.NamedTable(name, _, _) =>
-              addNode(tableDefinition.filter(_.table == name).head, Map("query" -> query.source))
-            // TODO Add Logic for Edges
-            case _ => // Do Nothing
-          })
+          plan collect { case x: ir.NamedTable =>
+            addNode(tableDefinition.filter(_.table == x.unparsed_identifier).head, Map("query" -> query.id))
+          }
+          generateEdges(plan, tableDefinition, query.id)
 
         case _ => logger.warn(s"Failed to produce plan from query: ${query.source}")
       }
     }
   }
 
+  // TODO Implement logic for fetching edges(parents) only upto certain level
   override def getRoot(table: String, level: Int): TableDefinition = getNodes
     .find(_.table == table)
     .getOrElse(throw new NoSuchElementException(s"No table ${table} found"))
