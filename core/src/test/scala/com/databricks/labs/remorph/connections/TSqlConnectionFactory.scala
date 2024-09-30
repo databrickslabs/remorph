6d542d163e7a82dd41351ba0e652cd3315315b8d
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-09-30 18:30:22 +0530

    code implemented

--- core/src/test/scala/com/databricks/labs/remorph/connections/TSqlConnectionFactory.scala
+++ core/src/test/scala/com/databricks/labs/remorph/connections/TSqlConnectionFactory.scala
@@ -4,6 +4,7 @@ import java.sql.{Connection, DriverManager}
 
 class TSqlConnectionFactory(env: EnvGetter) {
 
+  Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
   private val jdbcUrl = env.get("TEST_TSQL_JDBC")
   private val username = env.get("TEST_TSQL_USER")
   private val password = env.get("TEST_TSQL_PASS")
