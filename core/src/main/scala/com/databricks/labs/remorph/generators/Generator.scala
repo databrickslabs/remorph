3b69654afdb7e4a2e6420cfeb812f9f46d716af9
Valentin Kasas <valentin.kasas@xebia.com>
2024-10-09 12:13:12 +0200

    Introduce typed errors (#981)
    In a future PR, throwing exceptions will be removed in favor of
    returning `Result`

--- core/src/main/scala/com/databricks/labs/remorph/generators/Generator.scala
+++ core/src/main/scala/com/databricks/labs/remorph/generators/Generator.scala
@@ -1,9 +1,9 @@
 package com.databricks.labs.remorph.generators
 
-import com.databricks.labs.remorph.intermediate.TreeNode
+import com.databricks.labs.remorph.intermediate.{UnexpectedNode, TreeNode}
 import com.databricks.labs.remorph.transpilers.TranspileException
 
 trait Generator[In <: TreeNode[In], Out] {
   def generate(ctx: GeneratorContext, tree: In): Out
-  def unknown(tree: In): TranspileException = TranspileException(s"not implemented: $tree")
+  def unknown(tree: In): TranspileException = TranspileException(UnexpectedNode(tree))
 }
