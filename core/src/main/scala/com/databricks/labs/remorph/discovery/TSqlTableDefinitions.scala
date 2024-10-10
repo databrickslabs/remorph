0b6698e5b91ea86d9bc3e1570c23b7cddd109c9c
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-10-10 17:56:48 +0530

    indentation changed

--- core/src/main/scala/com/databricks/labs/remorph/discovery/TSqlTableDefinitions.scala
+++ core/src/main/scala/com/databricks/labs/remorph/discovery/TSqlTableDefinitions.scala
@@ -28,66 +28,66 @@ class TSqlTableDefinitions(conn: Connection) {
   private def getTableDefinitionQuery(catalogName: String): String = {
     s"""WITH column_info AS (
        |  SELECT
-       |      TABLE_CATALOG,
-       |      TABLE_SCHEMA,
-       |      TABLE_NAME,
-       |      STRING_AGG(
-       |              CONCAT(
-       |                      column_name, '§',
-       |                      CASE
-       |                          WHEN numeric_precision IS NOT NULL AND numeric_scale IS NOT NULL
-       |                              THEN CONCAT(data_type, '(', numeric_precision, ',', numeric_scale, ')')
-       |                          WHEN LOWER(data_type) = 'text'
-       |                              THEN CONCAT('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')
-       |                          ELSE data_type
-       |                          END,
-       |                      '§',
-       |                      CASE WHEN IS_NULLABLE = 'YES' THEN 'true' ELSE 'false' END,
-       |                      '§',ISNULL(CONVERT(NVARCHAR(MAX), ep.value), '')
-       |              ),
-       |              '‡'
-       |      ) WITHIN GROUP (ORDER BY ordinal_position) AS DERIVED_SCHEMA
+       |    TABLE_CATALOG,
+       |    TABLE_SCHEMA,
+       |    TABLE_NAME,
+       |    STRING_AGG(
+       |      CONCAT(
+       |        column_name, '§',
+       |        CASE
+       |          WHEN numeric_precision IS NOT NULL AND numeric_scale IS NOT NULL
+       |            THEN CONCAT(data_type, '(', numeric_precision, ',', numeric_scale, ')')
+       |          WHEN LOWER(data_type) = 'text'
+       |            THEN CONCAT('varchar', '(', CHARACTER_MAXIMUM_LENGTH, ')')
+       |          ELSE data_type
+       |        END,
+       |        '§',
+       |        CASE WHEN IS_NULLABLE = 'YES' THEN 'true' ELSE 'false' END,
+       |        '§',ISNULL(CONVERT(NVARCHAR(MAX), ep.value), '')
+       |      ),
+       |      '‡'
+       |    ) WITHIN GROUP (ORDER BY ordinal_position) AS DERIVED_SCHEMA
        |  FROM
-       |      ${catalogName}.INFORMATION_SCHEMA.COLUMNS c
-       |      INNER JOIN ${catalogName}.sys.tables t on c.table_name = t.name
-       |      LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id
+       |    ${catalogName}.INFORMATION_SCHEMA.COLUMNS c
+       |    INNER JOIN ${catalogName}.sys.tables t on c.table_name = t.name
+       |    LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id
        |  GROUP BY
-       |      TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
+       |    TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME
        |)
-       | , table_file_info AS (
+       |, table_file_info AS (
        |  SELECT
-       |      s.name AS TABLE_SCHEMA,
-       |      t.name AS TABLE_NAME,
-       |      f.physical_name AS location,
-       |      f.type_desc AS TABLE_FORMAT,
-       |      CAST(ROUND(SUM(a.used_pages) * 8.0 / 1024, 2) AS DECIMAL(18,2)) AS SIZE_GB
+       |    s.name AS TABLE_SCHEMA,
+       |    t.name AS TABLE_NAME,
+       |    f.physical_name AS location,
+       |    f.type_desc AS TABLE_FORMAT,
+       |    CAST(ROUND(SUM(a.used_pages) * 8.0 / 1024, 2) AS DECIMAL(18,2)) AS SIZE_GB
        |  FROM
-       |      ${catalogName}.sys.tables t
-       |          INNER JOIN
-       |      ${catalogName}.sys.indexes i ON t.object_id = i.object_id
-       |          INNER JOIN
-       |      ${catalogName}.sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
-       |          INNER JOIN
-       |      ${catalogName}.sys.allocation_units a ON p.partition_id = a.container_id
-       |          INNER JOIN
-       |      ${catalogName}.sys.schemas s ON t.schema_id = s.schema_id
-       |          INNER JOIN
-       |      ${catalogName}.sys.database_files f ON a.data_space_id = f.data_space_id
-       |          LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id AND ep.minor_id = 0
+       |    ${catalogName}.sys.tables t
+       |      INNER JOIN
+       |    ${catalogName}.sys.indexes i ON t.object_id = i.object_id
+       |      INNER JOIN
+       |    ${catalogName}.sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+       |      INNER JOIN
+       |    ${catalogName}.sys.allocation_units a ON p.partition_id = a.container_id
+       |      INNER JOIN
+       |    ${catalogName}.sys.schemas s ON t.schema_id = s.schema_id
+       |      INNER JOIN
+       |    ${catalogName}.sys.database_files f ON a.data_space_id = f.data_space_id
+       |      LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id AND ep.minor_id = 0
        |  GROUP BY
-       |      s.name, t.name, f.name, f.physical_name, f.type_desc
+       |    s.name, t.name, f.name, f.physical_name, f.type_desc
        |),
-       |  table_comment_info AS (
-       |      SELECT
-       |          s.name AS TABLE_SCHEMA,
-       |          t.name AS TABLE_NAME,
-       |          ep.value AS TABLE_COMMENT
-       |      FROM
-       |          ${catalogName}.sys.tables t
-       |              INNER JOIN ${catalogName}.sys.schemas s ON t.schema_id = s.schema_id
-       |              LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id
-       |              AND ep.minor_id = 0
-       |  )
+       |table_comment_info AS (
+       |  SELECT
+       |    s.name AS TABLE_SCHEMA,
+       |    t.name AS TABLE_NAME,
+       |    ep.value AS TABLE_COMMENT
+       |  FROM
+       |    ${catalogName}.sys.tables t
+       |      INNER JOIN ${catalogName}.sys.schemas s ON t.schema_id = s.schema_id
+       |      LEFT JOIN ${catalogName}.sys.extended_properties ep ON ep.major_id = t.object_id
+       |      AND ep.minor_id = 0
+       |)
        |SELECT
        |  sft.TABLE_CATALOG,
        |  sft.TABLE_SCHEMA,
@@ -100,23 +100,22 @@ class TSqlTableDefinitions(conn: Connection) {
        |  tci.TABLE_COMMENT
        |FROM
        |  column_info
-       |      JOIN ${catalogName}.INFORMATION_SCHEMA.TABLES sft
-       |           ON column_info.TABLE_CATALOG = sft.TABLE_CATALOG
-       |               AND column_info.TABLE_SCHEMA = sft.TABLE_SCHEMA
-       |               AND column_info.TABLE_NAME = sft.TABLE_NAME
-       |      LEFT JOIN ${catalogName}.INFORMATION_SCHEMA.VIEWS sfv
-       |                ON column_info.TABLE_CATALOG = sfv.TABLE_CATALOG
-       |                    AND column_info.TABLE_SCHEMA = sfv.TABLE_SCHEMA
-       |                    AND column_info.TABLE_NAME = sfv.TABLE_NAME
-       |      LEFT JOIN table_file_info tfi
-       |                ON column_info.TABLE_SCHEMA = tfi.TABLE_SCHEMA
-       |                    AND column_info.TABLE_NAME = tfi.TABLE_NAME
-       |      LEFT JOIN table_comment_info tci
-       |                  ON column_info.TABLE_SCHEMA = tci.TABLE_SCHEMA
-       |                      AND column_info.TABLE_NAME = tci.TABLE_NAME
-       |
+       |    JOIN ${catalogName}.INFORMATION_SCHEMA.TABLES sft
+       |      ON column_info.TABLE_CATALOG = sft.TABLE_CATALOG
+       |        AND column_info.TABLE_SCHEMA = sft.TABLE_SCHEMA
+       |        AND column_info.TABLE_NAME = sft.TABLE_NAME
+       |    LEFT JOIN ${catalogName}.INFORMATION_SCHEMA.VIEWS sfv
+       |      ON column_info.TABLE_CATALOG = sfv.TABLE_CATALOG
+       |        AND column_info.TABLE_SCHEMA = sfv.TABLE_SCHEMA
+       |        AND column_info.TABLE_NAME = sfv.TABLE_NAME
+       |    LEFT JOIN table_file_info tfi
+       |      ON column_info.TABLE_SCHEMA = tfi.TABLE_SCHEMA
+       |        AND column_info.TABLE_NAME = tfi.TABLE_NAME
+       |    LEFT JOIN table_comment_info tci
+       |      ON column_info.TABLE_SCHEMA = tci.TABLE_SCHEMA
+       |        AND column_info.TABLE_NAME = tci.TABLE_NAME
        |ORDER BY
-       |    sft.TABLE_CATALOG, sft.TABLE_SCHEMA, sft.TABLE_NAME;
+       |  sft.TABLE_CATALOG, sft.TABLE_SCHEMA, sft.TABLE_NAME;
        |""".stripMargin
   }
 
