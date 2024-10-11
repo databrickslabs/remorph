6ef3079b823c2c31e8795806b0163beea8aa7b21
sundar.shankar <sundar.shankar@databricks.com>
2024-10-11 11:19:06 +0530

    Added FileQueryHistory Implementation

--- core/src/main/scala/com/databricks/labs/remorph/discovery/FileQueryHistory.scala
+++ core/src/main/scala/com/databricks/labs/remorph/discovery/FileQueryHistory.scala
@@ -1,9 +1,33 @@
 package com.databricks.labs.remorph.discovery
 
-class FileQueryHistory extends QueryHistoryProvider {
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
+import scala.collection.JavaConverters._
+import scala.io.Source
+
+class FileQueryHistory(path: Path) extends QueryHistoryProvider {
+
+  private def extractQueriesFromFile(file: File): Seq[ExecutedQuery] = {
+    val fileContent = Source.fromFile(file)
+    val queries = fileContent.getLines().mkString("\n").split(";").map(_.trim).filter(_.nonEmpty)
+    queries.map(ExecutedQuery("10", _, QuerySpec(filename = Some(file.getName))))
+  }
+
+  private def extractQueriesFromFolder(folder: Path): Seq[ExecutedQuery] = {
+    val files =
+      Files
+        .walk(folder)
+        .iterator()
+        .asScala
+        .filter(f => Files.isRegularFile(f))
+        .toSeq
+        .filter(_.getFileName.toString.endsWith(".sql"))
+
+    files.flatMap(file => extractQueriesFromFile(file.toFile))
+  }
 
   override def history(): QueryHistory = {
-    val queries = Seq.empty[ExecutedQuery]
+    val queries = extractQueriesFromFolder(path)
     QueryHistory(queries)
   }
 
