ff4be80b307872ab9335ca17a313d615bd41abac
Sriram Mohanty <sriram.mohanty@databricks.com>
2024-10-16 11:05:51 +0530

    fixing test cases

--- core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeExpressionBuilderSpec.scala
+++ core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakeExpressionBuilderSpec.scala
@@ -269,7 +269,7 @@ class SnowflakeExpressionBuilderSpec
             window_function = CallFunction("ROW_NUMBER", Seq()),
             partition_spec = Seq(),
             sort_order = Seq(SortOrder(Id("a"), Descending, NullsFirst)),
-            frame_spec = Some(WindowFrame(RowsFrame, UnboundedPreceding, UnboundedFollowing))))
+            frame_spec = None))
       }
       "ROW_NUMBER() OVER (PARTITION BY a)" in {
         exampleExpr(
@@ -279,7 +279,7 @@ class SnowflakeExpressionBuilderSpec
             window_function = CallFunction("ROW_NUMBER", Seq()),
             partition_spec = Seq(Id("a")),
             sort_order = Seq(),
-            frame_spec = Some(WindowFrame(RowsFrame, UnboundedPreceding, UnboundedFollowing))))
+            frame_spec = None))
       }
       "NTILE(42) OVER (PARTITION BY a ORDER BY b, c DESC, d)" in {
         exampleExpr(
