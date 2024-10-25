1644849ea3656972a6985f4d7a568738f9427bae
sundar.shankar <sundar.shankar@databricks.com>
2024-10-25 15:52:08 +0530

    intermediate commit

---
+++ tests/resources/functional/snowflake/core_engine/test_cte/recursive_cte.sql
@@ -0,0 +1,27 @@
+-- snowflake sql:
+WITH RECURSIVE employee_hierarchy AS (
+    SELECT
+        employee_id,
+        manager_id,
+        employee_name,
+        1 AS level
+    FROM
+        employees
+    WHERE
+        manager_id IS NULL
+    UNION ALL
+    SELECT
+        e.employee_id,
+        e.manager_id,
+        e.employee_name,
+        eh.level + 1 AS level
+    FROM
+        employees e
+    INNER JOIN
+        employee_hierarchy eh ON e.manager_id = eh.employee_id
+)
+SELECT *
+FROM employee_hierarchy
+ORDER BY level, employee_id;
+
+-- databricks sql:
