d87ef3328dec80cbdb0639b47d7b4b0050de0d9a
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-11-11 18:41:41 +0100

    refactor parsing pipeline

--- core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakePlanParser.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakePlanParser.scala
@@ -1,9 +1,13 @@
 package com.databricks.labs.remorph.parsers.snowflake
 
-import com.databricks.labs.remorph.parsers.PlanParser
+import com.databricks.labs.remorph.intermediate.{LogicalPlan, ParsingErrors, PlanGenerationFailure}
+import com.databricks.labs.remorph.parsers.{PlanParser, ProductionErrorCollector}
 import com.databricks.labs.remorph.parsers.snowflake.rules._
-import com.databricks.labs.remorph.{intermediate => ir}
-import org.antlr.v4.runtime.{CharStream, Lexer, ParserRuleContext, TokenStream}
+import com.databricks.labs.remorph.{BuildingAst, KoResult, Parsing, Transformation, WorkflowStage, intermediate => ir}
+import org.antlr.v4.runtime.tree.ParseTree
+import org.antlr.v4.runtime.{CharStreams, CommonTokenStream, Parser}
+
+import scala.util.control.NonFatal
 
 class SnowflakePlanParser extends PlanParser[SnowflakeParser] {
 
@@ -15,6 +19,34 @@ class SnowflakePlanParser extends PlanParser[SnowflakeParser] {
   override protected def createPlan(tree: ParserRuleContext): ir.LogicalPlan = vc.astBuilder.visit(tree)
   override protected def addErrorStrategy(parser: SnowflakeParser): Unit =
     parser.setErrorHandler(new SnowflakeErrorStrategy)
+    val errListener = new ProductionErrorCollector(parsing.source, parsing.filename)
+    parser.removeErrorListeners()
+    parser.addErrorListener(errListener)
+    val tree = parser.snowflakeFile()
+    if (errListener.errorCount > 0) {
+      lift(KoResult(stage = WorkflowStage.PARSE, ParsingErrors(errListener.errors)))
+    } else {
+      update {
+        case p: Parsing => BuildingAst(tree, Some(p))
+        case _ => BuildingAst(tree)
+      }.flatMap { _ =>
+        try {
+          ok(createPlan(parser, tree))
+        } catch {
+          case NonFatal(e) =>
+            lift(KoResult(stage = WorkflowStage.PLAN, PlanGenerationFailure(e)))
+        }
+      }
+
+    }
+
+  }
+
+  private def createPlan(parser: Parser, tree: ParseTree): LogicalPlan = {
+    val plan = vc.astBuilder.visit(tree)
+    plan
+  }
+
   def dialect: String = "snowflake"
 
   // TODO: Note that this is not the correct place for the optimizer, but it is here for now
--- core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlPlanParser.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/tsql/TSqlPlanParser.scala
@@ -1,19 +1,51 @@
 package com.databricks.labs.remorph.parsers.tsql
 
-import com.databricks.labs.remorph.parsers.PlanParser
+import com.databricks.labs.remorph.intermediate.{LogicalPlan, ParsingErrors, PlanGenerationFailure}
+import com.databricks.labs.remorph.parsers.{PlanParser, ProductionErrorCollector}
 import com.databricks.labs.remorph.parsers.tsql.rules.{PullLimitUpwards, TSqlCallMapper, TopPercentToLimitSubquery, TrapInsertDefaultsAction}
-import com.databricks.labs.remorph.{intermediate => ir}
+import com.databricks.labs.remorph.{BuildingAst, KoResult, Parsing, Transformation, WorkflowStage, intermediate => ir}
 import org.antlr.v4.runtime._
+import org.antlr.v4.runtime.tree.ParseTree
+
+import scala.util.control.NonFatal
 
 class TSqlPlanParser extends PlanParser[TSqlParser] {
 
   val vc = new TSqlVisitorCoordinator(TSqlParser.VOCABULARY, TSqlParser.ruleNames)
 
-  override protected def createLexer(input: CharStream): Lexer = new TSqlLexer(input)
-  override protected def createParser(stream: TokenStream): TSqlParser = new TSqlParser(stream)
-  override protected def createTree(parser: TSqlParser): ParserRuleContext = parser.tSqlFile()
-  override protected def createPlan(tree: ParserRuleContext): ir.LogicalPlan = vc.astBuilder.visit(tree)
-  override protected def addErrorStrategy(parser: TSqlParser): Unit = parser.setErrorHandler(new TSqlErrorStrategy)
+  override def parse(parsing: Parsing): Transformation[LogicalPlan] = {
+    val input = CharStreams.fromString(parsing.source)
+    val lexer = new TSqlLexer(input)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new TSqlParser(tokens)
+    parser.setErrorHandler(new TSqlErrorStrategy)
+    val errListener = new ProductionErrorCollector(parsing.source, parsing.filename)
+    parser.removeErrorListeners()
+    parser.addErrorListener(errListener)
+    val tree = parser.tSqlFile()
+    if (errListener.errorCount > 0) {
+      lift(KoResult(stage = WorkflowStage.PARSE, ParsingErrors(errListener.errors)))
+    } else {
+      update {
+        case p: Parsing => BuildingAst(tree, Some(p))
+        case _ => BuildingAst(tree)
+      }.flatMap { _ =>
+        try {
+          ok(createPlan(parser, tree))
+        } catch {
+          case NonFatal(e) =>
+            lift(KoResult(stage = WorkflowStage.PLAN, PlanGenerationFailure(e)))
+        }
+      }
+
+    }
+
+  }
+
+  private def createPlan(parser: Parser, tree: ParseTree): LogicalPlan = {
+    val plan = vc.astBuilder.visit(tree)
+    plan
+  }
   def dialect: String = "tsql"
 
   // TODO: Note that this is not the correct place for the optimizer, but it is here for now
