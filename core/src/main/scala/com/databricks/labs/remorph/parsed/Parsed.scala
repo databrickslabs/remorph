cee2e2d043c350f7061f9ba25e1cdc987e9408a5
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-11-14 13:15:32 +0100

    more Parsed

--- core/src/main/scala/com/databricks/labs/remorph/parsed/Parsed.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsed/Parsed.scala
@@ -1,15 +1,29 @@
 package com.databricks.labs.remorph.parsed
 
+import org.antlr.v4.runtime.{ParserRuleContext, Token}
+
 case class ParsedLocation(line: Int, column: Int, tokenIndex: Int)
 
 object ParsedLocation {
   val empty: ParsedLocation = ParsedLocation(-1, -1, -1)
+
+  def fromToken(token: Token): ParsedLocation =
+    ParsedLocation(token.getLine, token.getCharPositionInLine, token.getTokenIndex)
 }
 
-case class ParsedRange(start: ParsedLocation, end: ParsedLocation)
+case class ParsedRange(start: ParsedLocation, end: ParsedLocation) {
+
+  def isAfterLine(line: Int): Boolean = start.line > line
+
+}
 
 object ParsedRange {
+
   val empty: ParsedRange = ParsedRange(ParsedLocation.empty, ParsedLocation.empty)
+
+  def fromParserRuleContext(ctx: ParserRuleContext): ParsedRange = {
+    ParsedRange(ParsedLocation.fromToken(ctx.start), ParsedLocation.fromToken(ctx.stop))
+  }
 }
 
 
