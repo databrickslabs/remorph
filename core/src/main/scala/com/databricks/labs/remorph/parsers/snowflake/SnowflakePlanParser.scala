81f43eb1f46f86ed6575a0f88275cc7aa6c54821
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-11-20 20:42:18 +0100

    rename for clarity

--- core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakePlanParser.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/SnowflakePlanParser.scala
@@ -20,7 +20,7 @@ class SnowflakePlanParser extends PlanParser[SnowflakeParser] {
   // TODO: Note that this is not the correct place for the optimizer, but it is here for now
   override protected def createOptimizer: ir.Rules[ir.LogicalPlan] = {
     ir.Rules(
-      new DealiasLCAFilter,
+      new DealiasLCAs,
       new ConvertFractionalSecond,
       new FlattenLateralViewToExplode(),
       new SnowflakeCallMapper,
