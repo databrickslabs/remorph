a555a9146b9774896f135fa378e3c971ec220d42
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-11-22 16:09:18 +0100

    rename for clarity

---
+++ tests/resources/functional/snowflake/core_engine/lca/lca_homonym.sql
@@ -0,0 +1,24 @@
+-- snowflake sql:
+select ca_zip
+     from (
+      SELECT
+      substr(ca_zip,1,5) ca_zip,
+      trim(name) as name,
+      count(*) over( partition by ca_zip)
+      FROM customer_address
+      WHERE ca_zip IN ('89436', '30868'));
+-- databricks sql:
+SELECT
+  ca_zip
+FROM
+SELECT
+  SUBSTR(ca_zip,1,5) AS ca_zip,
+  TRIM(name) AS name,
+  COUNT(*) OVER (
+    PARTITION BY
+      SUBSTR(ca_zip,1,5)
+  )
+FROM
+  customer_address
+WHERE
+  SUBSTR(ca_zip,1,5) IN ('89436', '30868');
