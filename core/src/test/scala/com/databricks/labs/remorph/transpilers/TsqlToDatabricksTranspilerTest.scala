0f4cf7ad02c364a60ff9bff9156a650a9b86e021
Andrew Snare <andrew.snare@databricks.com>
2024-11-26 13:45:11 +0100

    Transpiler tests for TSql set operations.

--- core/src/test/scala/com/databricks/labs/remorph/transpilers/TsqlToDatabricksTranspilerTest.scala
+++ core/src/test/scala/com/databricks/labs/remorph/transpilers/TsqlToDatabricksTranspilerTest.scala
@@ -9,9 +9,28 @@ class TsqlToDatabricksTranspilerTest extends AnyWordSpec with TranspilerTestComm
   "The TSQL-to-Databricks transpiler" when {
     "transpiling set operations" should {
       val expectedTranslations = Map(
-        "(SELECT a, b from c) UNION SELECT x, y FROM z" -> "(SELECT a, b from c) UNION (SELECT x, y from z);",
-        "(SELECT a, b from c) UNION ALL SELECT x, y FROM z" -> "(SELECT a, b from c) UNION ALL (SELECT x, y from z);",
-        "(SELECT a, b FROM c)" -> "SELECT a, b FROM c;")
+        "SELECT a, b FROM c UNION SELECT x, y FROM z" -> "(SELECT a, b FROM c) UNION (SELECT x, y FROM z);",
+        "SELECT a, b FROM c UNION ALL SELECT x, y FROM z" -> "(SELECT a, b FROM c) UNION ALL (SELECT x, y FROM z);",
+        "SELECT a, b FROM c EXCEPT SELECT x, y FROM z" -> "(SELECT a, b FROM c) EXCEPT (SELECT x, y FROM z);",
+        "SELECT a, b FROM c INTERSECT SELECT x, y FROM z" -> "(SELECT a, b FROM c) INTERSECT (SELECT x, y FROM z);",
+        "SELECT a, b FROM c UNION (SELECT x, y FROM z)" -> "(SELECT a, b FROM c) UNION (SELECT x, y FROM z);",
+        "(SELECT a, b FROM c) UNION SELECT x, y FROM z" -> "(SELECT a, b FROM c) UNION (SELECT x, y FROM z);",
+        "(SELECT a, b FROM c) UNION ALL SELECT x, y FROM z" -> "(SELECT a, b FROM c) UNION ALL (SELECT x, y FROM z);",
+        "(SELECT a, b FROM c)" -> "SELECT a, b FROM c;",
+        """SELECT a, b FROM c
+          |UNION
+          |SELECT d, e FROM f
+          |UNION ALL
+          |SELECT g, h FROM i
+          |EXCEPT
+          |SELECT j, k FROM l
+          |INTERSECT
+          |SELECT m, n FROM o""".stripMargin ->
+          """((((SELECT a, b FROM c) UNION
+            |   (SELECT d, e FROM f)) UNION ALL
+            |  (SELECT g, h FROM i)) EXCEPT
+            | (SELECT j, k FROM l)) INTERSECT
+            |(SELECT m, n FROM o);""".stripMargin)
       expectedTranslations.foreach { case (originalTSql, expectedDatabricksSql) =>
         s"correctly transpile: $originalTSql" in {
           originalTSql transpilesTo expectedDatabricksSql
