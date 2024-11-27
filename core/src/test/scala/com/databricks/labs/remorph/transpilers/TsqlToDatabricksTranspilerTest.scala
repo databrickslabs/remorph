a4500572fd34a3e74cec23eda9121b271e144e6b
Andrew Snare <andrew.snare@databricks.com>
2024-11-27 12:56:14 +0100

    Update transpiler test for INTERSECT to trivially check precedence.

--- core/src/test/scala/com/databricks/labs/remorph/transpilers/TsqlToDatabricksTranspilerTest.scala
+++ core/src/test/scala/com/databricks/labs/remorph/transpilers/TsqlToDatabricksTranspilerTest.scala
@@ -22,15 +22,19 @@ class TsqlToDatabricksTranspilerTest extends AnyWordSpec with TranspilerTestComm
           |SELECT d, e FROM f
           |UNION ALL
           |SELECT g, h FROM i
-          |EXCEPT
-          |SELECT j, k FROM l
           |INTERSECT
+          |SELECT j, k FROM l
+          |EXCEPT
           |SELECT m, n FROM o""".stripMargin ->
-          """((((SELECT a, b FROM c) UNION
-            |   (SELECT d, e FROM f)) UNION ALL
-            |  (SELECT g, h FROM i)) EXCEPT
-            | (SELECT j, k FROM l)) INTERSECT
-            |(SELECT m, n FROM o);""".stripMargin)
+          """(((SELECT a, b FROM c)
+            |  UNION
+            |  (SELECT d, e FROM f))
+            | UNION ALL
+            | (SELECT g, h FROM i))
+            |INTERSECT
+            | ((SELECT j, k FROM l)
+            |  EXCEPT
+            |  (SELECT m, n FROM o));""".stripMargin)
       expectedTranslations.foreach { case (originalTSql, expectedDatabricksSql) =>
         s"correctly transpile: $originalTSql" in {
           originalTSql transpilesTo expectedDatabricksSql
