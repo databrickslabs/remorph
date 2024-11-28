806538898a4868d8638fd8349ae6d8cc2118a441
Valentin Kasas <valentin.kasas@xebia.com>
2024-11-28 12:10:09 +0100

    Simplify DealiasLCAs implementation using transform (#1245)

--- core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/rules/DealiasLCAs.scala
+++ core/src/main/scala/com/databricks/labs/remorph/parsers/snowflake/rules/DealiasLCAs.scala
@@ -2,169 +2,51 @@ package com.databricks.labs.remorph.parsers.snowflake.rules
 
 import com.databricks.labs.remorph.intermediate.{Expression, _}
 
-class Dealiaser(val aliases: Map[String, Expression], val isInColumnExpression: Boolean = false) {
-
-  def dealiasProject(project: Project): Project = {
-    val input = dealiasInput(project.input)
-    val columns = dealiasExpressions(project.columns)
-    project.makeCopy(Array(input, columns)).asInstanceOf[Project]
-  }
-
-  private def dealiasInput(input: LogicalPlan): LogicalPlan = {
-    input match {
-      case filter: Filter => dealiasFilter(filter)
-      case _: LogicalPlan => input
-    }
-  }
-
-  private def dealiasFilter(filter: Filter): Filter = {
-    val transformed = dealiasExpression(filter.condition)
-    filter.makeCopy(Array(filter.input, transformed)).asInstanceOf[Filter]
-  }
-
-  private def dealiasExpressions(expressions: Seq[Expression]): Seq[Expression] = {
-    expressions map { dealiasExpression }
-  }
-
-  private def dealiasExpression(expression: Expression): Expression = {
-    expression match {
-      case alias: Alias => dealiasAlias(alias)
-      case name: Name => dealiasName(name)
-      case id: Id => dealiasId(id)
-      case in: In => dealiasIn(in)
-      case unary: Unary => dealiasUnary(unary)
-      case binary: Binary => dealiasBinary(binary)
-      case func: CallFunction => dealiasCallFunction(func)
-      case window: Window => dealiasWindow(window)
-      case subquery: SubqueryExpression => dealiasSubqueryExpression(subquery)
-      case expression: Expression => expression
-    }
-  }
-
-  private def dealiasAlias(alias: Alias): Alias = {
-    val filtered = aliases - alias.name.id
-    val expression = new Dealiaser(filtered, true).dealiasExpression(alias.child)
-    alias.makeCopy(Array(expression.asInstanceOf[AnyRef], alias.name.asInstanceOf[AnyRef])).asInstanceOf[Alias]
-  }
-
-  private def dealiasName(name: Name): Expression = {
-    // don't dealias column names when dealing with column expressions
-    if (isInColumnExpression) {
-      name
-    } else {
-      val alias = aliases.find(p => p._1 == name.name)
-      if (alias.isEmpty) {
-        name
-      } else {
-        val filtered = aliases - name.name
-        new Dealiaser(filtered).dealiasExpression(alias.get._2)
-      }
-    }
-  }
-
-  private def dealiasId(id: Id): Expression = {
-    // don't dealias column names when dealing with column expressions
-    if (isInColumnExpression) {
-      id
-    } else {
-      val alias = aliases.find(p => p._1 == id.id)
-      if (alias.isEmpty) {
-        id
-      } else {
-        val filtered = aliases - id.id
-        new Dealiaser(filtered).dealiasExpression(alias.get._2)
-      }
-    }
-  }
-
-  private def dealiasIn(in: In): Expression = {
-    val left = dealiasExpression(in.left)
-    val other = dealiasExpressions(in.other)
-    in.makeCopy(Array(left, other))
-  }
-
-  private def dealiasUnary(unary: Unary): Expression = {
-    val child = dealiasExpression(unary.child)
-    unary.makeCopy(Array(child))
-  }
-
-  private def dealiasBinary(binary: Binary): Expression = {
-    val head = dealiasExpression(binary.children.head)
-    val last = dealiasExpression(binary.children.last)
-    binary.makeCopy(Array(head, last))
-  }
-
-  private def dealiasCallFunction(func: CallFunction): CallFunction = {
-    val args = func.arguments map { dealiasExpression }
-    func.makeCopy(Array(func.function_name, args)).asInstanceOf[CallFunction]
-  }
-
-  private def dealiasWindow(window: Window): Expression = {
-    if (isInColumnExpression) {
-      // window expressions need to be dealiased, so switch to dealiasing behavior
-      new Dealiaser(aliases).dealiasWindow(window)
-    } else {
-      val partition = dealiasExpressions(window.partition_spec)
-      val sort_order = dealiasSortOrders(window.sort_order)
-      window.makeCopy(
-        Array(
-          window.window_function.asInstanceOf[AnyRef],
-          partition.asInstanceOf[AnyRef],
-          sort_order.asInstanceOf[AnyRef],
-          window.frame_spec.asInstanceOf[AnyRef],
-          window.ignore_nulls.asInstanceOf[AnyRef]))
-    }
-  }
-
-  private def dealiasSubqueryExpression(subquery: SubqueryExpression): Expression = {
-    val plan = subquery.plan match {
-      case project: Project => {
-        val aliases = Dealiaser.collectAliases(project.columns)
-        if (aliases.isEmpty) {
-          project
-        } else {
-          val dealiaser = new Dealiaser(aliases)
-          dealiaser.dealiasProject(project)
-        }
-      }
-      case plan: LogicalPlan => plan // TODO log or raise error ?
-    }
-    subquery.makeCopy(Array(plan))
-  }
-
-  private def dealiasSortOrders(sort_order: Seq[SortOrder]): Seq[SortOrder] = {
-    sort_order map { sort => dealiasSortOrder(sort) }
-  }
-
-  private def dealiasSortOrder(sort_order: SortOrder): SortOrder = {
-    val transformed = dealiasExpression(sort_order.child)
-    sort_order.makeCopy(Array(transformed, sort_order.direction, sort_order.nullOrdering)).asInstanceOf[SortOrder]
-  }
-
-}
-
-object Dealiaser {
-
-  def collectAliases(columns: Seq[Expression]): Map[String, Expression] = {
-    columns.collect { case Alias(e, name) if !e.isInstanceOf[Literal] => name.id -> e }.toMap
-  }
-}
-
 class DealiasLCAs extends Rule[LogicalPlan] with IRHelpers {
 
-  override def apply(plan: LogicalPlan): LogicalPlan = {
+  override def apply(plan: LogicalPlan): LogicalPlan = transformPlan(plan)
+
+  private def transformPlan(plan: LogicalPlan): LogicalPlan =
     plan transform { case project: Project =>
       dealiasProject(project)
     }
-  }
 
   private def dealiasProject(project: Project): Project = {
-    val aliases = Dealiaser.collectAliases(project.columns)
-    if (aliases.isEmpty) {
-      project
-    } else {
-      val dealiaser = new Dealiaser(aliases)
-      dealiaser.dealiasProject(project)
+    // Go through the Project's select list, collecting aliases
+    // and dealias expressions using the aliases collected thus far
+    val (aliases, dealiasedExpressions) =
+      project.expressions.foldLeft((Map.empty[String, Expression], Seq.empty[Expression])) {
+        case ((aliases, exprs), a @ Alias(expr, name)) =>
+          // LCA aren't supported in WINDOW clauses, so we must dealias them
+          val dw = dealiasWindow(expr, aliases)
+          val accumulatedExprs = exprs :+ CurrentOrigin.withOrigin(a.origin)(Alias(dw, name))
+          // An aliased expression may refer to a previous LCA, so before storing the mapping,
+          // we must dealias the expression to ensure that mapped expressions are fully dealiased.
+          val newFoundAlias = dealiasExpression(dw, aliases)
+          val updatedAliases = aliases + (name.id -> newFoundAlias)
+          (updatedAliases, accumulatedExprs)
+        case ((aliases, exprs), e) => (aliases, exprs :+ dealiasWindow(e, aliases))
+      }
+
+    val dealiasedInput = project.input transformDown { case f @ Filter(in, cond) =>
+      CurrentOrigin.withOrigin(f.origin)(Filter(in, dealiasExpression(cond, aliases)))
+    }
+
+    CurrentOrigin.withOrigin(project.origin)(Project(dealiasedInput, dealiasedExpressions))
+  }
+
+  private def dealiasWindow(expr: Expression, aliases: Map[String, Expression]): Expression = {
+    expr transformDown { case w: Window =>
+      w.mapChildren(dealiasExpression(_, aliases))
+    }
+  }
+
+  private def dealiasExpression(expr: Expression, aliases: Map[String, Expression]): Expression = {
+    expr transformUp {
+      case id: Id => aliases.getOrElse(id.id, id)
+      case n: Name => aliases.getOrElse(n.name, n)
+      case e: Exists => CurrentOrigin.withOrigin(e.origin)(Exists(transformPlan(e.relation)))
+      case s: ScalarSubquery => CurrentOrigin.withOrigin(s.origin)(ScalarSubquery(transformPlan(s.plan)))
     }
   }
 
