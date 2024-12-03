38fc3b3c0c96a2c654d2af20a42381ea2ff6e70c
Andrew Snare <andrew.snare@databricks.com>
2024-12-03 18:52:43 +0100

    Add equivalent TSQL functional tests to verify the same functionality works with TSQL.

---
+++ tests/resources/functional/tsql/cte/cte_set_operation_precedence.sql
@@ -0,0 +1,17 @@
+--
+-- CTEs are visible to all the SELECT queries within a subsequent sequence of set operations.
+--
+
+-- tsql sql:
+WITH a AS (SELECT 1, 2, 3)
+
+SELECT 4, 5, 6
+UNION
+SELECT * FROM a;
+
+-- databricks sql:
+WITH a AS (SELECT 1, 2, 3)
+
+(SELECT 4, 5, 6)
+UNION
+(SELECT * FROM a);
