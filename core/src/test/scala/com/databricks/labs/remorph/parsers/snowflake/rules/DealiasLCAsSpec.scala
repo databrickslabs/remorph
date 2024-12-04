7115068f57d65f2dd8698fb586debbb6ad6b61ac
Valentin Kasas <valentin.kasas@xebia.com>
2024-12-04 12:46:29 +0100

    Add a few tests for DealiasLCAs

---
+++ core/src/test/scala/com/databricks/labs/remorph/parsers/snowflake/rules/DealiasLCAsSpec.scala
@@ -0,0 +1,39 @@
+package com.databricks.labs.remorph.parsers.snowflake.rules
+
+import com.databricks.labs.remorph.{intermediate => ir}
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class DealiasLCAsSpec extends AnyWordSpec with Matchers {
+
+  val dealiaser = new DealiasLCAs
+
+  "DealiasLCAs" should {
+
+    "dealias a LCA pointing to an idempotent function" in {
+      val plan =
+        ir.Project(
+          ir.Filter(ir.NoTable(), ir.GreaterThan(ir.Id("abs"), ir.Literal(42))),
+          Seq(ir.Alias(ir.Abs(ir.Literal(-42)), ir.Id("abs"))))
+
+      dealiaser.transformPlan(plan) shouldBe
+        ir.Project(
+          ir.Filter(ir.NoTable(), ir.GreaterThan(ir.Abs(ir.Literal(-42)), ir.Literal(42))),
+          Seq(ir.Alias(ir.Abs(ir.Literal(-42)), ir.Id("abs"))))
+
+    }
+
+    "throw an exception when applied to an alias pointing to a non-idempotent function" in {
+      val plan =
+        ir.Project(
+          ir.Filter(ir.NoTable(), ir.GreaterThan(ir.Id("rn"), ir.Literal(42))),
+          Seq(ir.Alias(ir.Rand(None), ir.Id("rn"))))
+
+      val exception = intercept[Exception](dealiaser.transformPlan(plan))
+      exception shouldBe a[IllegalArgumentException]
+      exception.getMessage should include("RAND")
+    }
+
+  }
+}
