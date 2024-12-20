1022f2f39cb8e0ac2b8342afe54de1fcb33c3780
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-12-20 18:20:48 +0100

    Simplify transpiler api (#1361)
    Our current `transpile` APIs expose many types of return values, making
    it difficult to condense the logic (as required by LSP)
    
    This PR fixes the issue by providing a common base type for all
    transpile errors.
    It also clarifies related structures by renaming them or their fields.
    
    Progresses #1299
    
    Requires #1315

--- src/databricks/labs/remorph/transpiler/transpile_engine.py
+++ src/databricks/labs/remorph/transpiler/transpile_engine.py
@@ -3,8 +3,8 @@ import abc
 from collections.abc import Iterable
 from pathlib import Path
 
-from databricks.labs.remorph.config import TranspilationResult
-from databricks.labs.remorph.transpiler.transpile_status import ParserError, ValidationError
+from databricks.labs.remorph.config import TranspileResult
+from databricks.labs.remorph.transpiler.transpile_status import ValidationError
 
 
 class TranspileEngine(abc.ABC):
@@ -33,15 +33,17 @@ class TranspileEngine(abc.ABC):
 
     @abc.abstractmethod
     def transpile(
-        self, source_dialect: str, target_dialect: str, source_code: str, file_path: Path, error_list: list[ParserError]
-    ) -> TranspilationResult: ...
-
-    @abc.abstractmethod
-    def check_for_unsupported_lca(self, source_dialect, source_code, file_path) -> ValidationError | None: ...
+        self, source_dialect: str, target_dialect: str, source_code: str, file_path: Path
+    ) -> TranspileResult: ...
 
     @property
     @abc.abstractmethod
     def supported_dialects(self) -> list[str]: ...
+
+    @abc.abstractmethod
+    def check_for_unsupported_lca(
+        self, source_dialect: str, source_code: str, file_path: Path
+    ) -> ValidationError | None: ...
 
     def check_source_dialect(self, source_dialect: str) -> None:
         if source_dialect not in self.supported_dialects:
