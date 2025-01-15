7fc59ffd88c21d03efdf54de29ce589af8e84154
sundar.shankar <sundar.shankar@databricks.com>
2025-01-15 19:45:31 +0530

    Moved the Abstract class to private

--- src/databricks/labs/remorph/connections/credential_manager.py
+++ src/databricks/labs/remorph/connections/credential_manager.py
@@ -1,24 +1,21 @@
 from pathlib import Path
 import yaml
 
-
 class Credentials:
-
-    def __init__(self, product_info):
+    def __init__(self, product_info: str) -> None:
         self._product_info = product_info
-        self._credentials = self._load_credentials(self._get_local_version_file_path())
+        self._credentials: dict[str, Any] = self._load_credentials(self._get_local_version_file_path())
 
-    def _get_local_version_file_path(self):
+    def _get_local_version_file_path(self) -> Path:
         user_home = f"{Path(__file__).home()}"
         return Path(f"{user_home}/.databricks/labs/{self._product_info.product_name()}/credentials.yml")
 
-    def _load_credentials(self, file_path):
+    def _load_credentials(self, file_path: Path) -> dict[str, str]:
         with open(file_path, encoding="utf-8") as f:
             return yaml.safe_load(f)
 
-    def get(self, source):
+    def get(self, source: str) -> dict[str, str]:
         if source in self._credentials:
             return self._credentials[source]
         else:
-            raise KeyError(f"source system: {source} credentials not found not in credentials: {key}")
-
+            raise KeyError(f"source system: {source} credentials not found not in credentials: {source}")
--- src/databricks/labs/remorph/connections/database_manager.py
+++ src/databricks/labs/remorph/connections/database_manager.py
@@ -3,26 +3,26 @@ from pathlib import Path
 import yaml
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from typing import Any, Dict
+from typing import Any
 
-class ISourceSystemConnector(ABC):
+class _ISourceSystemConnector(ABC):
     @abstractmethod
-    def connect(self) -> None:
+    def connect(self) -> Engine:
         pass
 
     @abstractmethod
-    def execute_query(self, query: str) -> list[Dict[str, Any]]:
+    def execute_query(self, query: str) -> list[dict[str, Any]]:
         pass
 
-class BaseConnector(ISourceSystemConnector):
-    def __init__(self, config: Dict[str, Any]):
+class _BaseConnector(_ISourceSystemConnector):
+    def __init__(self, config: dict[str, Any]):
         self.config = config
         self.engine = None
 
-    def connect(self) -> None:
+    def connect(self) -> Engine:
         raise NotImplementedError("Subclasses should implement this method")
 
-    def execute_query(self, query: str) -> list[Dict[str, Any]]:
+    def execute_query(self, query: str) -> list[dict[str, Any]]:
         if not self.engine:
             raise ConnectionError("Not connected to the database.")
         Session = sessionmaker(bind=self.engine)
@@ -33,25 +33,26 @@ class BaseConnector(ISourceSystemConnector):
         finally:
             session.close()
 
-class SnowflakeConnector(BaseConnector):
-    def connect(self) -> None:
+class SnowflakeConnector(_BaseConnector):
+    def connect(self) -> Engine:
         connection_string = (
             f"snowflake://{self.config['user']}:{self.config['password']}@{self.config['account']}/"
             f"{self.config['database']}/{self.config['schema']}?warehouse={self.config['warehouse']}&role={self.config['role']}"
         )
         self.engine = create_engine(connection_string)
 
-class MSSQLConnector(BaseConnector):
-    def connect(self) -> None:
+class MSSQLConnector(_BaseConnector):
+    def connect(self) -> Engine:
         connection_string = (
             f"mssql+pyodbc://{self.config['user']}:{self.config['password']}@{self.config['server']}/"
             f"{self.config['database']}?driver={self.config['driver']}"
         )
         self.engine = create_engine(connection_string)
 
+#TODO: Move this application context
 class SourceSystemConnectorFactory:
     @staticmethod
-    def create_connector(db_type: str, config: Dict[str, Any]) -> ISourceSystemConnector:
+    def create_connector(db_type: str, config: dict[str, str]) -> ISourceSystemConnector:
         if db_type == "snowflake":
             return SnowflakeConnector(config)
         elif db_type == "mssql":
