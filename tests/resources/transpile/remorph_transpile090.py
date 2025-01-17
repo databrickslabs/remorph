e18dd265f65cdb5d3eb3c37b76dc41c4ffad5429
Sriram Mohanty <sriram.mohanty@databricks.com>
2025-01-17 15:45:35 +0530

    python demo example for transpilation added

--- tests/resources/transpile/remorph_transpile090.py
+++ tests/resources/transpile/remorph_transpile090.py
@@ -1,7 +1,6 @@
-#Installs the latest released version of the databricks-labs-remorph library.
-#%pip install databricks-labs-remorph
-
-dbutils.library.restartPython()
+# Installs the latest released version of the databricks-labs-remorph library.
+# %pip install databricks-labs-remorph
+# dbutils.library.restartPython()
 
 # Import necessary classes from the databricks SDK and remorph library
 from databricks.sdk import WorkspaceClient
@@ -11,18 +10,24 @@ from databricks.labs.remorph.transpiler import execute
 # This cell sets up the necessary configuration parameters for the remorph transpilation process.
 # Please provide the catalog_name,schema_name,source_dialect,input_source,output_folder.
 
-catalog_name ="remorph" # provide catalog name for remorph
-schema_name="default"            # provide schema name for remorph
-source_dialect="snowflake"        # Provide source dialect
-input_source = "/Workspace/Users/sriram.mohanty@databricks.com/snowflake/input_sql/"   #Provide input folder path which contains the DDLs
-output_folder = "/Workspace/Users/sriram.mohanty@databricks.com/snowflake/converted_sql/" #Output folder path
-skip_validation = True #Skip validation : generated queries are validated syntactically.To validate a query the table definations should be present.Hence for all the DDLs skip_validation will be always false.
+catalog_name = "remorph"  # provide catalog name for remorph
+schema_name = "default"  # provide schema name for remorph
+source_dialect = "snowflake"  # Provide source dialect
+input_source = "/Workspace/Users/sriram.mohanty@databricks.com/snowflake/input_sql/"  # Provide input folder path which contains the DDLs
+output_folder = "/Workspace/Users/sriram.mohanty@databricks.com/snowflake/converted_sql/"  # Output folder path
+skip_validation = True  # Skip validation : generated queries are validated syntactically.To validate a query the table definations should be present.Hence for all the DDLs skip_validation will be always false.
 
 # Initialize the WorkspaceClient with the specified product and version and TranspileConfig object with the provided configuration parameters.
-wsclient = WorkspaceClient(product="remorph",product_version="0.9.0")
-mrophconfig = MorphConfig(source=source_dialect,input_sql=input_source,output_folder=output_folder,catalog_name=catalog_name,schema_name=schema_name)
+wsclient = WorkspaceClient(product="remorph", product_version="0.9.0")
+mrophconfig = MorphConfig(
+    source=source_dialect,
+    input_sql=input_source,
+    output_folder=output_folder,
+    catalog_name=catalog_name,
+    schema_name=schema_name,
+)
 
 
-status = execute.morph(workspace_client=wsclient,config=mrophconfig)
-#display(status)
-#Please check output_folder for the converted DDLs
+status = execute.morph(workspace_client=wsclient, config=mrophconfig)
+# display(status)
+# Please check output_folder for the converted DDLs
