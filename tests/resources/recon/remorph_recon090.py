31be44da600984f39878f8f9f963577265dbb15b
Sriram Mohanty <sriram.mohanty@databricks.com>
2025-01-23 22:13:49 +0530

    python demo example for reconciliation added

--- tests/resources/recon/remorph_recon090.py
+++ tests/resources/recon/remorph_recon090.py
@@ -44,7 +44,7 @@ table_recon = TableRecon(
             source_name="source_employee_table",  # Provide the source table name
             target_name="target_employee_table",  # Provide the target table name
             column_mapping=[
-                ColumnMapping(source_name="emp_id", target_name="employee_id"),
+                ColumnMapping(source_name="emp_id", target_name="employee_id"), # Provide the source and target column name if they have different names
                 ColumnMapping(source_name="salary", target_name="sal"),
             ],
             join_columns=["emp_id"],  # for recon type all join_columns is mandatory
@@ -84,10 +84,6 @@ table_recon_agg = TableRecon(
                 ),  # Provide the column name and aggregation type https://github.com/databrickslabs/remorph/tree/main/docs/recon_configurations#supported-aggregate-functions
                 Aggregate(agg_columns=["salary"], type="min"),
                 Aggregate(agg_columns=["salary"], type="max"),
-            ],
-            column_mapping=[
-                ColumnMapping(source_name="emp_id", target_name="emp_id"),
-                ColumnMapping(source_name="salary", target_name="salary"),
             ],
             join_columns=["emp_id"],  # for recon type all join_columns is mandatory
         ),
