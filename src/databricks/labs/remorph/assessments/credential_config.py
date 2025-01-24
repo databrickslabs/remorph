30470e1d9dd655e5ee36b694b721740853416011
sundar.shankar <sundar.shankar@databricks.com>
2025-01-24 21:52:57 +0530

    added credential_config

--- src/databricks/labs/remorph/assessments/credential_config.py
+++ src/databricks/labs/remorph/assessments/credential_config.py
@@ -1,36 +1,31 @@
-from dataclasses import dataclass, field
+import yaml
 from typing import Optional
 
-@dataclass
-class SnowflakeConfig:
-    account: Optional[str] = None
-    connect_retries: Optional[int] = None
-    connect_timeout: Optional[int] = None
-    host: Optional[str] = None
-    insecure_mode: Optional[bool] = None
-    oauth_client_id: Optional[str] = None
-    oauth_client_secret: Optional[str] = None
-    password: Optional[str] = None
-    port: Optional[int] = None
-    private_key: Optional[str] = None
-    private_key_passphrase: Optional[str] = None
-    private_key_path: Optional[str] = None
-    role: Optional[str] = None
-    token: Optional[str] = None
-    user: Optional[str] = None
-    warehouse: Optional[str] = None
 
-@dataclass
-class MSSQLConfig:
-    database: Optional[str] = None
-    driver: Optional[str] = None
-    server: Optional[str] = None
-    port: Optional[int] = None
-    user: Optional[str] = None
-    password: Optional[str] = None
+class CredentialConfig:
+    def __init__(self, secret_vault_type: str, secret_vault_name: Optional[str], **kwargs):
+        self.secret_vault_type = secret_vault_type
+        self.secret_vault_name = secret_vault_name
+        self.additional_properties = kwargs
 
-@dataclass
-class CredentialsConfig:
-    secret_vault_type: Optional[str] = None
-    secret_vault_name: Optional[str] = None
+    def __repr__(self):
+        return (
+            f"Config(secret_vault_type={self.secret_vault_type}, "
+            f"secret_vault_name={self.secret_vault_name}, "
+            f"additional_properties={self.additional_properties})"
+        )
 
+    @classmethod
+    def load_credentials(cls, file_path: str) -> list['CredentialConfig']:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            credentials_data = yaml.safe_load(file)
+
+        credential_configs = []
+        for key, value in credentials_data.items():
+            if isinstance(value, dict):
+                config = cls(secret_vault_type=credentials_data.get('secret_vault_type', 'local'),
+                             secret_vault_name=credentials_data.get('secret_vault_name', None),
+                             **value)
+                credential_configs.append(config)
+
+        return credential_configs
