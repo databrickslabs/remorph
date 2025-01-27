e3c01a98183a2780cc4b7325b0ded94f6127e399
sundar.shankar <sundar.shankar@databricks.com>
2025-01-27 10:04:47 +0530

    refactor for integration tests.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,23 @@
+import logging
 import pytest
 from pyspark.sql import SparkSession
 
+from databricks.labs.remorph.__about__ import __version__
+
+logging.getLogger("tests").setLevel("DEBUG")
+logging.getLogger("databricks.labs.remorph").setLevel("DEBUG")
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def debug_env_name():
+    return "ucws"
+
+
+@pytest.fixture
+def product_info():
+    return "remorph", __version__
 
 @pytest.fixture(scope="session")
 def mock_spark() -> SparkSession:
