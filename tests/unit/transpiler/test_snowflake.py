a3077085035784758cb6caadab70e56c2f395413
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-01-27 11:59:27 +0100

    update test following sqlglot upgrade fix tests naming issue

--- tests/unit/transpiler/test_snowflake.py
+++ tests/unit/transpiler/test_snowflake.py
@@ -1,6 +1,20 @@
-"""
-    Test Cases to validate source Snowflake dialect
-"""
+from pathlib import Path
+
+import pytest
+
+from ..conftest import FunctionalTestFile, get_functional_test_files_from_directory
+
+path = Path(__file__).parent / Path('../../resources/functional/snowflake/')
+functional_tests = get_functional_test_files_from_directory(path, "snowflake", "databricks", False)
+test_names = [f.test_name for f in functional_tests]
+
+
+@pytest.mark.parametrize("sample", functional_tests, ids=test_names)
+def test_snowflake(dialect_context, sample: FunctionalTestFile):
+    validate_source_transpile, _ = dialect_context
+    validate_source_transpile(databricks_sql=sample.databricks_sql, source={"snowflake": sample.source}, pretty=True)
+
+
 
 
 def test_parse_parameter(dialect_context):
