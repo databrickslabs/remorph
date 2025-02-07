3468c34ad01042b93f496c1e2671565be3c029d9
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2025-02-07 06:48:26 +0000

    added user agent for sub command (#1441)
    This introduces the user agent with additional keys for tracking

--- src/databricks/labs/remorph/cli.py
+++ src/databricks/labs/remorph/cli.py
@@ -15,6 +15,7 @@ from databricks.labs.remorph.lineage import lineage_generator
 from databricks.labs.remorph.transpiler.execute import transpile as do_transpile
 from databricks.labs.remorph.reconcile.execute import RECONCILE_OPERATION_NAME, AGG_RECONCILE_OPERATION_NAME
 from databricks.labs.remorph.jvmproxy import proxy_command
+from databricks.sdk.core import with_user_agent_extra
 
 from databricks.sdk import WorkspaceClient
 
@@ -75,6 +76,7 @@ def transpile(
     schema_name: str,
 ):
     """Transpiles source dialect to databricks dialect"""
+    with_user_agent_extra("cmd", "execute-transpile")
     ctx = ApplicationContext(w)
     logger.debug(f"User: {ctx.current_user}")
     default_config = ctx.transpile_config
@@ -138,6 +140,7 @@ def _override_workspace_client_config(ctx: ApplicationContext, overrides: dict[s
 @remorph.command
 def reconcile(w: WorkspaceClient):
     """[EXPERIMENTAL] Reconciles source to Databricks datasets"""
+    with_user_agent_extra("cmd", "execute-reconcile")
     ctx = ApplicationContext(w)
     logger.debug(f"User: {ctx.current_user}")
     recon_runner = ReconcileRunner(
@@ -152,6 +155,7 @@ def reconcile(w: WorkspaceClient):
 @remorph.command
 def aggregates_reconcile(w: WorkspaceClient):
     """[EXPERIMENTAL] Reconciles Aggregated source to Databricks datasets"""
+    with_user_agent_extra("cmd", "execute-aggregates-reconcile")
     ctx = ApplicationContext(w)
     logger.debug(f"User: {ctx.current_user}")
     recon_runner = ReconcileRunner(
@@ -200,6 +204,7 @@ def install_assessment():
 @remorph.command()
 def install_transpile(w: WorkspaceClient):
     """Install the Remorph Transpile package"""
+    with_user_agent_extra("cmd", "install-transpile")
     installer = _installer(w)
     installer.run(module="transpile")
 
@@ -207,6 +212,7 @@ def install_transpile(w: WorkspaceClient):
 @remorph.command(is_unauthenticated=False)
 def install_reconcile(w: WorkspaceClient):
     """Install the Remorph Reconcile package"""
+    with_user_agent_extra("cmd", "install-reconcile")
     installer = _installer(w)
     installer.run(module="reconcile")
 
