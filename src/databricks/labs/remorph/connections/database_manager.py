a4723fb2185544973131e3ecab8df2fafd643b96
sundar.shankar <sundar.shankar@databricks.com>
2025-02-18 13:53:41 +0530

    fmt fixes

--- src/databricks/labs/remorph/connections/database_manager.py
+++ src/databricks/labs/remorph/connections/database_manager.py
@@ -11,6 +11,7 @@ from sqlalchemy.exc import OperationalError
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+
 class DatabaseConnector(ABC):
     @abstractmethod
     def _connect(self) -> Engine:
