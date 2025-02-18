09a0bf3603e45ed097059b24f02e9923fec0f736
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2025-02-18 08:08:13 +0000

    Initial Spec For Credential Management and SQLAlchemy Database Connectors (#1420)
    Initial Spec For Credential Management and SQLAlchemy Database
    Connectors

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,37 @@
+import os
+import logging
 import pytest
 from pyspark.sql import SparkSession
+
+from databricks.labs.remorph.__about__ import __version__
+
+logging.getLogger("tests").setLevel("DEBUG")
+logging.getLogger("databricks.labs.remorph").setLevel("DEBUG")
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def debug_env_name():
+    return "ucws"
+
+
+@pytest.fixture
+def product_info():
+    return "remorph", __version__
+
+
+def pytest_collection_modifyitems(config, items):
+    if os.getenv('TEST_ENV') == 'ACCEPTANCE':
+        selected_items = []
+        deselected_items = []
+        for item in items:
+            if 'tests/integration/connections' in str(item.fspath):
+                selected_items.append(item)
+            else:
+                deselected_items.append(item)
+        items[:] = selected_items
+        config.hook.pytest_deselected(items=deselected_items)
 
 
 @pytest.fixture(scope="session")
