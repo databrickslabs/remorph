8547e7608f782b4c57027061af232e6d48610fd1
Sriram Mohanty <sriram.mohanty@databricks.com>
2025-02-24 17:13:22 +0530

    table definition code added

--- src/databricks/labs/remorph/discovery/table_definition.py
+++ src/databricks/labs/remorph/discovery/table_definition.py
@@ -5,10 +5,18 @@ from databricks.labs.remorph.discovery.table import TableDefinition
 
 
 class TableDefinitionService(ABC):
+
+    def __init__(self, connection: Any):
+        self.connection = connection
+
     @abstractmethod
-    def _get_table_definition(self, catalog_name: str, connection: Any) -> Iterable[TableDefinition]:
+    def _get_table_definition(self, catalog_name: str) -> Iterable[TableDefinition]:
         pass
 
     @abstractmethod
     def _get_table_definition_query(self, catalog_name: str) -> str:
+        pass
+
+    @abstractmethod
+    def _get_all_catalog(self) -> Iterable[str]:
         pass
