94df54fdb7350c9c6623bf8f1816cf1b3ede8bc4
SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>
2025-03-06 11:29:15 +0000

    Pipeline Executor for Profiling (#1453)
    Introduces a config driven ETL framework, currently orchestrated using
    SQL files as Pipeline.

---
+++ src/databricks/labs/remorph/assessments/pipeline.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import logging
+import yaml
+import duckdb
+
+from databricks.labs.remorph.assessments.profiler_config import PipelineConfig, Step
+from databricks.labs.remorph.connections.database_manager import DatabaseManager
+
+logger = logging.getLogger(__name__)
+logger.setLevel("INFO")
+
+DB_NAME = "profiler_extract.db"
+
+
+class PipelineClass:
+    def __init__(self, config: PipelineConfig, executor: DatabaseManager):
+        self.config = config
+        self.executor = executor
+        self.db_path_prefix = Path(config.extract_folder)
+
+    def execute(self):
+        logging.info(f"Pipeline initialized with config: {self.config.name}, version: {self.config.version}")
+        for step in self.config.steps:
+            if step.flag == "active":
+                logging.debug(f"Executing step: {step.name}")
+                self._execute_step(step)
+        logging.info("Pipeline execution completed")
+
+    def _execute_step(self, step: Step):
+        logging.debug(f"Reading query from file: {step.extract_query}")
+        with open(step.extract_query, 'r', encoding='utf-8') as file:
+            query = file.read()
+
+        # Execute the query using the database manager
+        logging.info(f"Executing query: {query}")
+        result = self.executor.execute_query(query)
+
+        # Save the result to duckdb
+        self._save_to_db(result, step.name, str(step.mode))
+
+    def _save_to_db(self, result, step_name: str, mode: str, batch_size: int = 1000):
+        self._create_dir(self.db_path_prefix)
+        conn = duckdb.connect(str(self.db_path_prefix) + '/' + DB_NAME)
+        columns = result.keys()
+        # TODO: Add support for figuring out data types from SQLALCHEMY result object result.cursor.description is not reliable
+        schema = ' STRING, '.join(columns) + ' STRING'
+
+        # Handle write modes
+        if mode == 'overwrite':
+            conn.execute(f"CREATE OR REPLACE TABLE {step_name} ({schema})")
+        elif mode == 'append' and step_name not in conn.get_table_names(""):
+            conn.execute(f"CREATE TABLE {step_name} ({schema})")
+
+        # Batch insert using prepared statements
+        placeholders = ', '.join(['?' for _ in columns])
+        insert_query = f"INSERT INTO {step_name} VALUES ({placeholders})"
+
+        # Fetch and insert rows in batches
+        while True:
+            rows = result.fetchmany(batch_size)
+            if not rows:
+                break
+            conn.executemany(insert_query, rows)
+
+        conn.close()
+
+    @staticmethod
+    def _create_dir(dir_path: Path):
+        if not Path(dir_path).exists():
+            dir_path.mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def load_config_from_yaml(file_path: str) -> PipelineConfig:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = yaml.safe_load(file)
+        steps = [Step(**step) for step in data['steps']]
+        return PipelineConfig(
+            name=data['name'], version=data['version'], extract_folder=data['extract_folder'], steps=steps
+        )
