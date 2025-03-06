449dd2fe3038992a918c1f4b107d68ddcbd51030
Sriram Mohanty <69749553+sriram251-code@users.noreply.github.com>
2025-03-06 20:23:26 +0530

    Introduce table definition (#1450)
    This pull request introduces significant enhancements to the table
    discovery module by adding new data structures and services for geting
    table definitions. The changes include the creation of data classes for
    table and field information, the introduction of an abstract service for
    fetching table definitions, and a concrete implementation for T-SQL
    table definitions.
    
    ---------
    
    Co-authored-by: sundar.shankar <sundar.shankar@databricks.com>
    Co-authored-by: SundarShankar89 <72757199+sundarshankar89@users.noreply.github.com>

---
+++ tests/integration/discovery/test_tsql_table_definition.py
@@ -0,0 +1,22 @@
+import pytest
+
+from databricks.labs.remorph.discovery.tsql_table_definition import TsqlTableDefinitionService
+from ..connections.helpers import get_db_manager
+
+
+@pytest.fixture()
+def extractor(mock_credentials):
+    return get_db_manager("remorph", "mssql")
+
+
+def test_tsql_get_catalog(extractor):
+    tss = TsqlTableDefinitionService(extractor)
+    catalogs = list(tss.get_all_catalog())
+    assert catalogs is not None
+    assert len(catalogs) > 0
+
+
+def test_tsql_get_table_definition(extractor):
+    tss = TsqlTableDefinitionService(extractor)
+    table_def = tss.get_table_definition("labs_azure_sandbox_remorph")
+    assert table_def is not None
