32574b3c2915c4f713b149afc3e35de09afa37d4
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-03-11 13:54:50 +0100

    improve lsp server isolation

--- src/databricks/labs/remorph/transpiler/lsp/lsp_engine.py
+++ src/databricks/labs/remorph/transpiler/lsp/lsp_engine.py
@@ -380,6 +380,11 @@ class LSPEngine(TranspileEngine):
         env = deepcopy(os.environ)
         for name, value in self._config.remorph.env_vars.items():
             env[name] = value
+        # ensure modules are searched locally before being searched in remorph
+        if "PYTHONPATH" in env.keys():
+            env["PYTHONPATH"] = str(self._workdir) + os.pathsep + env["PYTHONPATH"]
+        else:
+            env["PYTHONPATH"] = str(self._workdir)
         args = self._config.remorph.command_line[1:]
         logger.debug(f"Starting LSP engine: {executable} {args} (cwd={os.getcwd()})")
         await self._client.start_io(executable, env=env, *args)
