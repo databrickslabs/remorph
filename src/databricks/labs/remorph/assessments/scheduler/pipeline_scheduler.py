5893ca9ef5ca512b88e6770f0b181b532392d19e
Will Girten <47335283+goodwillpunning@users.noreply.github.com>
2025-04-26 17:34:52 -0400

    Update src/databricks/labs/remorph/assessments/scheduler/pipeline_scheduler.py
    Co-authored-by: Andrew Snare <asnare@users.noreply.github.com>

--- src/databricks/labs/remorph/assessments/scheduler/pipeline_scheduler.py
+++ src/databricks/labs/remorph/assessments/scheduler/pipeline_scheduler.py
@@ -17,7 +17,7 @@ def get_hours_since_last_run(last_run_datetime: datetime) -> int:
 
 
 def get_days_since_last_run(last_run_datetime: datetime) -> int:
-    elapsed_time = datetime.now() - last_run_datetime
+    elapsed_time = dt.datetime.now(dt.timezone.utc) - last_run_datetime
     elapsed_seconds = elapsed_time.total_seconds()
     return int(divmod(elapsed_seconds, 86400)[0])
 
