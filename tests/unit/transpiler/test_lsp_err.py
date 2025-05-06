856a6262aaa5ab383a2392722c805ad8c8bd637f
Andrew Snare <andrew.snare@databricks.com>
2025-05-06 19:25:29 +0200

    Use pytest async facilities instead of starting a new task.

--- tests/unit/transpiler/test_lsp_err.py
+++ tests/unit/transpiler/test_lsp_err.py
@@ -1,6 +1,7 @@
-import asyncio
 import logging
 from pathlib import Path
+
+import pytest
 
 from databricks.labs.remorph.config import TranspileConfig
 from databricks.labs.remorph.transpiler.lsp.lsp_engine import LSPEngine
@@ -17,11 +18,12 @@ async def run_lsp_server():
     await lsp_engine.initialize(config)
 
 
-def test_stderr_captured_as_logs(caplog) -> None:
+@pytest.mark.asyncio
+async def test_stderr_captured_as_logs(caplog) -> None:
     # The LSP engine logs a message to stderr when it starts. We can verify that stderr is being captured and logged
     # by looking for the log entry that matches the stderr message.
     with caplog.at_level(logging.INFO):
-        asyncio.run(run_lsp_server())
+        await run_lsp_server()
 
     expected = (LSPEngine.__module__, logging.INFO, "Running LSP Test Server\u2026")
     assert expected in caplog.record_tuples
