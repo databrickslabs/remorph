e003ef4e9f61f11c1cbf3c3540eb9fe4bc09fb42
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-05-16 09:17:24 +0200

    formatting

--- src/databricks/labs/remorph/helpers/telemetry_utils.py
+++ src/databricks/labs/remorph/helpers/telemetry_utils.py
@@ -1,6 +1,5 @@
-import re
-
 from databricks.sdk.useragent import alphanum_pattern, semver_pattern
+
 
 def make_alphanum_or_semver(value: str) -> str:
     if alphanum_pattern.match(value) or semver_pattern.match(value):
