b96a93f303b9b1740d2d066cd507f1382b3b8f27
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-05-23 11:56:29 +0200

    fix failing tests

--- tests/unit/reconcile/test_recon_config.py
+++ tests/unit/reconcile/test_recon_config.py
@@ -1,29 +1,32 @@
+from databricks.labs.remorph.reconcile.recon_config import Layer
+
+
 def test_table_without_join_column(table_conf):
     table_conf = table_conf()
-    assert table_conf.get_join_columns("source") is None
-    assert table_conf.get_drop_columns("source") == set()
-    assert table_conf.get_partition_column("source") == set()
-    assert table_conf.get_partition_column("target") == set()
-    assert table_conf.get_filter("source") is None
-    assert table_conf.get_filter("target") is None
-    assert table_conf.get_threshold_columns("source") == set()
+    assert table_conf.get_join_columns(Layer.SOURCE) is None
+    assert table_conf.get_drop_columns(Layer.SOURCE) == set()
+    assert table_conf.get_partition_column(Layer.SOURCE) == set()
+    assert table_conf.get_partition_column(Layer.TARGET) == set()
+    assert table_conf.get_filter(Layer.SOURCE) is None
+    assert table_conf.get_filter(Layer.TARGET) is None
+    assert table_conf.get_threshold_columns(Layer.SOURCE) == set()
 
 
 def test_table_with_all_options(table_conf_with_opts):
     ## layer == source
 
-    assert table_conf_with_opts.get_join_columns("source") == {"s_nationkey", "s_suppkey"}
-    assert table_conf_with_opts.get_drop_columns("source") == {"s_comment"}
-    assert table_conf_with_opts.get_partition_column("source") == {"s_nationkey"}
-    assert table_conf_with_opts.get_partition_column("target") == set()
-    assert table_conf_with_opts.get_filter("source") == "s_name='t' and s_address='a'"
-    assert table_conf_with_opts.get_threshold_columns("source") == {"s_acctbal"}
+    assert table_conf_with_opts.get_join_columns(Layer.SOURCE) == {"s_nationkey", "s_suppkey"}
+    assert table_conf_with_opts.get_drop_columns(Layer.SOURCE) == {"s_comment"}
+    assert table_conf_with_opts.get_partition_column(Layer.SOURCE) == {"s_nationkey"}
+    assert table_conf_with_opts.get_partition_column(Layer.TARGET) == set()
+    assert table_conf_with_opts.get_filter(Layer.SOURCE) == "s_name='t' and s_address='a'"
+    assert table_conf_with_opts.get_threshold_columns(Layer.SOURCE) == {"s_acctbal"}
 
     ## layer == target
     assert table_conf_with_opts.get_join_columns("target") == {"s_nationkey_t", "s_suppkey_t"}
     assert table_conf_with_opts.get_drop_columns("target") == {"s_comment_t"}
-    assert table_conf_with_opts.get_partition_column("target") == set()
-    assert table_conf_with_opts.get_filter("target") == "s_name='t' and s_address_t='a'"
+    assert table_conf_with_opts.get_partition_column(Layer.TARGET) == set()
+    assert table_conf_with_opts.get_filter(Layer.TARGET) == "s_name='t' and s_address_t='a'"
     assert table_conf_with_opts.get_threshold_columns("target") == {"s_acctbal_t"}
 
 
