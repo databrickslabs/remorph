adead4285093f02b09bef9ab9c540f9886704cb9
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-05-28 08:39:18 +0200

    move failing test under acceptance

--- tests/integration/install/test_install.py
+++ tests/integration/install/test_install.py
@@ -133,45 +133,6 @@ def check_valid_version(version: str):
             assert False, f"{version} does not look like a valid semver"
 
 
-class _WorkspaceInstaller(WorkspaceInstaller):
-
-    def save_config(self, config: TranspileConfig | ReconcileConfig):
-        self._save_config(config)
-
-
-def test_stores_and_fetches_config(ws):
-    prompts = MockPrompts(
-        {
-            r"Open .* in the browser?": "no",
-        }
-    )
-    context = ApplicationContext(ws)
-    installer = _WorkspaceInstaller(
-        context.workspace_client,
-        prompts,
-        context.installation,
-        context.install_state,
-        context.product_info,
-        context.resource_configurator,
-        context.workspace_installation,
-    )
-    config = TranspileConfig(
-        transpiler_config_path="some_path",
-        source_dialect="some_dialect",
-        input_source="some_source",
-        output_folder="some_output",
-        error_file_path="some_file",
-        transpiler_options={"b": "c"},
-        sdk_config={"c": "d"},
-        skip_validation=True,
-        catalog_name="some_catalog",
-        schema_name="some_schema",
-    )
-    installer.save_config(config)
-    retrieved = ApplicationContext(ws).transpile_config
-    assert retrieved == config
-
-
 class PatchedPypiInstaller(PypiInstaller):
 
     @classmethod
