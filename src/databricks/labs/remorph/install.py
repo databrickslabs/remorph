e53cec7289c274e23a0e61f2e27039f548911cae
sundar.shankar <sundar.shankar@databricks.com>
2025-05-30 08:55:31 +0530

    Fix local install failing test

--- src/databricks/labs/remorph/install.py
+++ src/databricks/labs/remorph/install.py
@@ -437,7 +437,6 @@ class MavenInstaller(TranspilerInstaller):
             if self._artifact
             else self.get_current_maven_artifact_version(self._group_id, self._artifact_id)
         )
-        self._latest_version = self.get_current_maven_artifact_version(self._group_id, self._artifact_id)
         if self._latest_version is None:
             logger.warning(f"Could not determine the latest version of Databricks {self._product_name} transpiler")
             logger.error("Failed to install transpiler: Databricks {self._product_name} transpiler")
