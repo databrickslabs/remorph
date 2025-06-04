ac4a9dddcb8c348a0d1443e982e8965447d05413
Eric Vergnaud <eric.vergnaud@databricks.com>
2025-06-04 13:52:14 +0200

    test INFAPC/SPARKSQL

--- tests/integration/transpile/test_bladerunner.py
+++ tests/integration/transpile/test_bladerunner.py
@@ -1,9 +1,16 @@
+import logging
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
+from databricks.labs.remorph.config import TranspileConfig
 from databricks.labs.remorph.install import TranspilerInstaller
+from databricks.labs.remorph.transpiler.execute import transpile
+from databricks.labs.remorph.transpiler.lsp.lsp_engine import LSPEngine
 
 
-def test_transpiles_informatica_with_sparksql():
+logger = logging.getLogger(__name__)
+
+async def test_transpiles_informatica_with_sparksql(ws):
     artifact = (
         Path(__file__).parent.parent.parent
         / "resources"
@@ -14,4 +21,22 @@ def test_transpiles_informatica_with_sparksql():
     )
     assert artifact.exists()
     TranspilerInstaller.install_from_pypi("bladerunner", "databricks-labs-bladerunner", artifact)
+    # check execution
+    config_path = TranspilerInstaller.transpilers_path() / "morpheus" / "lib" / "config.yml"
+    lsp_engine = LSPEngine.from_config_path(config_path)
+    input_source = Path(__file__).parent.parent.parent / "resources" / "functional" / "dbt"
+    with TemporaryDirectory() as output_folder:
+        transpile_config = TranspileConfig(
+            transpiler_config_path=str(config_path),
+            source_dialect="informatica (desktop edition)",
+            input_source=str(input_source),
+            output_folder=output_folder,
+            skip_validation=False,
+            catalog_name="catalog",
+            schema_name="schema",
+            transpiler_options={"tech-target": "SPARKSQL"},
+        )
+        result = await transpile(ws, lsp_engine, transpile_config)
+        logger.info(result)
+
 
