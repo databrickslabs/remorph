9a00f7e91c32e7a5884426bfb30e1ebfd894613d
sundar.shankar <sundar.shankar@databricks.com>
2025-06-05 17:02:32 -0400

    Name fixes

--- src/databricks/labs/remorph/cli.py
+++ src/databricks/labs/remorph/cli.py
@@ -308,9 +308,9 @@ def install_transpile(w: WorkspaceClient, artifact: str | None = None):
 
 
 @remorph.command(is_unauthenticated=False)
-def install_reconcile(w: WorkspaceClient):
-    """Install the Remorph Reconcile package"""
-    with_user_agent_extra("cmd", "install-reconcile")
+def configure_reconcile(w: WorkspaceClient):
+    """Configure the Remorph Reconcile Package"""
+    with_user_agent_extra("cmd", "configure-reconcile")
     user = w.current_user
     logger.debug(f"User: {user}")
     dbsql_id = _create_warehouse(w)
